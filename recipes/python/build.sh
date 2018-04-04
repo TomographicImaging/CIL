816f803197e324f37dd713ce40a87dc8df2ca20d
Edoardo Pasca <edo.paskino@gmail.com>
2018-04-04 14:35:44 +0100

    removed unused recipe (#89)

--- recipes/python/build.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$CIL_VERSION" ]; then
-    echo "Need to set CIL_VERSION"
-    exit 1
-fi  
-mkdir ${SRC_DIR}/ccpi
-cp -r "${RECIPE_DIR}/../../../" ${SRC_DIR}/ccpi
-
-cd ${SRC_DIR}/ccpi/Wrappers/python
-$PYTHON setup.py install

1881dad5f1863ae53af362ee2ca88b8be0284789
Edoardo Pasca <edo.paskino@gmail.com>
2018-11-09 18:14:27 +0000

    Added initial unittest for reader
    bugfix for python 2.7

--- Wrappers/Python/ccpi/framework.py
+++ Wrappers/Python/ccpi/framework.py
@@ -17,7 +17,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from __future__ import absolute_import
 from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import abc
 import numpy
 import sys

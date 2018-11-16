27556d128d72063dc6fa59e1de7afbe64cbb52e9
Edoardo Pasca <edo.paskino@gmail.com>
2018-11-16 17:18:36 +0000

    project as CMake package
    only conda build is tested.

--- Wrappers/Python/conda-recipe/build.sh
+++ Wrappers/Python/conda-recipe/build.sh
@@ -1,5 +1,12 @@
 mkdir ${SRC_DIR}/ccpi
 cp -r "${RECIPE_DIR}/../../../" ${SRC_DIR}/ccpi
 
-cd ${SRC_DIR}/ccpi/Wrappers/Python
-$PYTHON setup.py install
+#cd ${SRC_DIR}/ccpi/Wrappers/Python
+#$PYTHON setup.py install
+mkdir ${SRC_DIR}/build/
+cd ${SRC_DIR}/build
+
+
+
+cmake ../ccpi $RECIPE_DIR/../../../ -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+make install

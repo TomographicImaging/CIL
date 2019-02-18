8a92d3602a07c6f73689fd550b8e4080857cb7f1
Edoardo Pasca <edo.paskino@gmail.com>
2019-02-18 16:24:59 +0000

    added operators directory

--- Wrappers/Python/setup.py
+++ Wrappers/Python/setup.py
@@ -28,7 +28,7 @@ cil_version='0.11.3'
 setup(
     name="ccpi-framework",
     version=cil_version,
-    packages=['ccpi' , 'ccpi.io', 'ccpi.optimisation'],
+    packages=['ccpi' , 'ccpi.io', 'ccpi.optimisation', 'ccpi.optimisation.operators'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine

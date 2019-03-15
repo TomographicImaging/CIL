5585a3caf6832908ea64089fe47666a4e1b72c76
Edoardo Pasca <edo.paskino@gmail.com>
2019-03-15 16:37:21 +0000

    add docstring

--- Wrappers/Python/ccpi/optimisation/operators/Operator.py
+++ Wrappers/Python/ccpi/optimisation/operators/Operator.py
@@ -12,12 +12,19 @@ class Operator(object):
         '''Returns if the operator is linear'''
         return False
     def direct(self,x, out=None):
+        '''Returns the application of the Operator on x'''
         raise NotImplementedError
     def norm(self):
+        '''Returns the norm of the Operator'''
         raise NotImplementedError
     def range_geometry(self):
+        '''Returns the range of the Operator: Y space'''
         raise NotImplementedError
     def domain_geometry(self):
+        '''Returns the domain of the Operator: X space'''
         raise NotImplementedError
     def __rmul__(self, scalar):
+        '''Defines the multiplication by a scalar on the left
+
+        returns a ScaledOperator'''
         return ScaledOperator(self, scalar)

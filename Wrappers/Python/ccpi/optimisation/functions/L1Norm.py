c1ce6acc73edd30115575cd13e4e3f2e1bb108e6
Edoardo Pasca <edo.paskino@gmail.com>
2019-04-01 16:36:27 +0100

    added header

--- Wrappers/Python/ccpi/optimisation/functions/L1Norm.py
+++ Wrappers/Python/ccpi/optimisation/functions/L1Norm.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#   This work is part of the Core Imaging Library developed by
+#   Visual Analytics and Imaging System Group of the Science Technology
+#   Facilities Council, STFC
+
+#   Copyright 2018-2019 Evangelos Papoutsellis and Edoardo Pasca
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 """
 Created on Wed Mar  6 19:42:34 2019
 

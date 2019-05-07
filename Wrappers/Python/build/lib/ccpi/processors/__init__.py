0a1af2dd369c13639c0348d89a156d762f9832eb
epapoutsellis <epapoutsellis@gmail.com>
2019-05-07 23:03:30 +0100

    delete build pycache

--- Wrappers/Python/build/lib/ccpi/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 30 13:51:09 2019
-
-@author: ofn77899
-"""
-
-from .CenterOfRotationFinder import CenterOfRotationFinder
-from .Normalizer import Normalizer

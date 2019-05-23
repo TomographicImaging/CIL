20f3490926d4f7a98c747d7f2093cec04e8d6005
epapoutsellis <epapoutsellis@gmail.com>
2019-05-23 17:30:18 +0100

    check accuracy, add seed

--- Wrappers/Python/ccpi/optimisation/functions/FunctionOperatorComposition.py
+++ Wrappers/Python/ccpi/optimisation/functions/FunctionOperatorComposition.py
@@ -76,8 +76,8 @@ if __name__ == '__main__':
     
     Aop = AstraProjectorSimple(ig, ag, 'cpu')    
 
-    u = ig.allocate('random_int')
-    u1 = ig.allocate('random_int')
+    u = ig.allocate('random_int', seed=15)
+    u1 = ig.allocate('random_int', seed=10)
     b = Aop.direct(u1)
     
         

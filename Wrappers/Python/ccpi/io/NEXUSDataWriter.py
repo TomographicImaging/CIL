4964469cdbe1794f970037f79f84dcfb9290e341
Edoardo Pasca <edo.paskino@gmail.com>
2019-07-02 16:24:15 +0100

    added header

--- Wrappers/Python/ccpi/io/NEXUSDataWriter.py
+++ Wrappers/Python/ccpi/io/NEXUSDataWriter.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu May  2 10:11:20 2019
+#  CCP in Tomographic Imaging (CCPi) Core Imaging Library (CIL).
 
-@author: evelina
-"""
+#   Copyright 2017 UKRI-STFC
+#   Copyright 2017 University of Manchester
 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import numpy
 import os
--- Wrappers/Python/ccpi/optimisation/functions/FunctionOperatorComposition.py
+++ Wrappers/Python/ccpi/optimisation/functions/FunctionOperatorComposition.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  8 09:55:36 2019
+#  CCP in Tomographic Imaging (CCPi) Core Imaging Library (CIL).
 
-@author: evangelos
-"""
+#   Copyright 2017 UKRI-STFC
+#   Copyright 2017 University of Manchester
 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 from ccpi.optimisation.functions import Function
 from ccpi.optimisation.functions import ScaledFunction
 

017404c7e9d88e59e3f98febc355c36bcc24e9d2
epapoutsellis <epapoutsellis@gmail.com>
2019-07-03 13:20:33 +0100

    fix docum operator dir

--- Wrappers/Python/ccpi/optimisation/operators/IdentityOperator.py
+++ Wrappers/Python/ccpi/optimisation/operators/IdentityOperator.py
@@ -1,18 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  6 19:30:51 2019
-
-@author: evangelos
-"""
+#========================================================================
+# Copyright 2019 Science Technology Facilities Council
+# Copyright 2019 University of Manchester
+#
+# This work is part of the Core Imaging Library developed by Science Technology
+# Facilities Council and University of Manchester
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#=========================================================================
 
 from ccpi.optimisation.operators import LinearOperator
 import scipy.sparse as sp
 import numpy as np
-from ccpi.framework import ImageData
 
 
 class Identity(LinearOperator):
+    
+    '''
+
+        Identity:  Id: X -> Y,  Id(x) = x\in Y
+                       
+                   X : gm_domain
+                   Y : gm_range ( Default: Y = X )
+                                                                                                                                   
+    '''    
+    
     
     def __init__(self, gm_domain, gm_range=None):
 
@@ -24,38 +46,75 @@ class Identity(LinearOperator):
         super(Identity, self).__init__()
         
     def direct(self,x,out=None):
+        
+        '''
+        
+            Returns Id(x)
+        
+        '''
+        
         if out is None:
             return x.copy()
         else:
             out.fill(x)
     
     def adjoint(self,x, out=None):
+        
+        '''
+        
+            Returns Id(x)
+        
+        '''         
+        
+        
         if out is None:
             return x.copy()
         else:
             out.fill(x)
         
     def calculate_norm(self, **kwargs):
+        
+        '''
+        
+            Evaluates operator norm of Identity
+        
+        '''        
+        
         return 1.0
         
-    def domain_geometry(self):       
+    def domain_geometry(self): 
+        
+        '''
+        
+            Returns domain_geometry of Identity
+        
+        '''        
+        
         return self.gm_domain
         
     def range_geometry(self):
+        
+        '''
+        
+            Returns range_geometry of Identity
+        
+        '''         
+        
         return self.gm_range
     
+            
     def matrix(self):
         
         return sp.eye(np.prod(self.gm_domain.shape))
     
     def sum_abs_row(self):
         
-        return self.gm_range.allocate(1)#ImageData(np.array(np.reshape(abs(self.matrix()).sum(axis=0), self.gm_domain.shape, 'F')))
- 
+        return self.gm_range.allocate(1)
+    
     def sum_abs_col(self):
         
-        return self.gm_domain.allocate(1)#ImageData(np.array(np.reshape(abs(self.matrix()).sum(axis=1), self.gm_domain.shape, 'F')))
-            
+        return self.gm_domain.allocate(1)
+    
     
 if __name__ == '__main__':
     
--- Wrappers/Python/ccpi/optimisation/operators/ZeroOperator.py
+++ Wrappers/Python/ccpi/optimisation/operators/ZeroOperator.py
@@ -1,16 +1,44 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  6 19:25:53 2019
+#========================================================================
+# Copyright 2019 Science Technology Facilities Council
+# Copyright 2019 University of Manchester
+#
+# This work is part of the Core Imaging Library developed by Science Technology
+# Facilities Council and University of Manchester
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#=========================================================================
 
-@author: evangelos
-"""
-
-import numpy as np
-from ccpi.framework import ImageData
 from ccpi.optimisation.operators import LinearOperator
 
 class ZeroOperator(LinearOperator):
+    
+    '''
+
+        ZeroOperator:  O: X -> Y,  maps any element of x\in X into the zero element in Y
+                       O(x) = O_{Y}
+                       
+                       X : gm_domain
+                       Y : gm_range ( Default: Y = X )
+                       
+                       
+                       Note: O^{*}: Y^{*} -> X^{*} (Adjoint)
+                       
+                              < O(x), y > = < x, O^{*}(y) >
+                       
+                                                      
+        
+    '''
     
     def __init__(self, gm_domain, gm_range=None):
         
@@ -23,22 +51,59 @@ class ZeroOperator(LinearOperator):
                    
         
     def direct(self,x,out=None):
+        
+        '''
+        
+            Returns O(x)
+        
+        '''
+        
+        
         if out is None:
             return self.gm_range.allocate()
         else:
             out.fill(self.gm_range.allocate())
     
     def adjoint(self,x, out=None):
+        
+        '''
+        
+            Returns O^{*}(y)
+        
+        '''        
+        
         if out is None:
             return self.gm_domain.allocate()
         else:
             out.fill(self.gm_domain.allocate())
         
     def calculate_norm(self, **kwargs):
-        return 0.
+        
+        '''
+        
+            Evaluates operator norm of ZeroOperator
+        
+        '''
+        
+        return 0
     
-    def domain_geometry(self):       
+    def domain_geometry(self): 
+        
+        '''
+        
+            Returns domain_geometry of ZeroOperator
+        
+        '''
+        
+        
         return self.gm_domain  
         
     def range_geometry(self):
+        
+        '''
+        
+            Returns domain_geometry of ZeroOperator
+        
+        '''
+        
         return self.gm_range

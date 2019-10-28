650356e61d157da1d7656f4b3b1e7a409fe05786
Edoardo Pasca <edo.paskino@gmail.com>
2019-10-28 10:32:15 +0000

    add future imports (#411)

--- Wrappers/Python/ccpi/optimisation/algorithms/Algorithm.py
+++ Wrappers/Python/ccpi/optimisation/algorithms/Algorithm.py
@@ -20,7 +20,9 @@
 #
 #=========================================================================
 
-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import time, functools
 from numbers import Integral
 

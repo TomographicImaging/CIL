72ba26645c77e2f6053004e520a2d7d05d5790ee
epapoutsellis <epapoutsellis@gmail.com>
2019-11-15 01:40:44 +0000

    BlockDataContainer should give a geometry when shape is the same

--- Wrappers/Python/ccpi/framework/BlockDataContainer.py
+++ Wrappers/Python/ccpi/framework/BlockDataContainer.py
@@ -61,7 +61,11 @@ class BlockDataContainer(object):
     def __init__(self, *args, **kwargs):
         ''''''
         self.containers = args
-        self.index = 0
+        self.index = 0        
+        self.geometry = None
+        if len(set([i.shape for i in self.containers])):
+            self.geometry = self.containers[0].geometry
+                
         shape = kwargs.get('shape', None)
         if shape is None:
            shape = (len(args),1)
@@ -508,26 +512,32 @@ if __name__ == '__main__':
     import numpy
 
     N, M = 2, 3
-    ig = ImageGeometry(N, M)    
-    BG = BlockGeometry(ig, ig)
+    ig = ImageGeometry(N, M) 
+    
+    ig1 = ImageGeometry(2*N, 4*M) 
+    BG = BlockGeometry(ig, ig1)
     
     U = BG.allocate('random_int')
     V = BG.allocate('random_int')
     
+    print(U.geometry)
     
-    print ("test sum BDC " )
-    w = U[0].as_array() + U[1].as_array()    
-    w1 = sum(U).as_array()    
-    numpy.testing.assert_array_equal(w, w1)
+    print(len(set([i.shape for i in U.containers]))==1)
     
-    print ("test sum BDC " )
-    z = numpy.sqrt(U[0].as_array()**2 + U[1].as_array()**2)
-    z1 = sum(U**2).sqrt().as_array()    
-    numpy.testing.assert_array_equal(z, z1)   
     
-    z2 = U.pnorm(2)
-    
-    zzz = U.dot(V)
+#    print ("test sum BDC " )
+#    w = U[0].as_array() + U[1].as_array()    
+#    w1 = sum(U).as_array()    
+#    numpy.testing.assert_array_equal(w, w1)
+#    
+#    print ("test sum BDC " )
+#    z = numpy.sqrt(U[0].as_array()**2 + U[1].as_array()**2)
+#    z1 = sum(U**2).sqrt().as_array()    
+#    numpy.testing.assert_array_equal(z, z1)   
+#    
+#    z2 = U.pnorm(2)
+#    
+#    zzz = U.dot(V)
     
    
 

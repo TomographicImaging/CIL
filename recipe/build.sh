3a6731c20d16b99ca23a1a41b4f304aaf02cb6b4
Edoardo Pasca <edo.paskino@gmail.com>
2019-11-15 11:39:17 +0000

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@ if [ -z "$CIL_VERSION" ]; then
     exit 1
 fi
 # mkdir ${SRC_DIR}/ccpi
-# cp -r "${RECIPE_DIR}/../../../" ${SRC_DIR}/ccpi
+mkdir -p ${SRC_DIR}/ccpi/Wrappers/Python
+cp -r "${RECIPE_DIR}/../Wrappers/Python/test" ${SRC_DIR}/ccpi/Wrappers/Python
 
 # cd ${SRC_DIR}/ccpi/Wrappers/Python
 # $PYTHON setup.py install
@@ -13,9 +14,11 @@ mkdir ${SRC_DIR}/build_framework
 #cp -r "${RECIPE_DIR}/../" ${SRC_DIR}/build_framework
 
 cd ${SRC_DIR}/build_framework
-cmake ${RECIPE_DIR}/../ -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
-
-make install
+cmake ${RECIPE_DIR}/../ -DCONDA_BUILD=ON \
+                        -DCMAKE_BUILD_TYPE="Release"\
+                        -DLIBRARY_LIB=$CONDA_PREFIX/lib \
+                        -DLIBRARY_INC=$CONDA_PREFIX \
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 make install
 # $PYTHON setup.py install

abcc2af666ff39759d992c17cffdca571650d185
epapoutsellis <epapoutsellis@gmail.com>
2019-11-15 00:01:10 +0000

    change kwargs in KL, not ready yet

--- Wrappers/Python/ccpi/optimisation/functions/KullbackLeibler.py
+++ Wrappers/Python/ccpi/optimisation/functions/KullbackLeibler.py
@@ -24,7 +24,7 @@ from __future__ import unicode_literals
 
 import numpy
 from ccpi.optimisation.functions import Function
-from ccpi.optimisation.functions.ScaledFunction import ScaledFunction 
+
 import functools
 import scipy.special
 
@@ -40,21 +40,27 @@ class KullbackLeibler(Function):
             
     '''
     
-    def __init__(self, data, **kwargs):
+    def __init__(self,  **kwargs):
         
-        super(KullbackLeibler, self).__init__()
+        super(KullbackLeibler, self).__init__(L=None)
         
-        self.b = data    
-        self.bnoise = data * 0.
+        self.x = kwargs.get('b', None)           
+        self.eta = kwargs.get('eta',self.x * 0.0)
         
+        if self.x is None:
+            raise ValueError('Please define data')
+                
+        if self.x.as_array().any()<0:
+            raise ValueError('Data should be is larger or equal to 0')                                        
                                                     
     def __call__(self, x):
         
 
         '''Evaluates KullbackLeibler at x'''
-
-        ind = x.as_array()>0
-        tmp = scipy.special.kl_div(self.b.as_array()[ind], x.as_array()[ind])                
+        
+        tmp_sum = (x + self.eta).as_array()
+        ind = tmp_sum > 0
+        tmp = scipy.special.kl_div(self.x.as_array()[ind], tmp_sum[ind])                
         return numpy.sum(tmp) 
 
     def log(self, datacontainer):
@@ -68,21 +74,33 @@ class KullbackLeibler(Function):
         
         '''Evaluates gradient of KullbackLeibler at x'''
         
-        if out is None:
-            return 1 - self.b/(x + self.bnoise)
-        else:
-
-            x.add(self.bnoise, out=out)
-            self.b.divide(out, out=out)
-            out.subtract(1, out=out)
-            out *= -1
+        tmp_sum = x + self.eta
+        tmp_sum_array = tmp_sum.as_array()  
+        tmp_out = x.geometry.allocate()
+        tmp_out.as_array()[tmp_sum_array>0] = 1 - self.x.as_array()[tmp_sum_array>0]/tmp_sum_array[tmp_sum_array>0]        
+                             
+        if out is None: 
+                          
+            return type(x)(tmp_out)
+        
+        else:    
+            
+            out.fill(tmp_out)
+            
+#            # TODO not working with ind
+#            x.add(self.background_term, out = out)            
+#            self.data.divide(out, out=out)
+#            out.as_array()[np.isinf(out.as_array())]=0
+#            out.subtract(1, out=out)
+#            out *= -1
+            
             
     def convex_conjugate(self, x):
         
         '''Convex conjugate of KullbackLeibler at x'''
         
-        xlogy = - scipy.special.xlogy(self.b.as_array(), 1 - x.as_array())
-        return numpy.sum(xlogy)
+        xlogy = - scipy.special.xlogy(self.data.as_array(), 1 - x.as_array())         
+        return numpy.sum(xlogy) - (self.background_term * x).sum()
             
     def proximal(self, x, tau, out=None):
         
@@ -93,151 +111,155 @@ class KullbackLeibler(Function):
         '''
 
         if out is None:        
-            return 0.5 *( (x - self.bnoise - tau) + ( (x + self.bnoise - tau)**2 + 4*tau*self.b   ) .sqrt() )
+            return 0.5 *( (x - self.eta - tau) + ( (x + self.eta - tau)**2 + 4*tau*self.x   ) .sqrt() )
         else:
             
-            tmp =  0.5 *( (x - self.bnoise - tau) + 
-                        ( (x + self.bnoise - tau)**2 + 4*tau*self.b   ) .sqrt()
+            tmp =  0.5 *( (x - self.eta - tau) + 
+                        ( (x + self.eta - tau)**2 + 4*tau*self.x   ) .sqrt()
                         )
-            x.add(self.bnoise, out=out)
+            x.add(self.eta, out=out)
             out -= tau
             out *= out
-            tmp = self.b * (4 * tau)
+            tmp = self.x * (4 * tau)
             out.add(tmp, out=out)
             out.sqrt(out=out)
             
-            x.subtract(self.bnoise, out=tmp)
+            x.subtract(self.eta, out=tmp)
             tmp -= tau
             
             out += tmp
             
             out *= 0.5
                             
-    def proximal_conjugate(self, x, tau, out=None):
-        
-        r'''Proximal operator of the convex conjugate of KullbackLeibler at x:
-           
-           .. math::     prox_{\tau * f^{*}}(x)
-        '''
-
-                
-        if out is None:
-            z = x + tau * self.bnoise
-            return 0.5*((z + 1) - ((z-1)**2 + 4 * tau * self.b).sqrt())
-        else:
-            
-            tmp = tau * self.bnoise
-            tmp += x
-            tmp -= 1
-            
-            self.b.multiply(4*tau, out=out)    
-            
-            out.add((tmp)**2, out=out)
-            out.sqrt(out=out)
-            out *= -1
-            tmp += 2
-            out += tmp
-            out *= 0.5
-
-    def __rmul__(self, scalar):
-        
-        '''Multiplication of KullbackLeibler with a scalar        
-            
-            Returns: ScaledFunction
-        '''
-        
-        return ScaledFunction(self, scalar) 
+#    def proximal_conjugate(self, x, tau, out=None):
+#        
+#        r'''Proximal operator of the convex conjugate of KullbackLeibler at x:
+#           
+#           .. math::     prox_{\tau * f^{*}}(x)
+#        '''
+#
+#                
+#        if out is None:
+#            z = x + tau * self.background_term
+#            return 0.5*((z + 1) - ((z-1)**2 + 4 * tau * self.data).sqrt())
+#        else:
+#            
+#            tmp = tau * self.background_term
+#            tmp += x
+#            tmp -= 1
+#            
+#            self.data.multiply(4*tau, out=out)    
+#            
+#            out.add((tmp)**2, out=out)
+#            out.sqrt(out=out)
+#            out *= -1
+#            tmp += 2
+#            out += tmp
+#            out *= 0.5
+#
+#    def __rmul__(self, scalar):
+#        
+#        '''Multiplication of KullbackLeibler with a scalar        
+#            
+#            Returns: ScaledFunction
+#        '''
+#        
+#        return ScaledFunction(self, scalar) 
 
 
 if __name__ == '__main__':
     
     from ccpi.framework import ImageGeometry
-    import numpy
+    import numpy as np
     
     M, N =  2,3
     ig = ImageGeometry(voxel_num_x=M, voxel_num_y = N)
-    u = ig.allocate('random_int')
-    b = ig.allocate('random_int')
-    u.as_array()[1,1]=0
-    u.as_array()[2,0]=0
-    b.as_array()[1,1]=0
-    b.as_array()[2,0]=0    
     
-    f = KullbackLeibler(b)
+    # if both u1, g1 > 0
+    u1 = ig.allocate('random_int', seed = 5)
+    g1 = ig.allocate('random_int', seed = 10)
+    b1 = ig.allocate('random_int', seed = 100)
     
+    f = KullbackLeibler(b=g1)        
+    numpy.testing.assert_equal(0.0, f(g1)) 
     
-#    longest = reduce(lambda x, y: len(x) if len(x) > len(y) else len(y), strings)
+    background_term = b1
+    background_term.as_array()[0,0] = -10000
+    background_term.as_array()[0,1] = - u1.as_array()[0,1]    
+    
+    f1 = KullbackLeibler(b=g1, eta = background_term)   
+    res_gradient = f1.gradient(u1)
+    print(res_gradient.as_array())
+    
+    res_gradient_out = u1.geometry.allocate()
+    f1.gradient(u1, out = res_gradient_out)
+    print(res_gradient_out.as_array())
+    
+#    u1.add(background_term, out = div)
+#    g1.divide(div, out=div)
+#    div.subtract(1, out=div)
+#    div *= -1
+#    div.as_array()[numpy.isinf(div.as_array())] = 0
 
-
-#    tmp = functools.reduce(lambda x, y: \
-#                           0 if x==0 and not numpy.isnan(y) else x * numpy.log(y), \
-#                           zip(b.as_array().ravel(), u.as_array().ravel()),0)
-    
-    
-#    np.multiply.reduce(X, 0)
-    
-    
-#                sf = reduce(lambda x,y: x + y[0]*y[1],
-#                            zip(self.as_array().ravel(),
-#                                other.as_array().ravel()),
-#                            0)        
-#cdef inline number_t xlogy(number_t x, number_t y) nogil:
-#    if x == 0 and not zisnan(y):
-#        return 0
-#    else:
-#        return x * zlog(y)        
-    
-#    if npy_isnan(x):
-#        return x
-#    elif x > 0:
-#        return -x * log(x)
-#    elif x == 0:
-#        return 0
-#    else:
-#        return -inf    
-    
-#        cdef inline double kl_div(double x, double y) nogil:
-#    if npy_isnan(x) or npy_isnan(y):
-#        return nan
-#    elif x > 0 and y > 0:
-#        return x * log(x / y) - x + y
-#    elif x == 0 and y >= 0:
-#        return y
-#    else:
-#        return inf    
-
-    
-    
-    
-#    def xlogy(self, dc1, dc2):
-        
-#        return numpy.sum(numpy.where(dc1.as_array() != 0, dc2.as_array() * numpy.log(dc2.as_array() / dc1.as_array()), 0))
-        
-           
-    
-#    f.xlog(u, b)
-    
-            
-
-    
-#    tmp1 = b.as_array()
-#    tmp2 = u.as_array()
 #    
-#    zz = scipy.special.xlogy(tmp1, tmp2)
+#    tmp_sum = u1 + background_term
+#    tmp_sum_array = tmp_sum.as_array()
+
+#    
+##    div.as_array()[tmp_sum_array>0] = g1.as_array()[tmp_sum_array>0]/tmp_sum_array[tmp_sum_array>0]
+##    
+##        
+#    g1.divide(tmp_sum, out=g1)
+#    g1.add(1)
+#    print(g1.as_array())
+    
+    
+#    np.copyto(div.as_array(),g1.as_array()[tmp_sum_array>0]/tmp_sum_array[tmp_sum_array>0])
+    
+    
+#    div.as_array()[tmp_sum_array>0].fill((g1.as_array()[tmp_sum_array>0]/tmp_sum_array[tmp_sum_array>0]))
+            
+#            )
+    
+#    np.copyto(tmp.array[k], t.array)
+    
+#    print(div.as_array())
+#    
+    
+#    ind = tmp_sum>0 
+#    
+#    out_grad = u1.geometry.allocate()
+#    
+#    np.copyto(out_grad.as_array()[ind], (1 - (g1.as_array()/tmp_sum))[ind])
+#    
+    
+#        ind = tmp_sum>0 
+#        
+#        if out is None: 
+#            
+#            self.out_grad.fill(1 - self.data.as_array()[ind]/tmp_sum[ind])
+#    
+    
+    
+#    f1 = KullbackLeibler(g1, background_term = u1)        
+#    numpy.testing.assert_equal(0.0, f1(g1))     
+    
+    
+    
+#    print(f(g1))
+#    print(f(u1))
+    
+#    g2 = g1.clone()
+#    g2.as_array()[0,1] = 0
+##    print(f(g2))
 #
-#    print(np.sum(zz))
-    
-    
-#    ww = f.xlogy(b, u)
-    
-#    print(ww)
-    
-    
-#cdef inline double kl_div(double x, double y) nogil:
-  
-    
+#
+#    tmp = scipy.special.kl_div(g1.as_array(), g2.as_array())  
+#    
+#    
+#    res_grad = f.gradient()
     
         
 
     
-    
+        

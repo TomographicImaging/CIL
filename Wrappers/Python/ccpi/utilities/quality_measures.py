6f9d7da2836c8c735e4d1efad2cb05d0271a68e1
Edoardo Pasca <edo.paskino@gmail.com>
2020-03-27 14:48:06 +0000

    fixed headers and moved test to test directory

--- Wrappers/Python/ccpi/utilities/quality_measures.py
+++ Wrappers/Python/ccpi/utilities/quality_measures.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  5 16:04:24 2020
+#  CCP in Tomographic Imaging (CCPi) Core Imaging Library (CIL).
 
-@author: evangelos
-"""
+#   Copyright 2017 UKRI-STFC
+#   Copyright 2017 University of Manchester
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 from ccpi.optimisation.functions import L2NormSquared, L1Norm
 import numpy as np

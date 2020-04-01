31271523ddac661c5f25b52844485f0c3c2e680e
Jakob Jorgensen, WS at HMXIF <jakob.jorgensen@manchester.ac.uk>
2020-04-01 22:55:22 +0100

    Change MaskOperator example to PDHG TV denoising simple and working.

--- Wrappers/Python/ccpi/optimisation/operators/MaskOperator.py
+++ Wrappers/Python/ccpi/optimisation/operators/MaskOperator.py
@@ -68,74 +68,150 @@ if __name__ == '__main__':
     print(y.as_array())
     print((diag*(diag*x)).as_array())
     
-    from ccpi.framework import ImageGeometry, ImageData, \
-                            AcquisitionGeometry, BlockDataContainer, AcquisitionData
-    
     import numpy as np 
     import numpy                          
     import matplotlib.pyplot as plt
     
-    from ccpi.optimisation.algorithms import CGLS
-    from ccpi.optimisation.operators import BlockOperator, Gradient, Identity
+    from ccpi.optimisation.algorithms import PDHG
+    
+    from ccpi.optimisation.operators import BlockOperator, Identity, Gradient
+    from ccpi.optimisation.functions import ZeroFunction, L1Norm, \
+                          MixedL21Norm, BlockFunction, L2NormSquared,\
+                              KullbackLeibler
     from ccpi.framework import TestData
-           
-    import os, sys
+    import os
+    import sys
+    
+    # user supplied input
+    if len(sys.argv) > 1:
+        which_noise = int(sys.argv[1])
+    else:
+        which_noise = 2
+    print ("Applying {} noise")
+    
+    if len(sys.argv) > 2:
+        method = sys.argv[2]
+    else:
+        method = '1'
+        
+    method = '0'
+    print ("method ", method)
+    
     
     loader = TestData(data_dir=os.path.join(sys.prefix, 'share','ccpi'))
     data = loader.load(TestData.SHAPES)
     ig = data.geometry
     ag = ig
     
-    noisy_data = ImageData(TestData.random_noise(data.as_array(), mode = 'gaussian', seed = 1))
-    #noisy_data = ImageData(data.as_array())
-    
     mask = ig.allocate()
     mask = mask + 1
     amask = mask.as_array()
-    amask[160:180,60:80] = 0.0
+    amask[140:160,10:90] = 0.0
+    amask[70:130,140:160] = 0.0
+    amask[15:50,180:240] = 0.0
+    amask[95:105,180:295] = 0.0
     
     MO = MaskOperator(mask)
+    
+    # Create noisy data. 
+    noises = ['gaussian', 'poisson', 's&p']
+    noise = noises[which_noise]
+    if noise == 's&p':
+        n1 = TestData.random_noise(data.as_array(), mode = noise, salt_vs_pepper = 0.9, amount=0.2)
+    elif noise == 'poisson':
+        scale = 5
+        n1 = TestData.random_noise( data.as_array()/scale, mode = noise, seed = 10)*scale
+    elif noise == 'gaussian':
+        n1 = TestData.random_noise(data.as_array(), mode = noise, seed = 10)
+    else:
+        raise ValueError('Unsupported Noise ', noise)
+    noisy_data = ig.allocate()
+    noisy_data.fill(n1)
     
     noisy_data = MO.direct(noisy_data)
     
     # Show Ground Truth and Noisy Data
-    plt.figure(figsize=(10,10))
-    plt.subplot(2,1,1)
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
     plt.imshow(data.as_array())
     plt.title('Ground Truth')
     plt.colorbar()
-    plt.subplot(2,1,2)
+    plt.subplot(1,2,2)
     plt.imshow(noisy_data.as_array())
     plt.title('Noisy Data')
     plt.colorbar()
     plt.show()
     
-    # Setup and run the regularised CGLS algorithm  (Tikhonov with Gradient)
-    x_init = ig.allocate() 
-    alpha = 2
-    op = Gradient(ig)
     
-    A = MO
+    # Regularisation Parameter depending on the noise distribution
+    if noise == 's&p':
+        alpha = 0.8
+    elif noise == 'poisson':
+        alpha = 1
+    elif noise == 'gaussian':
+        alpha = .3
     
-    block_op = BlockOperator( A, alpha * op, shape=(2,1))
-    block_data = BlockDataContainer(noisy_data, op.range_geometry().allocate())
-       
-    cgls = CGLS(x_init=x_init, operator = block_op, data = block_data)
-    cgls.max_iteration = 200
-    cgls.update_objective_interval = 5
-    cgls.run(200, verbose = True)
+    # fidelity
+    if noise == 's&p':
+        f2 = L1Norm(b=noisy_data)
+    elif noise == 'poisson':
+        f2 = KullbackLeibler(noisy_data)
+    elif noise == 'gaussian':
+        f2 = 0.5 * L2NormSquared(b=noisy_data)
+    
+    if method == '0':
+    
+        # Create operators
+        op1 = Gradient(ig, correlation=Gradient.CORRELATION_SPACE)
+        op2 = MO #Identity(ig, ag)
+    
+        # Create BlockOperator
+        operator = BlockOperator(op1, op2, shape=(2,1) ) 
+    
+        # Create functions      
+        f = BlockFunction(alpha * MixedL21Norm(), f2) 
+        g = ZeroFunction()
+        
+    else:
+        
+        operator = Gradient(ig)
+        f =  alpha * MixedL21Norm()
+        g = f2
+            
+        
+    # Compute operator Norm
+    normK = operator.norm()
+    
+    # Primal & dual stepsizes
+    sigma = 1
+    tau = 1/(sigma*normK**2)
+    
+    
+    # Setup and run the PDHG algorithm
+    pdhg = PDHG(f=f,g=g,operator=operator, tau=tau, sigma=sigma)
+    pdhg.max_iteration = 2000
+    pdhg.update_objective_interval = 100
+    pdhg.run(2000)
     
     # Show results
-    plt.figure(figsize=(20,10))
-    plt.subplot(3,1,1)
-    plt.imshow(data.as_array())
+    plt.figure(figsize=(20,5))
+    plt.subplot(1,4,1)
+    plt.imshow(data.subset(channel=0).as_array())
     plt.title('Ground Truth')
-    plt.subplot(3,1,2)
-    plt.imshow(noisy_data.as_array())
-    plt.title('Noisy')
-    plt.subplot(3,1,3)
-    plt.imshow(cgls.get_output().as_array())
-    plt.title('Regularised GGLS with Gradient')
+    plt.colorbar()
+    plt.subplot(1,4,2)
+    plt.imshow(noisy_data.subset(channel=0).as_array())
+    plt.title('Noisy Data')
+    plt.colorbar()
+    plt.subplot(1,4,3)
+    plt.imshow(pdhg.get_output().subset(channel=0).as_array())
+    plt.title('TV Reconstruction')
+    plt.colorbar()
+    plt.subplot(1,4,4)
+    plt.plot(np.linspace(0,ig.shape[1],ig.shape[1]), data.as_array()[int(ig.shape[0]/2),:], label = 'GTruth')
+    plt.plot(np.linspace(0,ig.shape[1],ig.shape[1]), pdhg.get_output().as_array()[int(ig.shape[0]/2),:], label = 'TV reconstruction')
+    plt.legend()
+    plt.title('Middle Line Profiles')
     plt.show()
     
         

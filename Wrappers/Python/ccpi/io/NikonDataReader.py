3d802f1df6e5dfffa6a7ea2399f0a72b097bf357
Evelina Ametova <evelinaametova@gmail.com>
2020-06-09 15:51:46 +0100

    Fixed Nikon reader, added tiff stack reader, small fix of print in framework

--- Wrappers/Python/ccpi/io/NikonDataReader.py
+++ Wrappers/Python/ccpi/io/NikonDataReader.py
@@ -15,20 +15,15 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from ccpi.framework import AcquisitionData, AcquisitionGeometry
+from ccpi.io.TIFFStackReader import TIFFStackReader
 import numpy
 import os
-
-
-pilAvailable = True
-try:    
-    from PIL import Image
-except:
-    pilAvailable = False
     
         
 class NikonDataReader(object):
@@ -42,52 +37,52 @@ class NikonDataReader(object):
             
             xtek_file       full path to .xtexct file
             
-            roi             region-of-interest to load. If roi = -1 (default), 
+            roi             region-of-interest to load. 
+                            If roi = {'horizontal_x': -1, 'vertical': -1} (default), 
                             full projections will be loaded. Otherwise roi is 
-                            given by [(row0, row1), (column0, column1)], where 
-                            row0, column0 are coordinates of top left corner and 
+                            given by {'horizontal_x': (column0, column1), 'vertical': (row0, row1)}
+                            where row0, column0 are coordinates of top left corner and 
                             row1, column1 are coordinates of bottom right corner.
                             
-            binning         number of pixels to bin (combine) along 0 (column) 
-                            and 1 (row) dimension. If binning = [1, 1] (default),
+            binning         number of pixels to bin (combine) along corresponsing lables. 
+                            If binning = {'horizontal_x': 1, 'vertical': 1} (default),
                             projections in original resolution are loaded. Note, 
-                            if binning[0] != binning[1], then loaded projections
-                            will have anisotropic pixels, which are currently not 
-                            supported by the Framework
+                            anisotropic pixels are currently not supported by Framework
             
             normalize       normalize loaded projections by detector 
                             white level (I_0). Default value is False, 
                             i.e. no normalization.
                             
-            flip            default = False, flip projections in the left-right direction
+            fliplr          default = False, flip projections in the left-right direction
+                            (about vertical axis)
                     
         '''
         
         self.xtek_file = kwargs.get('xtek_file', None)
-        self.roi = kwargs.get('roi', -1)
-        self.binning = kwargs.get('binning', [1, 1])
+        self.roi = kwargs.get('roi', {'horizontal_x': -1, 'vertical': -1})
+        self.binning = kwargs.get('binning', {'horizontal_x': 1, 'vertical': 1})
         self.normalize = kwargs.get('normalize', False)
-        self.flip = kwargs.get('flip', False)
+        self.fliplr = kwargs.get('fliplr', False)
         
         if self.xtek_file is not None:
             self.set_up(xtek_file = self.xtek_file,
                         roi = self.roi,
                         binning = self.binning,
                         normalize = self.normalize,
-                        flip = self.flip)
+                        fliplr = self.fliplr)
             
     def set_up(self, 
                xtek_file = None, 
-               roi = -1, 
-               binning = [1, 1],
+               roi = {'horizontal_x': -1, 'vertical': -1}, 
+               binning = {'horizontal_x': 1, 'vertical': 1},
                normalize = False,
-               flip = False):
+               fliplr = False):
         
         self.xtek_file = xtek_file
         self.roi = roi
         self.binning = binning
         self.normalize = normalize
-        self.flip = flip
+        self.fliplr = fliplr
         
         if self.xtek_file == None:
             raise Exception('Path to xtek file is required.')
@@ -96,9 +91,29 @@ class NikonDataReader(object):
         if not(os.path.isfile(self.xtek_file)):
             raise Exception('File\n {}\n does not exist.'.format(self.xtek_file))  
                 
-        # check that PIL library is installed
-        if (pilAvailable == False):
-            raise Exception("PIL (pillow) is not available, cannot load TIFF files.")
+        # check labels
+        for key in self.binning.keys():
+            if key not in ['horizontal_x', 'vertical']:
+                raise Exception("Wrong label. horizontal_x and vertical are expected")
+        # check labels
+        for key in self.roi.keys():
+            if key not in ['horizontal_x', 'vertical']:
+                raise Exception("Wrong label. horizontal_x and vertical are expected")
+                
+        # check if inputs for roi and binning are integer
+        if not (isinstance(self.binning['horizontal_x'], int) and \
+                isinstance(self.binning['vertical'], int)):
+            raise Exception("Integers are expected for binning")
+        
+        if self.roi['horizontal_x'] != -1:
+            if not (isinstance(self.roi['horizontal_x'][0], int) and \
+                    isinstance(self.roi['horizontal_x'][1], int)):
+                raise Exception("Integers are expected for roi")
+        
+        if self.roi['vertical'] != -1:
+            if not (isinstance(self.roi['vertical'][0], int) and \
+                    isinstance(self.roi['vertical'][1], int)):
+                raise Exception("Integers are expected for roi")
                 
         # parse xtek file
         with open(self.xtek_file, 'r') as f:
@@ -141,27 +156,27 @@ class NikonDataReader(object):
             elif line.startswith("AngularStep"):
                 angular_step = float(line.split('=')[1])
                 
-        if self.roi == -1:
-            self._roi_par = [(0, pixel_num_v_0), \
-                              (0, pixel_num_h_0)]
+        roi_par = []
+        if self.roi['vertical'] == -1:
+            roi_par.append((0, pixel_num_v_0))
         else:
-            self._roi_par = self.roi.copy()
-            if self._roi_par[0] == -1:
-                self._roi_par[0] = (0, pixel_num_v_0)
-            if self._roi_par[1] == -1:
-                self._roi_par[1] = (0, pixel_num_h_0)
+            roi_par.append(self.roi['vertical'])
+        if self.roi['horizontal_x'] == -1:
+            roi_par.append((0, pixel_num_h_0))
+        else:
+            roi_par.append(self.roi['horizontal_x'])
                 
         # calculate number of pixels and pixel size
-        if (self.binning == [1, 1]):
-            pixel_num_v = self._roi_par[0][1] - self._roi_par[0][0]
-            pixel_num_h = self._roi_par[1][1] - self._roi_par[1][0]
+        if (self.binning['horizontal_x'] == 1 and self.binning['vertical'] == 1):
+            pixel_num_v = roi_par[0][1] - roi_par[0][0]
+            pixel_num_h = roi_par[1][1] - roi_par[1][0]
             pixel_size_v = pixel_size_v_0
             pixel_size_h = pixel_size_h_0
         else:
-            pixel_num_v = (self._roi_par[0][1] - self._roi_par[0][0]) // self.binning[0]
-            pixel_num_h = (self._roi_par[1][1] - self._roi_par[1][0]) // self.binning[1]
-            pixel_size_v = pixel_size_v_0 * self.binning[0]
-            pixel_size_h = pixel_size_h_0 * self.binning[1]
+            pixel_num_v = (roi_par[0][1] - roi_par[0][0]) // self.binning['vertical']
+            pixel_num_h = (roi_par[1][1] - roi_par[1][0]) // self.binning['horizontal_x']
+            pixel_size_v = pixel_size_v_0 * self.binning['vertical']
+            pixel_size_h = pixel_size_h_0 * self.binning['horizontal_x']
         
         '''
         Parse the angles file .ang or _ctdata.txt file and returns the angles
@@ -170,7 +185,7 @@ class NikonDataReader(object):
         input_path = os.path.dirname(self.xtek_file)
         angles_ctdata_file = os.path.join(input_path, '_ctdata.txt')
         angles_named_file = os.path.join(input_path, self._experiment_name+'.ang')
-        angles = numpy.zeros(num_projections, dtype = 'float')
+        angles = numpy.zeros(num_projections, dtype = numpy.float32)
         
         # look for _ctdata.txt
         if os.path.exists(angles_ctdata_file):
@@ -198,7 +213,7 @@ class NikonDataReader(object):
             angles = numpy.flipud(angles + initial_angle) # angles are in the reverse order
             
         else:   # calculate angles based on xtek file
-            angles = initial_angle + angular_step * range(num_projections)
+            angles = numpy.asarray( [ initial_angle + angular_step * proj for proj in range(num_projections) ] , dtype=numpy.float32)
         
         # fill in metadata
         self._ag = AcquisitionGeometry(geom_type = 'cone', 
@@ -229,36 +244,51 @@ class NikonDataReader(object):
             
         # get path to projections
         path_projection = os.path.dirname(self.xtek_file)
-        
-        # get number of projections
         num_projections = numpy.shape(self._ag.angles)[0]
         
-        # allocate array to store projections    
-        data = numpy.zeros((num_projections, self._ag.pixel_num_v, self._ag.pixel_num_h), dtype = float)
+        reader = TIFFStackReader()
+        reader.set_up(path = path_projection,
+                      n_images = num_projections,
+                      binning = {'axis_0': self.binning['vertical'], 'axis_1': self.binning['horizontal_x']},
+                      roi = {'axis_0': self.roi['vertical'], 'axis_1': self.roi['horizontal_x']})
+
+        data = reader.load_images()
         
-        for i in range(num_projections):
-            
-            filename = (path_projection + '/' + self._experiment_name + '_{:04d}.tif').format(i + 1)
-            
-            try:
-                tmp = numpy.asarray(Image.open(filename), dtype = float)
-            except:
-                print('Error reading\n {}\n file.'.format(filename))
-                raise
-                
-            if (self.binning == [1, 1]):
-                data[i, :, :] = tmp[self._roi_par[0][0]:self._roi_par[0][1], self._roi_par[1][0]:self._roi_par[1][1]]
-            else:
-                shape = (self._ag.pixel_num_v, self.binning[0], 
-                         self._ag.pixel_num_h, self.binning[1])
-                data[i, :, :] = tmp[self._roi_par[0][0]:(self._roi_par[0][0] + (((self._roi_par[0][1] - self._roi_par[0][0]) // self.binning[0]) * self.binning[0])), \
-                                    self._roi_par[1][0]:(self._roi_par[1][0] + (((self._roi_par[1][1] - self._roi_par[1][0]) // self.binning[1]) * self.binning[1]))].reshape(shape).mean(-1).mean(1)
+        # sanity check
+        if (data.shape[0] != num_projections or\
+            data.shape[1] != self._ag.pixel_num_v or \
+            data.shape[2] != self._ag.pixel_num_h):
+            raise Exception("Something went wrong. Geometry and data dimensions do not match.")
         
+#        # get number of projections
+#        num_projections = numpy.shape(self._ag.angles)[0]
+#        
+#        # allocate array to store projections    
+#        data = numpy.zeros((num_projections, self._ag.pixel_num_v, self._ag.pixel_num_h), dtype = numpy.float32)
+#        
+#        for i in range(num_projections):
+#            
+#            filename = (path_projection + '/' + self._experiment_name + '_{:04d}.tif').format(i + 1)
+#            
+#            try:
+#                tmp = numpy.asarray(Image.open(filename), dtype = numpy.float32)
+#            except:
+#                print('Error reading\n {}\n file.'.format(filename))
+#                raise
+#                
+#            if (self.binning['horizontal_x'] == 1 and self.binning['vertical'] == 1):
+#                data[i, :, :] = tmp[self._roi_par[0][0]:self._roi_par[0][1], self._roi_par[1][0]:self._roi_par[1][1]]
+#            else:
+#                shape = (self._ag.pixel_num_v, self.binning['vertical'], 
+#                         self._ag.pixel_num_h, self.binning['horizontal_x'])
+#                data[i, :, :] = tmp[self._roi_par[0][0]:(self._roi_par[0][0] + (((self._roi_par[0][1] - self._roi_par[0][0]) // self.binning['vertical']) * self.binning['vertical'])), \
+#                                    self._roi_par[1][0]:(self._roi_par[1][0] + (((self._roi_par[1][1] - self._roi_par[1][0]) // self.binning['horizontal_x']) * self.binning['horizontal_x']))].reshape(shape).mean(-1).mean(1)
+#        
         if (self.normalize):
             data /= self._white_level
             data[data > 1] = 1
         
-        if self.flip:
+        if self.fliplr:
             return AcquisitionData(array = data[:, :, ::-1], 
                                    deep_copy = False,
                                    geometry = self._ag,
@@ -276,13 +306,17 @@ class NikonDataReader(object):
 
 '''
 # usage example
-xtek_file = '/home/evelina/nikon_data/SophiaBeads_256_averaged.xtekct'
+from ccpi.io import NikonDataReader
+import matplotlib.pyplot as plt
+
+# usage example
+xtek_file = '/media/newhd/shared/Data/SophiaBeads/SophiaBeads_256_averaged/SophiaBeads_256_averaged.xtekct'
 reader = NikonDataReader()
 reader.set_up(xtek_file = xtek_file,
-              binning = [1, 1],
-              roi = -1,
+              binning = {'horizontal_x': 8, 'vertical': 1},
+              roi = {'horizontal_x': (500,1500), 'vertical': (100,900)},
               normalize = True,
-              flip = True)
+              fliplr = True)
 
 data = reader.load_projections()
 print(data)
---
+++ Wrappers/Python/ccpi/io/TIFFStackReader.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+#  CCP in Tomographic Imaging (CCPi) Core Imaging Library (CIL).
+
+#   Copyright 2019 UKRI-STFC
+#   Copyright 2019 University of Manchester
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy
+import os
+
+
+pilAvailable = True
+try:    
+    from PIL import Image
+except:
+    pilAvailable = False
+
+
+class TIFFStackReader(object):
+    
+    def __init__(self, 
+                 **kwargs):
+        '''
+        Constructor
+        
+        Input:
+            
+            path            path to tiff files
+            
+            n_images        number of images to read, -1 (default) read all
+            
+            skip            number of images to skip, default 0
+            
+            roi             region-of-interest to load. 
+                            If roi = {'axis_0': -1, 'axis_1': -1} (default), 
+                            full images will be loaded. Otherwise roi is 
+                            given by {'axis_0': (row0, row1), 'axis_1': (column0, column1)}
+                            where row0, column0 are coordinates of top left corner and 
+                            row1, column1 are coordinates of bottom right corner.
+                            
+            binning         number of pixels to bin (combine) along corresponsing axis. 
+                            If binning = {'axis_0': 1, 'axis_1': 1} (default),
+                            images in original resolution are loaded. 
+            
+            transpose       Transpose loaded images, default False
+            
+            Notes:
+            roi and benning are specified for axes before transpose.
+            
+                    
+        '''
+        
+        self.path = kwargs.get('path', None)
+        self.n_images = kwargs.get('n_images', -1)
+        self.skip = kwargs.get('skip', 0)
+        self.roi = kwargs.get('roi', {'axis_0': -1, 'axis_1': -1})
+        self.binning = kwargs.get('binning', {'axis_0': 1, 'axis_1': 1})
+        self.transpose = kwargs.get('transpose', False)
+        
+        if self.path is not None:
+            self.set_up(path = self.path,
+                        n_images = self.n_images,
+                        skip = self.skip,
+                        roi = self.roi,
+                        binning = self.binning,
+                        transpose = self.normalize)
+            
+    def set_up(self, 
+               path = None, 
+               n_images = -1,
+               skip = 0,
+               roi = {'axis_0': -1, 'axis_1': -1}, 
+               binning = {'axis_0': 1, 'axis_1': 1},
+               transpose = False):
+        
+        self.path = path
+        self.n_images = n_images
+        self.skip = skip
+        self.roi = roi
+        self.binning = binning
+        self.transpose = transpose
+        
+        if self.path == None:
+            raise Exception('Path to tiff files is required.')
+        
+        if self.n_images != -1:
+             if not (isinstance(self.n_images, int)):
+                 raise Exception("Number of images to read must be integer.")
+                 
+        if self.skip != -1:
+             if not (isinstance(self.skip, int)):
+                 raise Exception("Number of images to skip must be integer.")
+            
+        # check if path exists
+        if not(os.path.exists(self.path)):
+            raise Exception('Path \n {}\n does not exist.'.format(self.path))  
+                
+        # check that PIL library is installed
+        if (pilAvailable == False):
+            raise Exception("PIL (pillow) is not available, cannot load TIFF files.")
+        
+        # check labels
+        for key in self.binning.keys():
+            if key not in ['axis_0', 'axis_1']:
+                raise Exception("Wrong label. axis_0 and axis_1 are expected")
+        # check labels
+        for key in self.roi.keys():
+            if key not in ['axis_0', 'axis_1']:
+                raise Exception("Wrong label. axis_0 and axis_1 are expected")
+                
+        # check if inputs for roi and binning are integer
+        if not (isinstance(self.binning['axis_0'], int) and \
+                isinstance(self.binning['axis_1'], int)):
+            raise Exception("Integers are expected for binning")
+        
+        if self.roi['axis_0'] != -1:
+            if not (isinstance(self.roi['axis_0'][0], int) and \
+                    isinstance(self.roi['axis_0'][1], int)):
+                raise Exception("Integers are expected for roi")
+        
+        if self.roi['axis_1'] != -1:
+            if not (isinstance(self.roi['axis_1'][0], int) and \
+                    isinstance(self.roi['axis_1'][1], int)):
+                raise Exception("Integers are expected for roi")
+        
+        self._tiff_files = [f for f in os.listdir(self.path) if (os.path.isfile(os.path.join(self.path, f)) and '.tif' in f.lower())]
+
+        if not self._tiff_files:
+            raise Exception("No tiff files were found in the directory \n{}".format(self.path))
+        
+        self._tiff_files.sort()
+        
+        if self.skip > len(self._tiff_files):
+            raise Exception("Number of files to skip is larger than number of tiff files in {}".format(self.path))        
+        
+                
+    def load_images(self):
+        
+        '''
+        Load images and return numpy array
+        '''
+        # load first image to find out dimensions
+        filename = os.path.join(self.path, self._tiff_files[0])
+        
+        try:
+            tmp = numpy.asarray(Image.open(filename), dtype = numpy.float32)
+        except:
+            print('Error reading\n {}\n file.'.format(filename))
+            raise
+        
+        n_rows_raw, n_cols_raw = tmp.shape
+
+        roi_par = []
+        if self.roi['axis_0'] == -1:
+            roi_par.append((0, n_rows_raw))
+        else:
+            roi_par.append(self.roi['axis_0'])
+        if self.roi['axis_1'] == -1:
+            roi_par.append((0, n_cols_raw))
+        else:
+            roi_par.append(self.roi['axis_1'])
+                
+        # calculate number of pixels and pixel size
+        if (self.binning['axis_0'] == 1 and self.binning['axis_1'] == 1):
+            n_rows = roi_par[0][1] - roi_par[0][0]
+            n_cols = roi_par[1][1] - roi_par[1][0]
+        else:
+            n_rows = (roi_par[0][1] - roi_par[0][0]) // self.binning['axis_0']
+            n_cols = (roi_par[1][1] - roi_par[1][0]) // self.binning['axis_1']
+        
+        if (self.n_images == -1 or self.n_images > (len(self._tiff_files) - self.skip)):
+            num_to_read = len(self._tiff_files) - self.skip
+        else:
+            num_to_read = self.n_images
+        
+        if not self.transpose:
+            im = numpy.zeros((num_to_read, n_rows, n_cols), dtype=numpy.float32)
+        else:
+            im = numpy.zeros((num_to_read, n_cols, n_rows), dtype=numpy.float32)
+            
+        for i in range(num_to_read):
+            
+            filename = filename = os.path.join(self.path, self._tiff_files[i + self.skip])
+            
+            try:
+                raw = numpy.asarray(Image.open(filename), dtype = numpy.float32)
+            except:
+                print('Error reading\n {}\n file.'.format(filename))
+                raise
+                
+            if (self.binning['axis_0'] == 1 and self.binning['axis_1'] == 1):
+                tmp = raw[roi_par[0][0]:roi_par[0][1], roi_par[1][0]:roi_par[1][1]]
+            else:
+                shape = (n_rows, self.binning['axis_0'], 
+                         n_cols, self.binning['axis_1'])
+                tmp = raw[roi_par[0][0]:(roi_par[0][0] + (((roi_par[0][1] - roi_par[0][0]) // self.binning['axis_0']) * self.binning['axis_0'])), \
+                          roi_par[1][0]:(roi_par[1][0] + (((roi_par[1][1] - roi_par[1][0]) // self.binning['axis_1']) * self.binning['axis_1']))].reshape(shape).mean(-1).mean(1)
+            
+            if self.transpose:
+                im[i, :, :] = numpy.transpose(tmp)
+            else:
+                im[i, :, :] = tmp
+        
+        return im
+
+
+'''
+import matplotlib.pyplot as plt
+from ccpi.io import TIFFStackReader
+
+path = '/media/newhd/shared/Data/SophiaBeads/SophiaBeads_256_averaged/'
+
+reader = TIFFStackReader()
+reader.set_up(path = path,
+              n_images = 100,
+              binning = {'axis_0': 5, 'axis_1': 6},
+              roi = {'axis_0': (100,900), 'axis_1': (200,700)},
+              skip = 100)
+
+data = reader.load_images()
+
+plt.imshow(data[1, :, :])
+plt.show()
+'''

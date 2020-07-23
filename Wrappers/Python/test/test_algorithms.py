21ad4f9deb338fdfac019fa264bb6d9d42c1cc98
Edoardo Pasca <edo.paskino@gmail.com>
2020-07-23 10:44:58 +0100

    check max is less or equal than upper bound

--- Wrappers/Python/test/test_algorithms.py
+++ Wrappers/Python/test/test_algorithms.py
@@ -184,7 +184,9 @@ class TestAlgorithms(unittest.TestCase):
         alg2 = SIRT(x_init=x_init, operator=identity, data=b, upper=0.3)
         alg2.max_iteration = 200
         alg2.run(20, verbose=True)
-        self.assertTrue(alg2.get_output().max()==0.3)
+        self.assertLessEqual(alg2.get_output().max(), 0.3)
+	# maybe we could add a test to compare alg.get_output() when < upper bound is 
+	# the same as alg2.get_output() and otherwise 0.3
         
     def test_FISTA(self):
         print ("Test FISTA")

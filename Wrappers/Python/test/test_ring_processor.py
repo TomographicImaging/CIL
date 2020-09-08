1257229cd3d58a5ade2706339bb1c1ecacd9d92c
Edoardo Pasca <edo.paskino@gmail.com>
2020-09-08 13:08:29 +0000

    added download from internet

--- Wrappers/Python/test/test_ring_processor.py
+++ Wrappers/Python/test/test_ring_processor.py
@@ -27,13 +27,39 @@ import os
 import numpy as np
 import unittest
 from ccpi.io import NEXUSDataReader
+try:
+    import wget
+    has_wget = True
+except ImportError as ie:
+    has_wget = False
+
 
 class TestRingProcessor(unittest.TestCase):
     
+    def setUp(self):
+        self.list_of_files = []
+        self.url = 'https://www.ccpi.ac.uk/sites/www.ccpi.ac.uk/files/'
+        self.cwd = os.getcwd()
+        np.random.seed(1)
+    # @property
+    # def list_of_files(self):
+    #     return self._list_of_files
+    # @list_of_files.setter
+    # def list_of_files(self, value):
+    #     print("adding", value)
+    #     self._list_of_files.append(value)
+
+    def tearDown(self):
+        for f in self.list_of_files:
+            the_file = os.path.join(self.cwd , f)
+            if os.path.exists(the_file):
+                os.remove(the_file)
+                print ("removed" , the_file)
+
     def test_2D_demo_ring(self):
         
         print("Start 2D ring removal in simulated sinogram")
-    
+        
         model = 1 # select a model number from the library
         N = 512 # set dimension of the phantom
         path = os.path.dirname(tomophantom.__file__)
@@ -63,22 +89,29 @@ class TestRingProcessor(unittest.TestCase):
         ring_recon = ring_removal.get_output()
         
         # load ring processed result sinogram 2D 
+        wget.download(self.url + 'result_sinogram_2D_ring_remover.nxs', 
+          out = self.cwd)
+        self.list_of_files.append('result_sinogram_2D_ring_remover.nxs')
+        
         reader = NEXUSDataReader()
-        reader.set_up(nexus_file = os.path.join(os.getcwd(), 'result_sinogram_2D_ring_remover.nxs'))
+        reader.set_up(nexus_file = os.path.join(self.cwd, self.list_of_files[-1]))
         tmp = reader.load_data()      
         
         print("Check ring remover sinogram 2D")
-        np.testing.assert_array_equal(tmp.as_array(), ring_recon.as_array()) 
+        np.testing.assert_array_almost_equal(tmp.as_array(), ring_recon.as_array()) 
         print("Test passed\n")        
                     
-    
+    @unittest.skipUnless(has_wget, "has wget")
     def test_3D(self):
         
         print("Start 3D ring removal in real data")
-        
+        wget.download(self.url + 'Sinogram_Rock_sample_3D.nxs', 
+          out = self.cwd)
+        self.list_of_files.append('Sinogram_Rock_sample_3D.nxs')
+        fname = os.path.join(self.cwd, self.list_of_files[-1])
         # Load data --> sirt after flat and after ring
         read_sinogram3D = NEXUSDataReader()
-        read_sinogram3D.set_up(nexus_file = 'Sinogram_Rock_sample_3D.nxs')
+        read_sinogram3D.set_up(nexus_file = fname)
         
         sinogram3D = read_sinogram3D.load_data()
         
@@ -89,21 +122,28 @@ class TestRingProcessor(unittest.TestCase):
         ring_recon = ring_removal.get_output()
         
         # load ring processed result sinogram rock 3D 
+        self.list_of_files.append('result_sinogram_rock_3D_ring_remover.nxs')
+        wget.download(self.url + self.list_of_files[-1],
+           out = self.cwd)
         reader = NEXUSDataReader()
-        reader.set_up(nexus_file = os.path.join(os.getcwd(), 'result_sinogram_rock_3D_ring_remover.nxs'))
+        reader.set_up(nexus_file = os.path.join(self.cwd, self.list_of_files[-1]))
         tmp = reader.load_data()      
         
         print("Check ring remover sinogram 3D")
-        np.testing.assert_array_equal(tmp.as_array(), ring_recon.as_array())          
+        np.testing.assert_array_almost_equal(tmp.as_array(), ring_recon.as_array())          
         print("Test passed\n")
         
     
     def test_2D_channels(self):
         
         print("Start 2D+channels ring removal in real data")
-    
+        self.list_of_files.append('Sinogram_Rock_sample_2D_channels.nxs')
+        wget.download(self.url + self.list_of_files[-1],
+           out = self.cwd)
+        
         read_sinogram2D_chan = NEXUSDataReader()
-        read_sinogram2D_chan.set_up(nexus_file = 'Sinogram_Rock_sample_2D_channels.nxs')
+        read_sinogram2D_chan.set_up(nexus_file = 
+              os.path.join(self.cwd, self.list_of_files[-1]))
         
         sinogram2D_chan = read_sinogram2D_chan.load_data()
         
@@ -114,23 +154,30 @@ class TestRingProcessor(unittest.TestCase):
         ring_recon = ring_removal.get_output()
         
         # load ring processed result sinogram rock 3D 
+        self.list_of_files.append('result_sinogram_rock_2D_channels_ring_remover.nxs')
+        wget.download(self.url + self.list_of_files[-1],
+           out = self.cwd)
+        
         reader = NEXUSDataReader()
-        reader.set_up(nexus_file = os.path.join(os.getcwd(), 'result_sinogram_rock_2D_channels_ring_remover.nxs'))
+        reader.set_up(nexus_file = os.path.join(self.cwd, self.list_of_files[-1]))
         tmp = reader.load_data() 
 
         print("Check ring remover sinogram 2D_channels")
-        np.testing.assert_array_equal(tmp.as_array(), ring_recon.as_array()) 
+        np.testing.assert_array_almost_equal(tmp.as_array(), ring_recon.as_array()) 
         print("Test passed\n")        
             
     
     def test_3D_channels(self):
         
         print("Start 3D+channels ring removal in real data")
-    
+        self.list_of_files.append('Sinogram_Rock_sample_3D_channels.nxs')
+        wget.download(self.url + self.list_of_files[-1],
+           out = self.cwd)
+        
         # Load data --> sirt after flat and after ring
         read_sinogram3D_chan = NEXUSDataReader()
-        read_sinogram3D_chan.set_up(nexus_file = 'Sinogram_Rock_sample_3D_channels.nxs')
-        
+        read_sinogram3D_chan.set_up(nexus_file = 
+           os.path.join(self.cwd , self.list_of_files[-1]))
         sinogram3D_chan = read_sinogram3D_chan.load_data()
         
         print(sinogram3D_chan.shape)
@@ -140,12 +187,17 @@ class TestRingProcessor(unittest.TestCase):
         ring_recon = ring_removal.get_output()
         
         # load ring processed result sinogram rock 3D channels
+        self.list_of_files.append('result_sinogram_rock_3D_channels_ring_remover.nxs')
+        wget.download(self.url + self.list_of_files[-1],
+           out = self.cwd)
+        
         reader = NEXUSDataReader()
-        reader.set_up(nexus_file = os.path.join(os.getcwd(), 'result_sinogram_rock_3D_channels_ring_remover.nxs'))
+        reader.set_up(nexus_file = 
+           os.path.join(self.cwd, self.list_of_files[-1]))
         tmp = reader.load_data() 
 
         print("Check ring remover sinogram 3D_channels")
-        np.testing.assert_array_equal(tmp.as_array(), ring_recon.as_array()) 
+        np.testing.assert_array_almost_equal(tmp.as_array(), ring_recon.as_array()) 
         print("Test passed\n")        
                 
                 

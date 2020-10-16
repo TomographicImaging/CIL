fb26f730a94c8eb51542176d27047c1027595aae
Edoardo Pasca <edo.paskino@gmail.com>
2020-10-16 14:40:31 +0100

    SPDHG: make more memory update_objective
    closes #665

--- Wrappers/Python/ccpi/optimisation/algorithms/SPDHG.py
+++ Wrappers/Python/ccpi/optimisation/algorithms/SPDHG.py
@@ -193,12 +193,18 @@ class SPDHG(Algorithm):
         self.save_previous_iteration(i, y_k)
         
     def update_objective(self):
-         p1 = self.f(self.operator.direct(self.x)) + self.g(self.x)
-         d1 = - self.f.convex_conjugate(self.y_old)
-         tmp = -1*self.operator.adjoint(self.y_old)
-         d1 -= self.g.convex_conjugate(tmp)
+        # p1 = self.f(self.operator.direct(self.x)) + self.g(self.x)
+        p1 = 0.
+        for i,op in enumerate(self.operator.operators):
+            p1 += self.f[i](op.direct(self.x))
+        p1 += self.g(self.x)
 
-         self.loss.append([p1, d1, p1-d1])
+        d1 = - self.f.convex_conjugate(self.y_old)
+        tmp = self.operator.adjoint(self.y_old)
+        tmp *= -1
+        d1 -= self.g.convex_conjugate(tmp)
+
+        self.loss.append([p1, d1, p1-d1])
 
     @property
     def objective(self):

10a5c7a7791b666efca620817ff4f345c5ff11c0
Evelina Ametova <evelinaametova@gmail.com>
2020-11-02 09:36:50 +0000

    updated NEXUS writer

--- Wrappers/Python/ccpi/io/NEXUSDataWriter.py
+++ Wrappers/Python/ccpi/io/NEXUSDataWriter.py
@@ -20,7 +20,7 @@ from __future__ import division
 from __future__ import print_function
 import numpy as np
 import os
-from ccpi.framework import AcquisitionData, AcquisitionGeometry, ImageData, ImageGeometry
+from ccpi.framework import AcquisitionData, ImageData
 import datetime
 
 
@@ -92,30 +92,65 @@ class NEXUSDataWriter(object):
             else:
                 ds_data.attrs['data_type'] = 'AcquisitionData'
             
-            for i in range(self.data_container.as_array().ndim):
+            for i in range(self.data_container.number_of_dimensions):
                 ds_data.attrs['dim{}'.format(i)] = self.data_container.dimension_labels[i]
             
-            if (isinstance(self.data_container, AcquisitionData)):      
-                ds_data.attrs['geom_type'] = self.data_container.geometry.geom_type
-                ds_data.attrs['dimension'] = self.data_container.geometry.dimension
-                if self.data_container.geometry.dist_source_center is not None:
-                    ds_data.attrs['dist_source_center'] = self.data_container.geometry.dist_source_center
-                if self.data_container.geometry.dist_center_detector is not None:
-                    ds_data.attrs['dist_center_detector'] = self.data_container.geometry.dist_center_detector
-                ds_data.attrs['pixel_num_h'] = self.data_container.geometry.pixel_num_h
-                ds_data.attrs['pixel_size_h'] = self.data_container.geometry.pixel_size_h
-                ds_data.attrs['pixel_num_v'] = self.data_container.geometry.pixel_num_v
-                ds_data.attrs['pixel_size_v'] = self.data_container.geometry.pixel_size_v
-                ds_data.attrs['channels'] = self.data_container.geometry.channels
-                ds_data.attrs['n_angles'] = self.data_container.geometry.angles.shape[0]
+            if (isinstance(self.data_container, AcquisitionData)): 
                 
-                # create the dataset to store rotation angles
-                ds_angles = f.create_dataset('entry1/tomo_entry/data/rotation_angle', 
-                                             (self.data_container.geometry.angles.shape), 
-                                             dtype = 'float32', 
-                                             data = self.data_container.geometry.angles)
+                # create group to store configuration
+                f.create_group('entry1/tomo_entry/config')
+                f.create_group('entry1/tomo_entry/config/source')
+                f.create_group('entry1/tomo_entry/config/detector')
+                f.create_group('entry1/tomo_entry/config/rotation_axis')
                 
-                #ds_angles.attrs['units'] = self.data_container.geometry.angle_unit
+                ds_data.attrs['geometry'] = self.data_container.geometry.config.system.geometry
+                ds_data.attrs['dimension'] = self.data_container.geometry.config.system.dimension   
+                ds_data.attrs['num_channels'] = self.data_container.geometry.config.channels.num_channels
+                
+                f.create_dataset('entry1/tomo_entry/config/detector/direction_row', 
+                                 (self.data_container.geometry.config.system.detector.direction_row.shape), 
+                                 dtype = 'float32', 
+                                 data = self.data_container.geometry.config.system.detector.direction_row)
+                
+                f.create_dataset('entry1/tomo_entry/config/detector/position', 
+                                 (self.data_container.geometry.config.system.detector.position.shape), 
+                                 dtype = 'float32', 
+                                 data = self.data_container.geometry.config.system.detector.position)
+                
+                f.create_dataset('entry1/tomo_entry/config/source/position', 
+                                 (self.data_container.geometry.config.system.source.position.shape), 
+                                 dtype = 'float32', 
+                                 data = self.data_container.geometry.config.system.source.position)
+                
+                f.create_dataset('entry1/tomo_entry/config/rotation_axis/position', 
+                                 (self.data_container.geometry.config.system.rotation_axis.position.shape), 
+                                 dtype = 'float32', 
+                                 data = self.data_container.geometry.config.system.rotation_axis.position)
+                
+                f.create_dataset('entry1/tomo_entry/config/rotation_axis/direction', 
+                                 (self.data_container.geometry.config.system.rotation_axis.direction.shape), 
+                                 dtype = 'float32', 
+                                 data = self.data_container.geometry.config.system.rotation_axis.direction)
+                
+                ds_data.attrs['num_pixels_h'] = self.data_container.geometry.config.panel.num_pixels[0]
+                ds_data.attrs['pixel_size_h'] = self.data_container.geometry.config.panel.pixel_size[0]
+                
+                if self.data_container.geometry.config.system.dimension == '3D':
+                    f.create_dataset('entry1/tomo_entry/config/detector/direction_col', 
+                                     (self.data_container.geometry.config.system.detector.direction_col.shape), 
+                                     dtype = 'float32', 
+                                     data = self.data_container.geometry.config.system.detector.direction_col)
+                    
+                    ds_data.attrs['num_pixels_v'] = self.data_container.geometry.config.panel.num_pixels[1]
+                    ds_data.attrs['pixel_size_v'] = self.data_container.geometry.config.panel.pixel_size[1]
+                    
+                angles = f.create_dataset('entry1/tomo_entry/config/angles', 
+                                                 (self.data_container.geometry.config.angles.angle_data.shape), 
+                                                 dtype = 'float32', 
+                                                 data = self.data_container.geometry.config.angles.angle_data)
+                angles.attrs['angle_unit'] = self.data_container.geometry.config.angles.angle_unit   
+                angles.attrs['initial_angle'] = self.data_container.geometry.config.angles.initial_angle
+                angles.attrs['num_positions'] = self.data_container.geometry.config.angles.num_positions
             
             else:   # ImageData
                 
@@ -129,47 +164,4 @@ class NEXUSDataWriter(object):
                 ds_data.attrs['center_y'] = self.data_container.geometry.center_y
                 ds_data.attrs['center_z'] = self.data_container.geometry.center_z
                 ds_data.attrs['channels'] = self.data_container.geometry.channels
-
-
-'''
-# usage example
-xtek_file = '/home/evelina/nikon_data/SophiaBeads_256_averaged.xtekct'
-reader = NikonDataReader()
-reader.set_up(xtek_file = xtek_file,
-              binning = [3, 1],
-              roi = [200, 500, 1500, 2000],
-              normalize = True)
-
-data = reader.load_projections()
-ag = reader.get_geometry()
-
-writer = NEXUSDataWriter()
-writer.set_up(file_name = '/home/evelina/test_nexus.nxs',
-              data_container = data)
-
-writer.write_file()
-
-ig = ImageGeometry(voxel_num_x = 100,
-                   voxel_num_y = 100)
-im = ImageData(array = numpy.zeros((100, 100), dtype = 'float'),
-               geometry = ig)
-im_writer = NEXUSDataWriter()
-
-im_writer.set_up(file_name = '/home/evelina/test_nexus_im.nxs',
-                 data_container = im)
-im_writer.write_file()
-
-ag = AcquisitionGeometry(geom_type = 'parallel', 
-                         dimension = '2D', 
-                         angles = numpy.array([0, 1]), 
-                         pixel_num_h = 200, 
-                         pixel_size_h = 1, 
-                         pixel_num_v = 100, 
-                         pixel_size_v = 1)
-
-ad = ag.allocate()
-ag_writer = NEXUSDataWriter()
-ag_writer.set_up(file_name = '/home/evelina/test_nexus_ag.nxs',
-                 data_container = ad)
-ag_writer.write_file()
-'''
+                ds_data.attrs['channel_spacing'] = self.data_container.geometry.channel_spacing

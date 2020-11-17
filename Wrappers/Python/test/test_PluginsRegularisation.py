2982929a74e9fc4df3f25d5f6e1e2b5cb4eb1f49
Edoardo Pasca <edo.paskino@gmail.com>
2020-11-17 20:55:09 +0000

    updated message

--- Wrappers/Python/test/test_PluginsRegularisation.py
+++ Wrappers/Python/test/test_PluginsRegularisation.py
@@ -23,10 +23,11 @@ print ("has_regularisation_toolkit", has_regularisation_toolkit)
 
 class TestPlugin(unittest.TestCase):
     def setUp(self):
+        print ("test plugins")
         pass
     def tearDown(self):
         pass
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_FGP_TV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions.regularisers import FGP_TV
@@ -34,42 +35,42 @@ class TestPlugin(unittest.TestCase):
         except ModuleNotFoundError as ie:
             print (ie)
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_ROF_TV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import ROF_TV
             assert True
         except ModuleNotFoundError as ie:
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_TGV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import TGV
             assert True
         except ModuleNotFoundError as ie:
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_LLT_ROF(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import LLT_ROF
             assert True
         except ModuleNotFoundError as ie:
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_FGP_dTV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import FGP_dTV
             assert True
         except ModuleNotFoundError as ie:
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_SB_TV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import SB_TV
             assert True
         except ModuleNotFoundError as ie:
             assert False
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is installed")
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_import_TNV(self):
         try:
             from cil.plugins.ccpi_regularisation.functions import TNV

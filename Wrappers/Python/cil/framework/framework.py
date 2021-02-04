056ecd5641abc4c732bc5e952473947162d7df2a
gfardell <gemma.fardell@stfc.ac.uk>
2021-02-04 13:44:40 +0000

    removed imports

--- Wrappers/Python/cil/framework/framework.py
+++ Wrappers/Python/cil/framework/framework.py
@@ -21,8 +21,6 @@ from __future__ import print_function
 
 import copy
 import numpy
-import sys
-from datetime import timedelta, datetime
 import warnings
 from functools import reduce
 from numbers import Number

53dd347bcfdba5c1afe359b91a77e509ff832ba2
Edoardo Pasca <edo.paskino@gmail.com>
2021-03-05 12:01:40 +0000

    uses find_library only in Windows

--- Wrappers/Python/cil/optimisation/operators/GradientOperator.py
+++ Wrappers/Python/cil/optimisation/operators/GradientOperator.py
@@ -237,17 +237,15 @@ from ctypes import util
 if platform.system() == 'Linux':
     dll = 'libcilacc.so'
 elif platform.system() == 'Windows':
-    dll = 'cilacc.dll'
+    dll_file = 'cilacc.dll'
+    dll = util.find_library(dll_file)
 elif platform.system() == 'Darwin':
     dll = 'libcilacc.dylib'
 else:
     raise ValueError('Not supported platform, ', platform.system())
 
-dll_path = util.find_library(dll)
-if dll_path is None:
-    cilacc = ctypes.cdll.LoadLibrary(dll)
-else:
-    cilacc = ctypes.cdll.LoadLibrary(dll_path)
+cilacc = ctypes.cdll.LoadLibrary(dll)
+
 c_float_p = ctypes.POINTER(ctypes.c_float)
 
 cilacc.openMPtest.restypes = ctypes.c_int32

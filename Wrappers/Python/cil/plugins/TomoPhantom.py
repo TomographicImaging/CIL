1d4e44281bd8acaf67f5d54a7b7cf1e0295c6e30
Edoardo Pasca <edo.paskino@gmail.com>
2021-04-16 01:12:35 +0100

    completed get_ImageData and 2D of get_AcquisitionData

--- Wrappers/Python/cil/plugins/TomoPhantom.py
+++ Wrappers/Python/cil/plugins/TomoPhantom.py
@@ -16,7 +16,7 @@
 #   limitations under the License.
 
 from cil.framework import ImageGeometry, AcquisitionGeometry
-from cil.framework import ImageData, AcquisitionData
+from cil.framework import ImageData, AcquisitionData, DataOrder
 import tomophantom
 from tomophantom import TomoP2D, TomoP3D
 import os
@@ -94,6 +94,7 @@ def check_model_params(model, dims=2):
 def get_ImageData(model, geometry):
     '''Returns an ImageData relative to geometry with the model model from tomophantom'''
     ig = geometry.copy()
+    ig.set_labels(DataOrder.CIL_IG_LABELS)
     num_dims = len(ig.dimension_labels)
     
     if ImageGeometry.CHANNEL in ig.dimension_labels:
@@ -103,9 +104,14 @@ def get_ImageData(model, geometry):
             # 3D+time for tomophantom
             # output dimensions channel and then spatial, 
             # e.g. [ 'channel', 'vertical', 'horizontal_y', 'horizontal_x' ]
-            if not ((ig.shape[0] == ig.shape[1]) and (ig.shape[1] == ig.shape[2])) :
+            dimensions = [0,1,2,3]
+            for i,ax in enumerate(dimensions):
+                if ax == ImageGeometry.CHANNEL:
+                    dimensions.pop(i)
+            if not ((ig.shape[dimensions[0]] == ig.shape[dimensions[1]]) and\
+                    (ig.shape[dimensions[1]] == ig.shape[dimensions[2]])) :
                 raise ValueError('Can only handle cubic ImageData, got shape'.format(ig.shape))
-            N = ig.shape[0]
+            N = ig.shape[dimensions[0]]
             num_model = name_to_model_number(model)
             phantom_arr = TomoP3D.ModelTemporal(num_model, N, path_library3D)
         elif num_dims == 3:
@@ -120,7 +126,7 @@ def get_ImageData(model, geometry):
             if ig.shape[dimensions[0]] != ig.shape[dimensions[1]]:
                 raise ValueError('Can only handle square ImageData, got shape {} {}'\
                     .format(ig.shape[dimensions[0]], ig.shape[dimensions[1]]))
-            N = dimensions[0]
+            N = ig.shape[dimensions[0]]
             num_model = name_to_model_number(model)
             phantom_arr = TomoP2D.ModelTemporal(num_model, N, path_library2D)
         else:
@@ -128,14 +134,10 @@ def get_ImageData(model, geometry):
     else:
         if num_dims == 3:
             # 3D
-            dimensions = [0,1,2,4]
-            for i,ax in enumerate(dimensions):
-                if ax == ImageGeometry.CHANNEL:
-                    dimensions.pop(i)
-            
-            if not ((dimensions[0] != dimensions[1]) and (dimensions[1] != dimensions[2])) :
+                        
+            if not ((ig.shape[0] != ig.shape[1]) and (ig.shape[1] != ig.shape[2])) :
                 raise ValueError('Can only handle cubic ImageData, got shape'.format(ig.shape))
-            N = dimensions[0]
+            N = ig.shape[0]
             num_model = name_to_model_number(model)
             phantom_arr = TomoP3D.Model(num_model, N, path_library3D)
         elif num_dims == 2:
@@ -148,17 +150,84 @@ def get_ImageData(model, geometry):
         else:
             raise ValueError('Wrong ImageGeometry')
 
-        im_data = ImageData(phantom_arr, geometry=ig.copy(), suppress_warning=True)
+        im_data = ImageData(phantom_arr, geometry=ig, suppress_warning=True)
+        im_data.reorder(list(geometry.dimension_labels))
         return im_data
-    pass
+    
 
 
-def get_AcquisitionData(model, geometry):
+def get_AcquisitionData(model, geometry, fill_factor=1):
     '''Returns an AcquisitionData relative to geometry with the model model from tomophantom
     
     Notice this is only for circular paralell beam scans.
     '''
-    pass
+    ag = geometry.copy()
+    ag.set_labels(DataOrder.CIL_AG_LABELS)
+    num_dims = len(ag.dimension_labels)
+    # angles -- 1D array of projection angles in degrees
+    # https://github.com/dkazanc/TomoPhantom/blob/v1.4.9/Wrappers/Python/src/TomoP3D.pyx#L315
+    
+    conversion = 1. 
+    if ag.config.angles.angle_unit == AcquisitionGeometry.RADIAN:
+        conversion = 180. / np.pi
+    angles = np.asarray([ conversion * el for el in ag.angles ] , dtype=np.float32)
+    
+    if AcquisitionGeometry.CHANNEL in ag.dimension_labels:
+        if not is_model_temporal(model):
+            raise ValueError('Selected model {} is not a temporal model, please change your selection'.format(model))
+        if num_dims == 4:
+            # 3D+time for tomophantom
+            # Creates 4D (3D + time) analytical projection data of the dimension [TimeFrames, AngTot, Vert_det, Horiz_det]
+            # https://github.com/dkazanc/TomoPhantom/blob/v1.4.9/Wrappers/Python/src/TomoP3D.pyx#L307
+            dimensions = [0,1,2,3]
+            for i,ax in enumerate(dimensions):
+                if ax == AcquisitionGeometry.CHANNEL:
+                    dimensions.pop(i)
+            if not ((ag.shape[dimensions[0]] == ag.shape[dimensions[1]]) and\
+                    (ag.shape[dimensions[1]] == ag.shape[dimensions[2]])) :
+                raise ValueError('Can only handle cubic ImageData, got shape'.format(ag.shape))
+            N = ag.shape[dimensions[0]]
+            num_model = name_to_model_number(model)
+            
+            phantom_arr = TomoP3D.ModelSinoTemporal(num_model, N, *ag.panel.num_pixels, angles,  path_library3D)
+        elif num_dims == 3:
+            # 2D+time for tomophantom
+            # output dimensions channel and then spatial, 
+            # e.g. [ 'channel', 'vertical', 'horizontal_x' ]
+            dimensions = [0,1,2]
+            for i,ax in enumerate(dimensions):
+                if ax == AcquisitionGeometry.CHANNEL:
+                    dimensions.pop(i)
+                    
+            if ag.shape[dimensions[0]] != ag.shape[dimensions[1]]:
+                raise ValueError('Can only handle square ImageData, got shape {} {}'\
+                    .format(ag.shape[dimensions[0]], ag.shape[dimensions[1]]))
+            N = ag.shape[dimensions[0]]
+            num_model = name_to_model_number(model)
+            phantom_arr = TomoP2D.ModelSinoTemporal(num_model, N, ag.panel.num_pixels, angles, path_library2D)
+        else:
+            raise ValueError('Wrong ImageGeometry')
+    else:
+        if num_dims == 3:
+            # 3D
+            # Creates 3D analytical projection data of the dimension [AngTot, Vert_det, Horiz_det] 
+            # https://github.com/dkazanc/TomoPhantom/blob/v1.4.9/Wrappers/Python/src/TomoP3D.pyx#L273
+            if not ((ag.shape[0] != ag.shape[1]) and (ag.shape[1] != ag.shape[2])) :
+                raise ValueError('Can only handle cubic ImageData, got shape'.format(ag.shape))
+            N = ag.shape[0]
+            num_model = name_to_model_number(model)
+            phantom_arr = TomoP3D.ModelSino(num_model, N, *ag.panel.num_pixels, angles,  path_library3D)
+        elif num_dims == 2:
+            # 2D
+            N = int( ag.config.panel.num_pixels[0] * fill_factor )
+            num_model = name_to_model_number(model)
+            phantom_arr = TomoP2D.ModelSino(num_model, N, ag.config.panel.num_pixels[0], angles, path_library2D)
+        else:
+            raise ValueError('Wrong ImageGeometry')
+
+        acq_data = AcquisitionData(phantom_arr, geometry=ag, suppress_warning=True)
+        acq_data.reorder(list(geometry.dimension_labels))
+        return acq_data
 
 if __name__ == '__main__':
     # ig = ImageGeometry(512,512,512)

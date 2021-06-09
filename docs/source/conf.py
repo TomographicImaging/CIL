dfb16936f7e42e4039c5a0dc5e144d361502fec2
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-06-09 11:51:48 +0100

    Update sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Wrappers/Python/cil'))
+sys.path.insert(0, os.path.abspath('../Wrappers/Python/'))
 
 
 # -- Project information -----------------------------------------------------

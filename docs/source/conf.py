d2449634620aa0fa31b7cdef450e0dcaad7ebf40
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-06-11 11:56:51 +0100

    Add path to cil-astra

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../Wrappers/Python/'))
+sys.path.insert(0, os.path.abspath('../../CIL-ASTRA/Wrappers/Python/'))
 
 # -- Project information -----------------------------------------------------
 

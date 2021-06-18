6a48b4ee3ded3183de22bc56b259ff80ddb6f561
GitHub Action <action@github.com>
2021-06-18 17:16:42 +0000

    Update documentation

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v21.1.0-41-ga972bda',
+    VERSION: 'v21.1.0-42-gb813262',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

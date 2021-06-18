0c16eba9132fce7c72a10cf93fc1bf5e88bb0f91
Edoardo Pasca <edo.paskino@gmail.com>
2021-06-18 07:35:36 +0100

    minor code simplifications

--- Wrappers/Python/cil/processors/Padder.py
+++ Wrappers/Python/cil/processors/Padder.py
@@ -17,18 +17,19 @@
 
 
 from cil.framework import DataProcessor, AcquisitionData, ImageData
-import warnings
 import numpy
+from numbers import Number
 
 
 class Padder(DataProcessor):
     r'''
-    Processor to pad an array. Please use the desiried method to configure a processor for your needs.
+    Processor to pad an array. Please use the desired method to configure a processor for your needs.
     '''
 
     @staticmethod
     def constant(pad_width=None, constant_values=0):
         r'''This creates a Padder processor which pads with a constant value.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         :param constant_values: The values to set the padded values for each axis, specified as a dictionary containing a tuple (before, after). A tuple (before, after) yields same before and after pad for each axis. float is a shortcut for before = after = pad value for all axes. Default is 0.
@@ -40,6 +41,7 @@ class Padder(DataProcessor):
     @staticmethod
     def edge(pad_width=None):
         r'''This creates a Padder processor which pads with the edge values of array.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         '''
@@ -49,6 +51,7 @@ class Padder(DataProcessor):
     @staticmethod
     def linear_ramp(pad_width=None, end_values=0):
         r'''This creates a Padder processor which pads with the linear ramp between end_value and the array edge value.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         :param end_values: The values used for the ending value of the linear_ramp and that will form the edge of the padded array., specified as a dictionary containing a tuple (before, after). A tuple (before, after) yields same before and after pad for each axis. float is a shortcut for before = after = pad value for all axes. Default is 0.
@@ -60,6 +63,7 @@ class Padder(DataProcessor):
     @staticmethod
     def reflect(pad_width=None):
         r'''This creates a Padder processor which pads with the reflection of the vector mirrored on the first and last values of the vector along each axis.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         '''
@@ -69,6 +73,7 @@ class Padder(DataProcessor):
     @staticmethod
     def symmetric(pad_width=None):
         r'''This creates a Padder processor which pads with the reflection of the vector mirrored along the edge of the array.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         '''
@@ -78,6 +83,7 @@ class Padder(DataProcessor):
     @staticmethod
     def wrap(pad_width=None):
         r'''This creates a Padder processor which pads with the wrap of the vector along the axis. The first values are used to pad the end and the end values are used to pad the beginning.
+
         :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
         '''
@@ -90,12 +96,27 @@ class Padder(DataProcessor):
                  constant_values=0,
                  end_values=0):
         r'''
-        Processor to pad an array. 
-        :param pad_width: number of values padded to the edges of each axis, specified as a dictionary containing a tuple (before, after) or an int (before=after). A tuple (before, after) yields same before and after pad for each axis. int is a shortcut for before = after = pad width for all axes.
+        Processor to pad an array wrapping numpy.pad.
+
+        :param mode: specifies the method to use for padding. Available methods: constant, 
+        edge, linear_ramp, reflect, symmetric, wrap.
+        :type mode: string, default 'constant'.
+        :param pad_width: number of values padded to the edges of each axis, specified as 
+        a dictionary containing a tuple (before, after) or an int (before=after). The dictionary keys
+        must be the dimension_labels of the DataContainer (ImageData or AcquisitionData) that one wants
+        to pad.
+         A tuple (before, after) yields same before and after pad for each axis. 
+         int is a shortcut for before = after = pad width for all axes.
         :type pad_width: int or tuple or dict
-        :param constant_values: Used in 'constant' mode. The values to set the padded values for each axis, specified as a dictionary containing a tuple (before, after). A tuple (before, after) yields same before and after pad for each axis. float is a shortcut for before = after = pad value for all axes. Default is 0.
+        :param constant_values: Used in 'constant' mode. The values to set the padded values for each axis, 
+        specified as a dictionary containing a tuple (before, after). 
+        A tuple (before, after) yields same before and after pad for each axis. 
+        float is a shortcut for before = after = pad value for all axes. Default is 0.
         :type constant_values: float or tuple or dict
-        :param edge_values: Used in 'linear_ramp'. The values used for the ending value of the linear_ramp and that will form the edge of the padded array., specified as a dictionary containing a tuple (before, after). A tuple (before, after) yields same before and after pad for each axis. float is a shortcut for before = after = pad value for all axes. Default is 0.
+        :param edge_values: Used in 'linear_ramp'. The values used for the ending value of the linear_ramp
+         and that will form the edge of the padded array, specified as a dictionary containing a tuple
+         (before, after). A tuple (before, after) yields same before and after pad for each axis.
+         float is a shortcut for before = after = pad value for all axes. Default is 0.
         :type edge_values: float or tuple or dict
         '''
 
@@ -136,20 +157,12 @@ class Padder(DataProcessor):
         geometry_0 = data.geometry
         geometry = geometry_0.copy()
 
-        res, pad_width_param = self._parse_param(data, self.pad_width, ndim, dimension_labels)
+        pad_width_param = self._parse_param(data, self.pad_width, ndim, 'pad_width')
         
-        if res == False:
-            raise ValueError('Cannot parse provided pad_width. Please, provide int, tuple or dictionary with dimension lables.')
+        constant_values_param = self._parse_param(data, self.constant_values, ndim, \
+             'constant_values')
 
-        res, constant_values_param = self._parse_param(data, self.constant_values, ndim, dimension_labels)
-
-        if res == False:
-            raise ValueError('Cannot parse provided constant_values. Please, provide float, tuple or dictionary with dimension lables.')
-        
-        res, end_values_param = self._parse_param(data, self.end_values, ndim, dimension_labels)
-
-        if res == False:
-            raise ValueError('Cannot parse provided end_values. Please, provide float, tuple or dictionary with dimension lables.')
+        end_values_param = self._parse_param(data, self.end_values, ndim, 'end_values')
 
         for dim in range(ndim):
 
@@ -170,7 +183,8 @@ class Padder(DataProcessor):
             # if AcquisitionData
             else:
                 if dimension_labels[dim] == 'channel':
-                    geometry.set_channels(num_channels=geometry_0.config.channels.num_channels+pad_width_param[dim][0]+pad_width_param[dim][1])
+                    geometry.set_channels(num_channels=geometry_0.config.channels.num_channels + \
+                        pad_width_param[dim][0] + pad_width_param[dim][1])
                 elif dimension_labels[dim] == 'angle':
                     # pad angles vector
                     initial_angle = geometry_0.config.angles.initial_angle
@@ -180,9 +194,11 @@ class Padder(DataProcessor):
                         if self.mode in ['reflect', 'symmetric', 'wrap', 'edge']:
                             angles = numpy.pad(angles_0, pad_width_param[dim], mode=self.mode)
                         elif self.mode == 'constant':
-                            angles = numpy.pad(angles_0, pad_width_param[dim], mode=self.mode, constant_values=constant_values_param[dim])
+                            angles = numpy.pad(angles_0, pad_width_param[dim], mode=self.mode,\
+                                constant_values=constant_values_param[dim])
                         elif self.mode == 'linear_ramp':
-                            angles = numpy.pad(angles_0, pad_width_param[dim], mode=self.mode, end_values=end_values_param[dim])
+                            angles = numpy.pad(angles_0, pad_width_param[dim], mode=self.mode, \
+                                end_values=end_values_param[dim])
                     else:
                         angles = angles_0.copy()
                     geometry.set_angles(angles, initial_angle=initial_angle, angle_unit=angle_unit)
@@ -193,27 +209,28 @@ class Padder(DataProcessor):
                     geometry.config.panel.num_pixels[0] += pad_width_param[dim][0]
                     geometry.config.panel.num_pixels[0] += pad_width_param[dim][1]
         
-        data_padded = geometry.allocate()
+        if out == None:
+            data_padded = geometry.allocate()
+        else:
+            data_padded = out
+        
         
         if self.mode in ['reflect', 'symmetric', 'wrap', 'edge']:
             data_padded.fill(numpy.pad(data.as_array(), pad_width_param, mode=self.mode))
         elif self.mode == 'constant':
-            data_padded.fill(numpy.pad(data.as_array(), pad_width_param, mode=self.mode, constant_values=constant_values_param))
+            data_padded.fill(numpy.pad(data.as_array(), pad_width_param, mode=self.mode, \
+                constant_values=constant_values_param))
         elif self.mode == 'linear_ramp':
-            data_padded.fill(numpy.pad(data.as_array(), pad_width_param, mode=self.mode, end_values=end_values_param))
+            data_padded.fill(numpy.pad(data.as_array(), pad_width_param, mode=self.mode, \
+                end_values=end_values_param))
         
         if out == None:
             return data_padded
-        else:
-            out = data_padded
 
 
-    def _parse_param(self, data, param, ndim, dimension_labels):
+    def _parse_param(self, data, param, ndim, descr):
 
-        res = True
-        pad_param = []
-
-        if not hasattr(param, "__len__"):
+        if isinstance(param, Number):
             pad_param = [(param, param)] * ndim
         elif isinstance(param, tuple) and len(param) == 2:
             pad_param = [param] * ndim
@@ -225,8 +242,12 @@ class Padder(DataProcessor):
                     pad_param[idx] = (param[key], param[key])
                 elif isinstance(param[key], tuple) and len(param[key]) == 2:
                     pad_param[idx] = param[key]
+                else:
+                    raise ValueError('Cannot parse provided {}. Expecting a tuple of length 2. Got {}'\
+                        .format(descr, param[key]))
         else:
-            res = False
+            raise ValueError('Cannot parse provided {}. Expecting int, tuple or dictionary with dimension lables. Got'\
+                .format(descr, type(param)))
 
-        return res, pad_param
+        return pad_param
 

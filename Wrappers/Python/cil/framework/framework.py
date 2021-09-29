8cc41966431a9788af5795308e7aabdbbce982ef
epapoutsellis <epapoutsellis@gmail.com>
2021-09-29 21:46:01 +0100

    remove print statement

--- Wrappers/Python/cil/framework/framework.py
+++ Wrappers/Python/cil/framework/framework.py
@@ -9,7 +9,7 @@
 
 #   http://www.apache.org/licenses/LICENSE-2.0
 
-#   Unless required by applicable law or agreed to in writing, software
+#   Unless required by applicable law or agreed to in writing, software 
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
@@ -2668,7 +2668,6 @@ class AcquisitionData(DataContainer):
             if force:
                 geometry_new = None
             else:
-                print(ve)
                 raise ValueError ("Unable to return slice of requested AcquisitionData. Use 'force=True' to return DataContainer instead.")
 
         #get new data

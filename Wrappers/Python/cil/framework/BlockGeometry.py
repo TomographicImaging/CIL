51906da1ea90b88da427a1dab89825eb1de22752
epapoutsellis <epapoutsellis@gmail.com>
2021-10-04 20:17:39 +0100

    Fix arguments in allocate method

--- Wrappers/Python/cil/framework/BlockGeometry.py
+++ Wrappers/Python/cil/framework/BlockGeometry.py
@@ -14,8 +14,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import numpy
-from numbers import Number
+
 import functools
 from cil.framework import BlockDataContainer
 

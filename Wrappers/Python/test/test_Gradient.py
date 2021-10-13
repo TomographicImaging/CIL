252a2a5e834a458f58d3383e215c131b2434e6a4
epapoutsellis <epapoutsellis@gmail.com>
2021-10-13 18:52:43 +0100

    minor change

--- Wrappers/Python/test/test_Gradient.py
+++ Wrappers/Python/test/test_Gradient.py
@@ -410,7 +410,7 @@ class TestGradientOperator(unittest.TestCase):
         numpy.testing.assert_array_almost_equal(res1[1].as_array(), res2[1].as_array())  
 
         # check dot_test
-        self.assertTrue(LinearOperator.dot_test(Grad))              
+        self.assertTrue(LinearOperator.dot_test(Grad)) 
 
 
 

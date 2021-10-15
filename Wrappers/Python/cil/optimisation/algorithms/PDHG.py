0a18eb02c29a5408d378a22e5c26e62d2c64ebfd
epapoutsellis <epapoutsellis@gmail.com>
2021-10-15 08:45:50 +0100

    raise warning for PDHG and use_axpby

--- Wrappers/Python/cil/optimisation/algorithms/PDHG.py
+++ Wrappers/Python/cil/optimisation/algorithms/PDHG.py
@@ -76,6 +76,9 @@ class PDHG(Algorithm):
                 raise ValueError('{} received both initial and the deprecated x_init parameter. It is not clear which one we should use.'\
                     .format(self.__class__.__name__))
         self._use_axpby = use_axpby
+        if use_axpby:
+            warnings.warn('use_axpby` cannot be use for complex data. Use `use_axpby=False')
+                             
 
         if f is not None and operator is not None and g is not None:
             self.set_up(f=f, g=g, operator=operator, tau=tau, sigma=sigma, initial=initial)

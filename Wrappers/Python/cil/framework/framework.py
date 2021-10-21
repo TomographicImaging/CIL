f4102f1e06e562aeaf058f4c909b71568c29088c
epapoutsellis <epapoutsellis@gmail.com>
2021-10-21 15:39:09 +0100

    remove newline

--- Wrappers/Python/cil/framework/framework.py
+++ Wrappers/Python/cil/framework/framework.py
@@ -3099,8 +3099,11 @@ class DataOrder():
     @staticmethod
     def check_order_for_engine(engine, geometry):
         order_requested = DataOrder.get_order_for_engine(engine, geometry)
+
         if order_requested == list(geometry.dimension_labels):
             return True
         else:
             raise ValueError("Expected dimension_label order {0}, got {1}.\nTry using `data.reorder('{2}')` to permute for {2}"
                  .format(order_requested, list(geometry.dimension_labels), engine))
+
+

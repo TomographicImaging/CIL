ae0bb5cc35d59b2dda0c825fd464fe04e358cc6c
epapoutsellis <epapoutsellis@gmail.com>
2021-10-22 09:25:16 +0100

    merge master

--- Wrappers/Python/test/test_PluginsRegularisation.py
+++ Wrappers/Python/test/test_PluginsRegularisation.py
@@ -15,12 +15,18 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import sys
 import unittest
 import numpy
+import numpy as np
+from cil.framework import DataContainer
+from cil.framework import ImageData
+from cil.framework import AcquisitionData
+from cil.framework import ImageGeometry
+from cil.framework import AcquisitionGeometry
 from cil.utilities import dataexample
-from cil.optimisation.functions import TotalVariation
-import os
-from utils import has_nvidia_smi
+from timeit import default_timer as timer
+
 
 try:
     from ccpi.filters import regularisers
@@ -28,18 +34,18 @@ try:
     from cil.plugins.ccpi_regularisation.functions import FGP_TV
     has_regularisation_toolkit = True
 except ImportError as ie:
+    # raise ImportError(ie + "\n\n" + 
+    #                   "This plugin requires the additional package ccpi-regularisation\n" +
+    #                   "Please install it via conda as ccpi-regularisation from the ccpi channel\n"+
+    #                   "Minimal version is 20.04")
     has_regularisation_toolkit = False
 print ("has_regularisation_toolkit", has_regularisation_toolkit)
 TNV_fixed = False
 
 class TestPlugin(unittest.TestCase):
     def setUp(self):
-
-        #Default test image
-        self.data = dataexample.SIMPLE_PHANTOM_2D.get(size=(64,64))
-        self.alpha = 2.0
-        self.iterations = 500        
-
+        # print ("test plugins")
+        pass
     def tearDown(self):
         pass
     @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
@@ -77,71 +83,218 @@ class TestPlugin(unittest.TestCase):
 
     @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
     def test_FGP_TV_complex(self):
-        datarr = self.data.as_array()
-        cmpx = numpy.zeros(self.data.shape, dtype=numpy.complex64)
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        cmpx = np.zeros(data.shape, dtype=np.complex)
         cmpx.real = datarr[:]
         cmpx.imag = datarr[:]
-        self.data.array = cmpx
+        data.array = cmpx
         reg = FGP_TV()
-        out = reg.proximal(self.data, 1)
+        out = reg.proximal(data, 1)
         outarr = out.as_array()
-        numpy.testing.assert_almost_equal(outarr.imag, outarr.real)
+        np.testing.assert_almost_equal(outarr.imag, outarr.real)
+
+    def rmul_test(self, f):
+
+        alpha = f.alpha
+        scalar = 2.123
+        af = scalar*f
+
+        assert (id(af) == id(f))
+        assert af.alpha == scalar * alpha
+    
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_FGP_TV_rmul(self):
+        from cil.plugins.ccpi_regularisation.functions import FGP_TV
+        f = FGP_TV()
+
+        self.rmul_test(f)
+    
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_FGP_TGV_rmul(self):
+        from cil.plugins.ccpi_regularisation.functions import FGP_TGV
+        f = FGP_TGV()
+
+        self.rmul_test(f)
 
     @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
-    def test_TotalVariation_vs_FGP_TV_cpu(self):
+    def test_FGP_TGV_rmul(self):
+        from cil.plugins.ccpi_regularisation.functions import TNV
+        f = TNV()
 
-        # Isotropic TV cil
-        TV_cil_iso = self.alpha * TotalVariation(max_iteration=self.iterations)
+        self.rmul_test(f)
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_FGP_dTV_rmul(self):
+        from cil.plugins.ccpi_regularisation.functions import FGP_dTV
+        data = dataexample.CAMERA.get(size=(256,256))
+        f = FGP_dTV(data)
 
-        # Anisotropic TV cil
-        TV_cil_aniso = self.alpha * TotalVariation(max_iteration=self.iterations, isotropic=False)
-
-        # Isotropic FGP_TV CCPiReg toolkit (cpu)
-        TV_regtoolkit_cpu_iso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'cpu')
-
-        # Anisotropic FGP_TV CCPiReg toolkit (cpu)
-        TV_regtoolkit_cpu_aniso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'cpu', isotropic=False)
-
-        res_TV_cil_iso = TV_cil_iso.proximal(self.data, tau=1.0)
-        res_TV_cil_aniso = TV_cil_aniso.proximal(self.data, tau=1.0)
-        res_TV_regtoolkit_cpu_iso = TV_regtoolkit_cpu_iso.proximal(self.data, tau=1.0)
-        res_TV_regtoolkit_cpu_aniso = TV_regtoolkit_cpu_aniso.proximal(self.data, tau=1.0)  
-
-        # compare TV vs FGP_TV (anisotropic, isotropic, cpu)
-        numpy.testing.assert_array_almost_equal(res_TV_cil_iso.array, res_TV_regtoolkit_cpu_iso.array, decimal=3)              
-        numpy.testing.assert_array_almost_equal(res_TV_cil_aniso.array, res_TV_regtoolkit_cpu_aniso.array, decimal=3)
-       
-    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")  
-    @unittest.skipUnless((has_nvidia_smi()), "Skipping as there is no GPU")        
-    def test_TotalVariation_vs_FGP_TV_gpu(self):   
-
-        # Isotropic TV cil
-        TV_cil_iso = self.alpha * TotalVariation(max_iteration=self.iterations)
-        res_TV_cil_iso = TV_cil_iso.proximal(self.data, tau=1.0)        
-
-        # Anisotropic TV cil
-        TV_cil_aniso = self.alpha * TotalVariation(max_iteration=self.iterations, isotropic=False) 
-        res_TV_cil_aniso = TV_cil_aniso.proximal(self.data, tau=1.0)               
+        self.rmul_test(f)
         
-        # Isotropic FGP_TV CCPiReg toolkit (gpu)
-        TV_regtoolkit_gpu_iso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'gpu') 
-        res_TV_regtoolkit_gpu_iso = TV_regtoolkit_gpu_iso.proximal(self.data, tau=1.0)
 
-        # Anisotropic FGP_TV CCPiReg toolkit (gpu)
-        TV_regtoolkit_gpu_aniso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'gpu', isotropic=False)  
-        res_TV_regtoolkit_gpu_aniso = TV_regtoolkit_gpu_aniso.proximal(self.data, tau=1.0)  
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_functionality_FGP_TV(self):
 
-        # Anisotropic FGP_TV CCPiReg toolkit (cpu)
-        TV_regtoolkit_cpu_aniso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'cpu', isotropic=False)         
-        res_TV_regtoolkit_cpu_aniso = TV_regtoolkit_cpu_aniso.proximal(self.data, tau=1.0)          
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        from cil.plugins.ccpi_regularisation.functions import FGP_TV
+        from ccpi.filters import regularisers
 
-        # Isotropic FGP_TV CCPiReg toolkit (cpu)
-        TV_regtoolkit_cpu_iso = self.alpha * FGP_TV(max_iteration=self.iterations, device = 'cpu')
-        res_TV_regtoolkit_cpu_iso = TV_regtoolkit_cpu_iso.proximal(self.data, tau=1.0)        
+        tau = 1.
+        fcil = FGP_TV()
+        outcil = fcil.proximal(data, tau=tau)
+        # use CIL defaults
+        outrgl, info = regularisers.FGP_TV(datarr, fcil.alpha*tau, fcil.max_iteration, fcil.tolerance, 0, 1, 'cpu' )
+        np.testing.assert_almost_equal(outrgl, outcil.as_array())
 
-        numpy.testing.assert_array_almost_equal(res_TV_cil_iso.array, res_TV_regtoolkit_gpu_iso.array, decimal=3)
-        numpy.testing.assert_array_almost_equal(res_TV_regtoolkit_cpu_iso.array, res_TV_regtoolkit_gpu_iso.array, decimal=3)
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_functionality_TGV(self):
 
-        numpy.testing.assert_array_almost_equal(res_TV_cil_aniso.array, res_TV_regtoolkit_gpu_aniso.array, decimal=3)
-        numpy.testing.assert_array_almost_equal(res_TV_regtoolkit_cpu_aniso.array, res_TV_regtoolkit_gpu_aniso.array, decimal=3)    
- 
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        from cil.plugins.ccpi_regularisation.functions import TGV
+        from ccpi.filters import regularisers
+
+        tau = 1.
+        fcil = TGV()
+        outcil = fcil.proximal(data, tau=tau)
+        # use CIL defaults
+        outrgl, info = regularisers.TGV(datarr, fcil.alpha*tau, 1,1, fcil.max_iteration, 12, fcil.tolerance, 'cpu' )
+
+        np.testing.assert_almost_equal(outrgl, outcil.as_array())
+
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_functionality_FGP_dTV(self):
+
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        ref = data*0.3
+        from cil.plugins.ccpi_regularisation.functions import FGP_dTV
+        from ccpi.filters import regularisers
+
+        tau = 1.
+        fcil = FGP_dTV(ref)
+        outcil = fcil.proximal(data, tau=tau)
+        # use CIL defaults
+        outrgl, info = regularisers.FGP_dTV(datarr, ref.as_array(), fcil.alpha*tau, fcil.max_iteration, fcil.tolerance, 0.01, 0, 1, 'cpu' )
+        np.testing.assert_almost_equal(outrgl, outcil.as_array())
+
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_functionality_TNV(self):
+
+        # fake a 2D+channel image
+        d = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
+        ig = ImageGeometry(160, 135, channels=91)
+        data = ig.allocate(None)
+        data.fill(d)
+        del d
+    
+        datarr = data.as_array()
+        from cil.plugins.ccpi_regularisation.functions import TNV
+        from ccpi.filters import regularisers
+
+        tau = 1.
+        
+        # CIL defaults
+        outrgl = regularisers.TNV(datarr, 1, 100, 1e-6 )
+        
+        fcil = TNV()
+        outcil = fcil.proximal(data, tau=tau)
+        np.testing.assert_almost_equal(outrgl, outcil.as_array())
+
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_TNV_raise_on_2D(self):
+
+        # data = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        from cil.plugins.ccpi_regularisation.functions import TNV
+        
+        tau = 1.
+        
+        fcil = TNV()
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_TNV_raise_on_3D_nochannel(self):
+
+        # data = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
+        data = dataexample.CAMERA.get(size=(256,256))
+        datarr = data.as_array()
+        from cil.plugins.ccpi_regularisation.functions import TNV
+        
+        tau = 1.
+        
+        fcil = TNV()
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_TNV_raise_on_4D(self):
+
+        from cil.plugins.ccpi_regularisation.functions import TNV
+        
+        data = ImageGeometry(3,4,5,channels=5).allocate(1)
+
+        tau = 1.
+        
+        fcil = TNV()
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True
+
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_FGP_TV_raise_on_4D_data(self):
+
+        from cil.plugins.ccpi_regularisation.functions import FGP_TV
+        
+        tau = 1.
+        fcil = FGP_TV()
+        data = ImageGeometry(3,4,5,channels=10).allocate(0)
+
+
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True
+
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_TGV_raise_on_4D_data(self):
+
+        from cil.plugins.ccpi_regularisation.functions import TGV
+        
+        tau = 1.
+        fcil = TGV()
+        data = ImageGeometry(3,4,5,channels=10).allocate(0)
+
+
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True
+    @unittest.skipUnless(has_regularisation_toolkit, "Skipping as CCPi Regularisation Toolkit is not installed")
+    def test_FGP_dTV_raise_on_4D_data(self):
+
+        from cil.plugins.ccpi_regularisation.functions import FGP_dTV
+        
+        tau = 1.
+        
+        data = ImageGeometry(3,4,5,channels=10).allocate(0)
+        ref = data * 2
+        
+        fcil = FGP_dTV(ref)
+
+        try:
+            outcil = fcil.proximal(data, tau=tau)
+            assert False
+        except ValueError:
+            assert True

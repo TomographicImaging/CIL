f376a3955b9b9994e019e8f4be2efe8924b06e49
epapoutsellis <epapoutsellis@gmail.com>
2021-10-25 08:43:13 +0100

    add Gradient test in test_SIRF

--- Wrappers/Python/test/test_SIRF.py
+++ Wrappers/Python/test/test_SIRF.py
@@ -34,7 +34,7 @@ except ImportError as ie:
 
 class GradientSIRF(object):
     
-    @unittest.skipUnless(has_sirf, "Skipping as SIRF is not availale")
+    @unittest.skipUnless(has_sirf, "Skipping as SIRF is not available")
     def test_Gradient(self):
 
         #######################################

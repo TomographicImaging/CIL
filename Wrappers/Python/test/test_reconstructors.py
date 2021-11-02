3a24e89cab773885b99c07e86ef32fe0e52f8fcf
gfardell <gemma.fardell@stfc.ac.uk>
2021-11-02 15:31:10 +0000

    Update unittests with simulated datasets

--- Wrappers/Python/test/test_reconstructors.py
+++ Wrappers/Python/test/test_reconstructors.py
@@ -16,7 +16,10 @@
 #   limitations under the License.
 
 from cil.framework import AcquisitionGeometry
-from cil.reconstructors import FBP, Reconstructor
+from cil.reconstructors import Reconstructor
+from cil.reconstructors import FBP
+from cil.utilities.dataexample import SIMULATED_CONE_BEAM_DATA, SIMULATED_SPHERE_VOLUME
+
 import unittest
 from scipy.fftpack  import fft, ifft
 import numpy as np
@@ -204,11 +207,11 @@ class Test_FBP(unittest.TestCase):
 
         fbp = FBP(ad)
         out1 = ad.copy()
-        fbp._FBP__pre_filtering(out1)
+        fbp.pre_filtering(out1)
 
         #by hand
         filter = fbp.get_filter_array()
-        weights = fbp._FBP__calculate_weights_fdk()
+        weights = fbp.calculate_weights()
         pad0 = (len(filter)-ag.pixel_num_h)//2
         pad1 = len(filter)-ag.pixel_num_h-pad0
 
@@ -232,7 +235,7 @@ class Test_FBP(unittest.TestCase):
         ad = ag.allocate(0)
 
         fbp = FBP(ad)
-        weights = fbp._FBP__calculate_weights_fdk()
+        weights = fbp.calculate_weights()
 
         scaling =  7.5 * np.pi
         weights_new = np.ones_like(weights)
@@ -254,120 +257,77 @@ class Test_FBP(unittest.TestCase):
 class Test_FBP_results(unittest.TestCase):
     
     def setUp(self):
-        #%% Setup Geometry
-        voxel_num_xy = 255
-        voxel_num_z = 15
 
-        mag = 2
-        src_to_obj = 50
-        src_to_det = src_to_obj * mag
+        self.acq_data = SIMULATED_CONE_BEAM_DATA.get()
+        self.img_data = SIMULATED_SPHERE_VOLUME.get()
 
-        pix_size = 0.2
-        det_pix_x = voxel_num_xy
-        det_pix_y = voxel_num_z
+        self.acq_data=np.log(self.acq_data)
+        self.acq_data*=-1.0
 
-        num_projections = 1000
-        angles = np.linspace(0, 360, num=num_projections, endpoint=False)
+        self.ig = self.img_data.geometry
+        self.ag = self.acq_data.geometry
 
-        self.ag = AcquisitionGeometry.create_Cone2D([0,-src_to_obj],[0,src_to_det-src_to_obj])\
-                                           .set_angles(angles)\
-                                           .set_panel(det_pix_x, pix_size)\
-                                           .set_labels(['angle','horizontal'])
-
-        self.ig = self.ag.get_ImageGeometry()
-
-        self.ag3D = AcquisitionGeometry.create_Cone3D([0,-src_to_obj,0],[0,src_to_det-src_to_obj,0])\
-                                     .set_angles(angles)\
-                                     .set_panel((det_pix_x,det_pix_y), (pix_size,pix_size))\
-                                     .set_labels(['angle','vertical','horizontal'])        
-        self.ig3D = self.ag3D.get_ImageGeometry()
-
-        self.ad3D = self.ag3D.allocate('random')
-        self.ig3D = self.ag3D.get_ImageGeometry()
-
-        kernel_size = self.ag3D.pixel_num_h
-        kernel_radius = (kernel_size - 1) // 2
-        y, x = np.ogrid[-kernel_radius:kernel_radius+1, -kernel_radius:kernel_radius+1]
-
-        circle1 = [5,0,0] #r,x,y
-        dist1 = ((x - circle1[1])**2 + (y - circle1[2])**2)**0.5
-
-        circle2 = [5,80,0] #r,x,y
-        dist2 = ((x - circle2[1])**2 + (y - circle2[2])**2)**0.5
-
-        circle3 = [25,0,80] #r,x,y
-        dist3 = ((x - circle3[1])**2 + (y - circle3[2])**2)**0.5
-
-        mask1 =(dist1 - circle1[0]).clip(0,1) 
-        mask2 =(dist2 - circle2[0]).clip(0,1) 
-        mask3 =(dist3 - circle3[0]).clip(0,1) 
-        phantom = 1 - np.logical_and(np.logical_and(mask1, mask2),mask3)
-
-        self.golden_data = self.ig3D.allocate(0)
-        for i in range(4):
-            self.golden_data.fill(array=phantom, vertical=7+i)
-        self.golden_data2D = phantom
-
-        Op = ProjectionOperator(self.ig3D, self.ag3D, direct_method='interpolated')
-        self.data = Op.direct(self.golden_data)
-        self.data2D = self.data.get_slice(vertical='centre')
 
     @unittest.skipUnless(has_tigre and has_ipp, "TIGRE or IPP not installed")
-    def test_results(self):
+    def test_results_3D(self):
 
-        fbp_tigre = FBP_tigre(self.ig3D, self.ag3D)
-        reco_tigre = fbp_tigre(self.data)
+        fbp = FBP(self.acq_data)
 
-        fbp_cil = FBP(self.data)
-        reco_cil = fbp_cil.run()
+        reco = fbp.run()
+        np.testing.assert_allclose(reco.as_array(), self.img_data.as_array(),atol=1e-3)    
 
-        #check close to tigre
-        diff = (reco_tigre-reco_cil).abs().max()
-        self.assertLess(diff, 0.006)
-    
-        #construct TIGRE's filter
-        n = 2**fbp_cil.fft_order
-        ramp = ramp_flat(n)
-        filt = filter('ram_lak',ramp[0],n,1,False)
-        fbp_cil.set_filter(filt)
-        reco_cil_filter = fbp_cil.run()
-        #check close to sim (aliasing makes the difference lareg at edges)
-        diff = (self.golden_data-reco_cil).abs().max()
-        self.assertLess(diff, 0.6)
+        reco2 = reco.copy()
+        reco2.fill(0)
+        fbp.run(out=reco2)
+        np.testing.assert_array_equal(reco.as_array(), reco2.as_array())  
 
-        #very small difference expected with the same filter
-        diff = (reco_cil_filter-reco_tigre).abs().max()
-        self.assertLess(diff, 5e-6)
 
     @unittest.skipUnless(has_tigre and has_ipp, "TIGRE or IPP not installed")
     def test_results_2D(self):
-        fbp_cil_2D = FBP(self.data2D)
-        reco_cil_2D = fbp_cil_2D.run()
-        diff = (reco_cil_2D - self.golden_data2D).abs().max()
-        self.assertLess(diff, 1)
+
+        data2D = self.acq_data.get_slice(vertical='centre')
+        img_data2D = self.img_data.get_slice(vertical='centre')
+
+        fbp = FBP(data2D)
+        reco = fbp.run()
+        np.testing.assert_allclose(reco.as_array(), img_data2D.as_array(),atol=1e-3)    
+
+        reco2 = reco.copy()
+        reco2.fill(0)
+        fbp.run(out=reco2)
+        np.testing.assert_array_equal(reco.as_array(), reco2.as_array()) 
+
 
     @unittest.skipUnless(has_tigre and has_ipp, "TIGRE or IPP not installed")
-    def test_results_inplace(self):
+    def test_results_with_tigre(self):
 
-        fbp_cil_2D = FBP(self.data2D)
-        reco_cil_2D = fbp_cil_2D.run()
+        fbp_tigre = FBP_tigre(self.ig, self.ag)
+        reco_tigre = fbp_tigre(self.acq_data)
+    
+        #fbp CIL with TIGRE's filter
+        fbp_cil = FBP(self.acq_data)
+        n = 2**fbp_cil.fft_order
+        ramp = ramp_flat(n)
+        filt = filter('ram_lak',ramp[0],n,1,False)
 
-        reco_cil_2D_b = reco_cil_2D.copy()
-        reco_cil_2D_b.fill(0)
-        fbp_cil_2D.run(out=reco_cil_2D_b)
-        diff = (reco_cil_2D - reco_cil_2D_b).abs().max()
-        self.assertLess(diff,1e-8)
+        fbp_cil = FBP(self.acq_data)
+        fbp_cil.set_filter(filt)
+        reco_cil = fbp_cil.run()
+
+        #with the same filter results should be virtually identical
+        np.testing.assert_array_equal(reco_cil.as_array(), reco_tigre.as_array(),atol=1e-8) 
+
 
     @unittest.skipUnless(has_tigre and has_ipp, "TIGRE or IPP not installed")
     def test_results_inplace_filtering(self):
 
-        fbp_cil_2D = FBP(self.data2D)
+        fbp_cil_2D = FBP(self.acq_data)
         reco_cil_2D = fbp_cil_2D.run()
 
-        data_filtered= self.data2D.copy()
+        data_filtered= self.acq_data.copy()
         fbp_cil_filter_inplace = FBP(data_filtered)
         fbp_cil_filter_inplace.set_filter_inplace(True)
         fbp_cil_filter_inplace.run(out=reco_cil_2D)
 
-        diff = (data_filtered - self.data2D).abs().mean()
-        self.assertGreater(diff,0.5)
+        diff = (data_filtered - self.acq_data).abs().mean()
+        self.assertGreater(diff,0.8)

8159507e52ba823dd9d9917b42012b5240915111
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-11-04 14:19:31 +0000

    Add test for importing version

---
+++ Wrappers/Python/test/test_version.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestModuleBase(unittest.TestCase):
+    def test_version(self):
+        try:
+            from cil import version
+            a = version.version
+            print ("version", a)
+            self.assertTrue(isinstance(a, str))
+        except ImportError as ie:
+            self.assertFalse(True, str(ie))

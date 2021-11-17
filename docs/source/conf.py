d25540f2e020235706603fb1a91662dea9ccc926
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-17 22:12:21 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import re
 
-sys.path.insert(0, os.path.abspath('../../Wrappers/Python/'))
+sys.path.insert(0, os.path.abspath('../Wrappers/Python/'))
 
 from cil import version
 

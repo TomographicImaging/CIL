09970514ae4a3ba4deaae4a2516390a1524a972d
Edoardo Pasca <edo.paskino@gmail.com>
2021-12-28 15:58:02 +0000

    created unittest

---
+++ Wrappers/Python/test/test_cupy.py
@@ -0,0 +1,46 @@
+from cil.framework import ImageGeometry, ImageData
+from cil.utilities import dataexample 
+import numpy as np 
+import cupy as cp
+from unittest import TestCase
+
+
+class TestCupyIntegration(TestCase):
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_creation_and_copy_ImageData(self):
+
+
+        ig = ImageGeometry(100,100)
+
+        camera = dataexample.CAMERA.get((100,100))
+        # cp.copyto(garr, camera.as_array())
+        # garr = cp.array(camera.as_array())
+
+
+        # datac = ImageData(geometry=camera.geometry, backend='numpy')
+        datag = ImageData(geometry=camera.geometry, backend='cupy')
+
+
+        # print ("python copying")
+        # for i in range(datag.shape[0]):
+        #     for j in range(datag.shape[1]):
+        #         datag.array[i,j] = camera.as_array()[i,j]
+        # print ("done")
+        datag.fill(camera)
+        # garr = datag.array
+        # print (type(garr))
+
+
+
+        # from cil.utilities.display import show2D
+        # # show2D([garr.get(), camera])
+        # show2D([datag.as_array().get(), camera])
+
+        np.testing.assert_almost_equal(datag.as_array().get(), camera.as_array())
+        
+

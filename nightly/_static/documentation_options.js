cce82be920e859961b8f2e08a92706f04e28b86b
GitHub Action <action@github.com>
2022-02-07 10:59:43 +0000

    Update documentation

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v21.3.1-3-g27660d64',
+    VERSION: 'v21.3.1-4-g0fbfe44c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

ee570693ac222bbcf459953b6b9f78c17839d812
Edoardo Pasca <edo.paskino@gmail.com>
2022-04-02 15:07:26 +0100

    working numba implementation

--- experiments/proximal.py
+++ experiments/proximal.py
@@ -1,4 +1,5 @@
 #%%
+from tokenize import Number
 from cil.optimisation.functions import MixedL21Norm
 from cil.optimisation.operators import GradientOperator
 from cil.framework import ImageGeometry
@@ -6,6 +7,8 @@ from cil.utilities import dataexample
 from cil.utilities.display import show2D
 from cil.io import NEXUSDataReader
 from cil.processors import Slicer
+
+from numbers import Number
 
 import time
 import os
@@ -24,17 +27,17 @@ except ImportError:
     
 
 @numba.jit(nopython=True)
-def some_calc(arr, abstau):
+def _proximal_step(arr, abstau):
     tmp = arr.ravel()
     for i in numba.prange(tmp.size):
         if tmp[i] == 0:
             continue
-        el = tmp[i] / abstau
-        el -= 1.
+        a = tmp[i] / abstau
+        el = a - 1
         if el <= 0.0:
             el = 0.
         
-        tmp[i] = el / tmp[i]
+        tmp[i] = el / a 
     return arr
     # tmp /= np.abs(tau)
     # # res = (tmp - 1).maximum(0.0) * x/tmp
@@ -60,13 +63,13 @@ def proximal_new(self, x, tau, out=None):
     # datacontainers of tau to be able to run
     
     tmp = x.pnorm(2)
-    if has_numba:
-        resarray = some_calc(np.asarray(tmp.as_array(), order='C', dtype=np.float32), np.abs(tau))
+    if has_numba and isinstance(tau, Number):
+        resarray = _proximal_step(np.asarray(tmp.as_array(), order='C', dtype=np.float32), 
+                             np.abs(tau, dtype=np.float32))
         res = tmp 
         res.fill(resarray)
     else:
         tmp /= np.abs(tau, dtype=np.float32)
-        # res = (tmp - 1).maximum(0.0) * x/tmp
         res = tmp - 1
         res.maximum(0.0, out=res)
         res /= tmp
@@ -139,11 +142,11 @@ if __name__ == '__main__':
     fname = os.path.abspath('C:/Users/ofn77899/Data/CTMeeting2022/small_normSPDHG_eTV_alpha_0.0003_it_1260.nxs')
     reader.set_up(file_name=fname)
     data = reader.read()
-    data = data.get_slice(vertical='centre')
+    # data = data.get_slice(vertical='centre')
 
-    roi = {'horizontal_x':(121, 164, 1),
-           'horizontal_y':(83,117,1)}
-    data = Slicer(roi=roi)(data)
+    # roi = {'horizontal_x':(121, 164, 1),
+    #        'horizontal_y':(83,117,1)}
+    # data = Slicer(roi=roi)(data)
 
     print (data.geometry)
 
@@ -156,20 +159,20 @@ if __name__ == '__main__':
 
     g = GradientOperator(data.geometry)
 
-    y = g.direct(data)
+    
 #%%
-    gamma = 1.
+    y = g.direct(data)
+    gamma = 2.
     dt1 = []
-
+    absgamma = np.abs(gamma, dtype=np.float32)
     ##############
     for i in tqdm(range(repeat)):
         t0 = time.time()
         # current_impl = f.proximal_conjugate(y, gamma)
         # current_impl = f.proximal(y, gamma)
         tmp = y.pnorm(2)
-    
-        res1 = some_calc(np.asarray(tmp.as_array(), order='C', dtype=np.float32), np.abs(gamma))
-        
+        tmp = np.asarray(tmp.as_array(), order='C', dtype=np.float32)
+        res1 = some_calc(tmp, absgamma)
         t1 = time.time()
         dt1.append(t1-t0)
     
@@ -182,7 +185,7 @@ if __name__ == '__main__':
         # new_impl = proximal_new(f, y, gamma)
         
         tmp = y.pnorm(2)
-        tmp /= np.abs(gamma, dtype=np.float32)
+        tmp /= absgamma
         # res = (tmp - 1).maximum(0.0) * x/tmp
         res = tmp - 1
         res.maximum(0.0, out=res)
@@ -199,7 +202,7 @@ if __name__ == '__main__':
     title = ['new', 'new numba']
 
     ratio = (res2/res1).as_array()
-    ratio[np.isnan(ratio)] = 0
+    ratio[np.isnan(ratio)] = 1
     show2D([res2, res1], 
             title=title, origin='upper-left', cmap='terrain', fix_range=False)
     show2D([res2-res1, ratio], title=['diff', 'ratio {} {}'.format((ratio).sum(),res2.as_array().size)], 
@@ -231,9 +234,9 @@ if __name__ == '__main__':
             title=title, origin='upper-left', cmap='terrain', fix_range=False)
     show2D([current_impl[0]- new_impl[0], current_impl[1] -new_impl[1] , data], 
             cmap='seismic', fix_range=(-15,15))
-
+#%%
     for i in range(len(current_impl)):
-        np.testing.assert_allclose(current_impl[i].as_array(), new_impl[i].as_array()/2, atol=1e-5, rtol=1e-6 )
+        np.testing.assert_allclose(current_impl[i].as_array(), new_impl[i].as_array(), atol=1e-5, rtol=1e-6 )
 
     current_impl *= 0
     new_impl *=0

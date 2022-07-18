537e80f084af116ffb43c4f73172ba14c7394c40
epapoutsellis <epapoutsellis@gmail.com>
2022-07-18 19:02:17 +0100

    remove KL numba tests, already in test_function_KL

--- Wrappers/Python/test/test_function_KullbackLeibler.py
+++ Wrappers/Python/test/test_function_KullbackLeibler.py
@@ -148,8 +148,8 @@ class TestKullbackLeiblerNumba(unittest.TestCase):
         # separate the u1 vertical=0
         f_mask = KullbackLeibler(b=g1.copy(), backend="numba", mask=mask.copy(), eta=eta.copy())
         f_mask_c = KullbackLeibler(b=g1.copy(), backend="numba", mask=mask_c.copy(), eta=eta.copy())
-        f_on_mask = KullbackLeibler(b=g1.subset(horizontal_x=0), backend="numba", eta=eta.subset(horizontal_x=0))
-        u1_on_mask = u1.subset(horizontal_x=0)
+        f_on_mask = KullbackLeibler(b=g1.get_slice(horizontal_x=0), backend="numba", eta=eta.get_slice(horizontal_x=0))
+        u1_on_mask = u1.get_slice(horizontal_x=0)
 
         tau = 400.4
         self.tau = tau

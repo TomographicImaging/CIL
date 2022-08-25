c0881d01ce88d389459c7dc55ad8e0d79d24f409
epapoutsellis <epapoutsellis@gmail.com>
2022-08-25 18:04:15 +0100

    Improve cvxpy tests

--- Wrappers/Python/test/test_SGDFunction.py
+++ Wrappers/Python/test/test_SGDFunction.py
@@ -2,65 +2,87 @@ import unittest
 from utils import initialise_tests
 from cil.optimisation.operators import MatrixOperator
 from cil.optimisation.functions import LeastSquares, SubsetSumFunction, SGDFunction
+from cil.optimisation.algorithms import GD
 from cil.framework import VectorData
 import numpy as np                  
                   
-
 initialise_tests()
+
+from utils import has_cvxpy
+
+if has_cvxpy:
+    import cvxpy
 
 class TestSGDFunction(unittest.TestCase):
                     
-    def setUp(self):   
 
+    def setUp(self):
+        
         np.random.seed(10)
         n = 50
         m = 500
-        self.n_subsets = 10
 
+        A = np.random.uniform(0,1, (m, n)).astype('float32')
+        b = (A.dot(np.random.randn(n)) + 0.1*np.random.randn(m)).astype('float32')
+
+        self.Aop = MatrixOperator(A)
+        self.bop = VectorData(b) 
+        
         Anp = np.random.uniform(0,1, (m, n)).astype('float32')
         xnp = np.random.uniform(0,1, (n,)).astype('float32')
         bnp = Anp.dot(xnp)
 
-        Ai = np.vsplit(Anp, self.n_subsets) 
-        bi = [bnp[i:i+n] for i in range(0, m, n)]     
+        # split data, operators, functions
+        self.n_subsets = 10
 
-        self.Aop = MatrixOperator(Anp)
-        self.bop = VectorData(bnp) 
-        self.ig = self.Aop.domain        
-        self.x_cil = self.ig.allocate('random')
+        Ai = np.vsplit(A, self.n_subsets) 
+        bi = [b[i:i+n] for i in range(0, m, n)]     
 
         self.fi_cil = []
         for i in range(self.n_subsets):   
-            Ai_cil = MatrixOperator(Ai[i])
-            bi_cil = VectorData(bi[i])
-            self.fi_cil.append(LeastSquares(Ai_cil, bi_cil, c=1.0))
+            self.Ai_cil = MatrixOperator(Ai[i])
+            self.bi_cil = VectorData(bi[i])
+            self.fi_cil.append(LeastSquares(self.Ai_cil, self.bi_cil, c = 0.5))
+            
+        self.F = LeastSquares(self.Aop, b=self.bop, c = 0.5/self.n_subsets) 
+        
+        self.ig = self.Aop.domain
+        self.precond = lambda i, x: self.ig.allocate(1.)
+        self.F_SGD = SGDFunction(self.fi_cil, replacement = True, precond = self.precond)           
 
-        self.f = (1/self.n_subsets) * LeastSquares(self.Aop, b=self.bop, c=1.0)
-        self.f_SGD = SGDFunction(self.fi_cil, sampling="sequential")
-
-        self.precond = lambda i, x: 3./self.ig.allocate(2.5)
-        self.f_SGD_precond = SGDFunction(self.fi_cil, sampling="sequential", precond=self.precond)
+        self.initial = self.ig.allocate()  
 
     def test_gradient(self):
 
-        out1 = self.x_cil.geometry.allocate()
-        out2 = self.x_cil.geometry.allocate()
+        out1 = self.ig.allocate()
+        out2 = self.ig.allocate()
+
+        x = self.ig.allocate('random')
 
         # No preconditioning
-        self.f_SGD.gradient(self.x_cil, out=out1)
+        self.F_SGD.gradient(x, out=out1)
 
-        self.f_SGD[self.f_SGD.subset_num].gradient(self.x_cil, out=out2)
+        self.F_SGD[self.F_SGD.subset_num].gradient(x, out=out2)
         np.testing.assert_allclose(out1.array, out2.array, atol=1e-3) 
 
-        out3 = self.x_cil.geometry.allocate()
-        out4 = self.x_cil.geometry.allocate()
+    @unittest.skipUnless(has_cvxpy, "CVXpy not installed") 
+    def test_with_cvxpy(self):
+        
+        u_cvxpy = cvxpy.Variable(self.ig.shape[0])
+        objective = cvxpy.Minimize( 0.5/self.n_subsets * cvxpy.sum_squares(self.Aop.A @ u_cvxpy - self.bop.array))
+        p = cvxpy.Problem(objective)
+        p.solve(verbose=True, solver=cvxpy.SCS, eps=1e-4) 
 
-        # With preconditioning
-        self.f_SGD_precond.gradient(self.x_cil, out=out3)
+        step_size = 1./self.F.L # Theoretical step size = 1./(16*self.F_SAG.L) 
+        epochs = 100
+        sgd = GD(initial = self.initial, objective_function = self.F_SGD, step_size = step_size,
+                    max_iteration = epochs * self.n_subsets, 
+                    update_objective_interval = epochs * self.n_subsets)
+        sgd.run(verbose=0)    
 
-        self.f_SGD_precond[self.f_SGD.subset_num].gradient(self.x_cil, out=out4)
-        out4*=self.precond(self.f_SGD_precond.subset_num, 3./self.ig.allocate(2.5))
-        np.testing.assert_allclose(out3.array, out4.array, atol=1e-3)         
+        np.testing.assert_allclose(p.value, sgd.objective[-1], atol=1e-1)
+
+        np.testing.assert_allclose(u_cvxpy.value, sgd.solution.array, atol=1e-1)             
 
 
 

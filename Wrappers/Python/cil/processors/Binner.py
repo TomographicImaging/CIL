586b9785f252fab23ed247d4e44b486ec3ed091d
Gemma Fardell <47746591+gfardell@users.noreply.github.com>
2023-01-27 13:30:53 +0000

    Update Wrappers/Python/cil/processors/Binner.py
    Co-authored-by: Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
    Signed-off-by: Gemma Fardell <47746591+gfardell@users.noreply.github.com>

--- Wrappers/Python/cil/processors/Binner.py
+++ Wrappers/Python/cil/processors/Binner.py
@@ -115,7 +115,7 @@ class Binner(DataProcessor):
 
         Parameters
         ----------
-        input : DataContainer, Geometry
+        dataset : DataContainer, Geometry
             The input DataContainer
         """
 

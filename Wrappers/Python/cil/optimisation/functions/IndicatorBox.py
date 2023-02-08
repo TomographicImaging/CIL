1c90064eb46459a1461b44d629373003155f2468
Edoardo Pasca <edo.paskino@gmail.com>
2023-02-08 21:06:47 +0000

    modify header according to contribution's guide

--- Wrappers/Python/cil/optimisation/functions/IndicatorBox.py
+++ Wrappers/Python/cil/optimisation/functions/IndicatorBox.py
@@ -1,6 +1,11 @@
 # Copyright 2022 United Kingdom Research and Innovation
 # Copyright 2022 The University of Manchester
 
+# Author(s): 
+#   Evangelos Papoutsellis (UKRI)
+#   Edoardo Pasca (UKRI)
+#   Gemma Fardell (UKRI)
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,11 +17,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Author(s): 
-# Evangelos Papoutsellis (UKRI)
-# Edoardo Pasca (UKRI)
-# Gemma Fardell (UKRI)
 
 from cil.optimisation.functions import Function
 import numpy as np

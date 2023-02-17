44334cac6202eaa4017a9dcedbaa901420c09b0e
Edoardo Pasca <edo.paskino@gmail.com>
2023-02-17 15:32:05 +0000

    Update Wrappers/Python/cil/framework/BlockDataContainer.py
    Co-authored-by: Gemma Fardell <47746591+gfardell@users.noreply.github.com>
    Signed-off-by: Edoardo Pasca <edo.paskino@gmail.com>

--- Wrappers/Python/cil/framework/BlockDataContainer.py
+++ Wrappers/Python/cil/framework/BlockDataContainer.py
@@ -33,7 +33,7 @@ class BlockDataContainer(object):
        indipendently on the fact that the `BlockDataContainer` could be nested
     3) algebra between `BlockDataContainer` and one `DataContainer` is possible.
        It will require that all the `DataContainers` in the block to be
-       compatible with the `DataContainer` we want to algebra with. 
+       compatible with the `DataContainer` we want to operate with. 
     4) algebra between `BlockDataContainer` and a `Number` is possible and it
        will be done with each element of the `BlockDataContainer` even if nested
 

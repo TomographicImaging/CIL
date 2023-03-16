841f4a700a1f957381fc3ff1c40dc01c787fa9cb
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2023-03-16 10:06:13 +0000

    Licensing headers

--- Wrappers/Python/test/test_functions.py
+++ Wrappers/Python/test/test_functions.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-#  Copyright 2023 United Kingdom Research and Innovation
-#  Copyright 2023 The University of Manchester
-#
-# Author(s):
-#   Evangelos Papoutsellis (UKRI)
-#   Edoardo Pasca (UKRI)
-#   Gemma Fardell (UKRI)
+#  Copyright 2019 United Kingdom Research and Innovation
+#  Copyright 2019 The University of Manchester
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,6 +13,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#
+# Authors:
+# CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
 
 import unittest
 
--- src/Core/FBP_filtering.cpp
+++ src/Core/FBP_filtering.cpp
@@ -1,3 +1,22 @@
+// -*- coding: utf-8 -*-
+//  Copyright 2021 United Kingdom Research and Innovation
+//  Copyright 2021 The University of Manchester
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+// Authors:
+// CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
+
 #include "FBP_filtering.h"
 
 int filter_projections_avh(float * data, const float * filter, const float* weights, int order, long num_proj, long pix_y, long pix_x)

34140736210871a22bdce085de04ec5f3ce4352d
Margaret Duff <margaret.duff@stfc.ac.uk>
2023-09-14 09:29:06 +0000

    Changes requested by Edo

--- Wrappers/Python/cil/optimisation/functions/TotalVariation.py
+++ Wrappers/Python/cil/optimisation/functions/TotalVariation.py
@@ -27,9 +27,8 @@ import warnings
 import logging
 
 
-
 class TotalVariation(Function):
-    
+
     r""" Total variation Function
 
     .. math:: \mathrm{TV}(u) := \|\nabla u\|_{2,1} = \sum \|\nabla u\|_{2},\, (\mbox{isotropic})
@@ -48,7 +47,7 @@ class TotalVariation(Function):
     algorithm to solve:
 
     .. math:: \mathrm{prox}_{\tau \mathrm{TV}}(b) := \underset{u}{\mathrm{argmin}} \frac{1}{2\tau}\|u - b\|^{2} + \mathrm{TV}(u)
-    
+
     The algorithm used for the proximal operator of TV is the Fast Gradient Projection algorithm (or FISTA)
     applied to the _dual problem_ of the above problem, see :cite:`BeckTeboulle_b`, :cite:`BeckTeboulle_a`, :cite:`Zhu2010`.
 
@@ -60,12 +59,13 @@ class TotalVariation(Function):
 
     max_iteration : :obj:`int`, default = 5
         Maximum number of iterations for the FGP algorithm to solve to solve the dual problem 
-        of the Total Variation Denoising problem (ROF). If warmstart=False, this should be around 100,
+        of the Total Variation Denoising problem (ROF). If warm_start=False, this should be around 100,
         or larger, with a set tolerance. 
     tolerance : :obj:`float`, default = None
         Stopping criterion for the FGP algorithm used to to solve the dual problem 
-        of the Total Variation Denoising problem (ROF)
-        
+        of the Total Variation Denoising problem (ROF). If the difference between iterates in the FGP algorithm is less than the tolerance
+        the iterations end before the max_iteration number. 
+
         .. math:: \|x^{k+1} - x^{k}\|_{2} < \mathrm{tolerance}
 
     correlation : :obj:`str`, default = `Space`
@@ -98,17 +98,17 @@ class TotalVariation(Function):
 
         .. math:: \underset{u}{\mathrm{argmin}} \frac{1}{2\frac{\tau}{1+\gamma\tau}}\|u - \frac{b}{1+\gamma\tau}\|^{2} + \mathrm{TV}(u) 
 
-    warmstart : :obj:`boolean`, default = True
+    warm_start : :obj:`boolean`, default = True
         If set to true, the FGP algorithm used to solve the dual problem of the Total Variation Denoising problem (ROF) is initiated by the final value from the previous iteration and not at zero. 
         This allows the max_iteration value to be reduced to 5-10 iterations. 
-        
+
 
     Note
     ----
 
-    With warmstart set to the default, True, the TV function will keep in memory the range of the gradient of the image to be denoised, i.e. N times the dimensionality of the image. This increases the memory requirements. 
+    With warm_start set to the default, True, the TV function will keep in memory the range of the gradient of the image to be denoised, i.e. N times the dimensionality of the image. This increases the memory requirements. 
     However, during the evaluation of `proximal` the memory requirements will be unchanged as the same amount of memory will need to be allocated and deallocated. 
-    
+
     Note
     ----
 
@@ -153,52 +153,50 @@ class TotalVariation(Function):
     >>> TV = alpha * TotalVariation(isotropic=False, strong_convexity_constant=gamma)
     >>> sol = TV.proximal(b, tau = 1.0)    
 
-    """   
-
+    """
 
     def __init__(self,
-                 max_iteration=10, 
-                 tolerance = None, 
-                 correlation = "Space",
-                 backend = "c",
-                 lower = None,
-                 upper = None,
-                 isotropic = True,
-                 split = False,
-                 info = False, 
-                 strong_convexity_constant = 0,
-                 warmstart=True):
+                 max_iteration=10,
+                 tolerance=None,
+                 correlation="Space",
+                 backend="c",
+                 lower=None,
+                 upper=None,
+                 isotropic=True,
+                 split=False,
+                 info=False,
+                 strong_convexity_constant=0,
+                 warm_start=True):
 
-        
-        super(TotalVariation, self).__init__(L = None)
+        super(TotalVariation, self).__init__(L=None)
 
         # Regularising parameter = alpha
         self.regularisation_parameter = 1.
-        
+
         self.iterations = max_iteration
-        
+
         self.tolerance = tolerance
-        
+
         # Total variation correlation (isotropic=Default)
         self.isotropic = isotropic
-        
+
         # correlation space or spacechannels
         self.correlation = correlation
-        self.backend = backend        
-        
+        self.backend = backend
+
         # Define orthogonal projection onto the convex set C
         if lower is None:
             lower = -np.inf
         if upper is None:
-            upper = np.inf            
+            upper = np.inf
         self.lower = lower
         self.upper = upper
         self.projection_C = IndicatorBox(lower, upper).proximal
-             
-        # Setup GradientOperator as None. This is to avoid domain argument in the __init__     
+
+        # Setup GradientOperator as None. This is to avoid domain argument in the __init__
         self._gradient = None
         self._domain = None
-        
+
         self.info = info
         if self.info:
             warnings.warn(" `info` is deprecate. Please use logging instead.")
@@ -206,8 +204,8 @@ class TotalVariation(Function):
         # splitting Gradient
         self.split = split
 
-        #For the warmstart functionality
-        self.warmstart = warmstart
+        # For the warm_start functionality
+        self.warm_start = warm_start
         self._p2 = None
 
         # Strong convexity for TV
@@ -219,11 +217,10 @@ class TotalVariation(Function):
         else:
             self.func = MixedL11Norm()
 
-     
     def _get_p2(self):
-        r"""The initial value for the dual in the proximal calculation - allocated to zero in the case of warmstart=False
-          or initialised as the last iterate seen in the proximal calculation in the case warmstart=True ."""
-          
+        r"""The initial value for the dual in the proximal calculation - allocated to zero in the case of warm_start=False
+          or initialised as the last iterate seen in the proximal calculation in the case warm_start=True ."""
+
         if self._p2 is None:
             return self.gradient.range_geometry().allocate(0)
         else:
@@ -236,11 +233,11 @@ class TotalVariation(Function):
     @regularisation_parameter.setter
     def regularisation_parameter(self, value):
         if not isinstance(value, Number):
-            raise TypeError("regularisation_parameter: expected a number, got {}".format(type(value)))
+            raise TypeError(
+                "regularisation_parameter: expected a number, got {}".format(type(value)))
         self._regularisation_parameter = value
 
     def __call__(self, x):
-        
         r""" Returns the value of the TotalVariation function at :code:`x` ."""
 
         try:
@@ -253,45 +250,42 @@ class TotalVariation(Function):
         if self._L is None:
             self.calculate_Lipschitz()
 
-        if self.strong_convexity_constant>0:
-            strongly_convex_term = (self.strong_convexity_constant/2)*x.squared_norm()
+        if self.strong_convexity_constant > 0:
+            strongly_convex_term = (
+                self.strong_convexity_constant/2)*x.squared_norm()
         else:
             strongly_convex_term = 0
 
         return self.regularisation_parameter * self.func(self.gradient.direct(x)) + strongly_convex_term
 
-                            
-    def proximal(self, x, tau, out = None):
+    def proximal(self, x, tau, out=None):
+        r""" Returns the proximal operator of the TotalVariation function at :code:`x` ."""
 
-        r""" Returns the proximal operator of the TotalVariation function at :code:`x` ."""        
-
-        if self.strong_convexity_constant>0:
+        if self.strong_convexity_constant > 0:
 
             strongly_convex_factor = (1 + tau * self.strong_convexity_constant)
             x /= strongly_convex_factor
             tau /= strongly_convex_factor
-        
+
         if out is None:
             solution = self._fista_on_dual_rof(x, tau)
         else:
-            self._fista_on_dual_rof(x, tau, out = out)
+            self._fista_on_dual_rof(x, tau, out=out)
 
-        if self.strong_convexity_constant>0:
+        if self.strong_convexity_constant > 0:
             x *= strongly_convex_factor
             tau *= strongly_convex_factor
 
         if out is None:
-            return solution 
-    
+            return solution
 
-    def _fista_on_dual_rof(self, x, tau, out = None):
-        
+    def _fista_on_dual_rof(self, x, tau, out=None):
         r""" Runs the Fast Gradient Projection (FGP) algorithm to solve the dual problem 
         of the Total Variation Denoising problem (ROF).
 
         .. math: \max_{\|y\|_{\infty}<=1.} \frac{1}{2}\|\nabla^{*} y + x \|^{2} - \frac{1}{2}\|x\|^{2}
 
-        """        
+        """
         try:
             self._domain = x.geometry
         except:
@@ -300,17 +294,15 @@ class TotalVariation(Function):
         # Compute Lipschitz constant provided that domain is not None.
         # Lipschitz constant depends on the GradientOperator, which is configured only if domain is not None
         if self._L is None:
-            self.calculate_Lipschitz()            
-        
-        # initialise
-        t = 1        # line 2 
+            self.calculate_Lipschitz()
 
-        # dual variable - its content is overwritten during iterations 
+        # initialise
+        t = 1
+
+        # dual variable - its content is overwritten during iterations
         p1 = self.gradient.range_geometry().allocate(None)
         p2 = self._get_p2()
-        # temp_q should be equal to p2 
-        tmp_q = p2.copy()  
-
+        tmp_q = p2.copy()
 
         # multiply tau by -1 * regularisation_parameter here so it's not recomputed every iteration
         # when tau is an array this is done inplace so reverted at the end
@@ -323,48 +315,48 @@ class TotalVariation(Function):
         should_return = False
         if out is None:
             should_return = True
-            out = self.gradient.domain_geometry().allocate(0)   
+            out = self.gradient.domain_geometry().allocate(0)
 
         for k in range(self.iterations):
-                                                                                   
-            t0 = t
-            self.gradient.adjoint(tmp_q, out = out)
-            out.sapyb(tau_reg_neg, x, 1.0, out=out)
-            self.projection_C(out, tau=None, out = out)
 
+            t0 = t
+            self.gradient.adjoint(tmp_q, out=out)
+            out.sapyb(tau_reg_neg, x, 1.0, out=out)
+            self.projection_C(out, tau=None, out=out)
 
             self.gradient.direct(out, out=p1)
 
             multip = (-self.L)/tau_reg_neg
 
             tmp_q.sapyb(1., p1, multip, out=tmp_q)
-            
-            if self.tolerance is not None and k%5==0: # testing convergence criterion 
+
+            if self.tolerance is not None and k % 5 == 0:
                 p1 *= multip
                 error = p1.norm()
                 error /= tmp_q.norm()
-                if error < self.tolerance:                           
+                if error < self.tolerance:
                     break
 
             self.func.proximal_conjugate(tmp_q, 1.0, out=p1)
-                       
-            t = (1 + np.sqrt(1 + 4 * t0 ** 2)) / 2 
-            p1.subtract(p2, out=tmp_q) 
-            tmp_q *= (t0-1)/t 
+
+            t = (1 + np.sqrt(1 + 4 * t0 ** 2)) / 2
+            p1.subtract(p2, out=tmp_q)
+            tmp_q *= (t0-1)/t
             tmp_q += p1
 
-            #switch p1 and p2 references
+            # switch p1 and p2 references
             tmp = p1
             p1 = p2
             p2 = tmp
-        if self.warmstart:
+        if self.warm_start:
             self._p2 = p2
 
         if self.info:
             if self.tolerance is not None:
-                logging.info("Stop at {} iterations with tolerance {} .".format(k, error))
+                logging.info(
+                    "Stop at {} iterations with tolerance {} .".format(k, error))
             else:
-                logging.info("Stop at {} iterations.".format(k))                
+                logging.info("Stop at {} iterations.".format(k))
 
         # return tau to its original state if it was modified
         if id(tau_reg_neg) == id(tau):
@@ -373,18 +365,17 @@ class TotalVariation(Function):
         if should_return:
             return out
 
+    def convex_conjugate(self, x):
+        r""" Returns the value of convex conjugate of the TotalVariation function at :code:`x` ."""
+        return 0.0
 
-    def convex_conjugate(self,x):   
-        r""" Returns the value of convex conjugate of the TotalVariation function at :code:`x` ."""             
-        return 0.0    
-    
     def calculate_Lipschitz(self):
         r""" Default value for the Lipschitz constant."""
-        
+
         # Compute the Lipschitz parameter from the operator if possible
         # Leave it initialised to None otherwise
-        self._L = (1./self.gradient.norm())**2  
-    
+        self._L = (1./self.gradient.norm())**2
+
     @property
     def gradient(self):
         r""" GradientOperator is created if it is not instantiated yet. The domain of the `_gradient`,
@@ -392,14 +383,17 @@ class TotalVariation(Function):
 
         """
         if self._domain is not None:
-            self._gradient = GradientOperator(self._domain, correlation = self.correlation, backend = self.backend)
+            self._gradient = GradientOperator(
+                self._domain, correlation=self.correlation, backend=self.backend)
         else:
-            raise ValueError(" The domain of the TotalVariation is {}. Please use the __call__ or proximal methods first before calling gradient.".format(self._domain))            
+            raise ValueError(
+                " The domain of the TotalVariation is {}. Please use the __call__ or proximal methods first before calling gradient.".format(self._domain))
 
         return self._gradient
 
     def __rmul__(self, scalar):
-        if not isinstance (scalar, Number):
-            raise TypeError("scalar: Expected a number, got {}".format(type(scalar)))
+        if not isinstance(scalar, Number):
+            raise TypeError(
+                "scalar: Expected a number, got {}".format(type(scalar)))
         self.regularisation_parameter *= scalar
         return self

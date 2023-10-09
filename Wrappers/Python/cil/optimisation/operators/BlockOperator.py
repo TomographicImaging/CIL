1ca3a2b599a218cddc4c85b332000a56f154349d
Margaret Duff <margaret.duff@stfc.ac.uk>
2023-10-09 08:52:49 +0000

    Comments from Edo fixed

--- Wrappers/Python/cil/optimisation/operators/BlockOperator.py
+++ Wrappers/Python/cil/optimisation/operators/BlockOperator.py
@@ -29,7 +29,8 @@ try:
     has_sirf = True
 except ImportError as ie:
     has_sirf = False
-       
+
+
 class BlockOperator(Operator):
     r'''A Block matrix containing Operators
 
@@ -37,21 +38,22 @@ class BlockOperator(Operator):
     following form:
 
     .. math::
-    
+
       \min Regulariser + Fidelity
 
-    
+
     BlockOperators have a generic shape M x N, and when applied on an 
     Nx1 BlockDataContainer, will yield and Mx1 BlockDataContainer.
     Notice: BlockDatacontainer are only allowed to have the shape of N x 1, with
     N rows and 1 column.
-    
+
     User may specify the shape of the block, by default is a row vector
 
     Operators in a Block are required to have the same domain column-wise and the
     same range row-wise.
     '''
     __array_priority__ = 1
+
     def __init__(self, *args, **kwargs):
         '''
         Class creator
@@ -64,7 +66,7 @@ class BlockOperator(Operator):
             :param: shape (:obj:`tuple`, optional): If shape is passed the Operators in 
                   vararg are considered input in a row-by-row fashion. 
                   Shape and number of Operators must match.
-                  
+
         Example:
             BlockOperator(op0,op1) results in a row block
             BlockOperator(op0,op1,shape=(1,2)) results in a column block
@@ -72,95 +74,91 @@ class BlockOperator(Operator):
         self.operators = args
         shape = kwargs.get('shape', None)
         if shape is None:
-            shape = (len(args),1)
+            shape = (len(args), 1)
         self.shape = shape
-        n_elements = functools.reduce(lambda x,y: x*y, shape, 1)
+        n_elements = functools.reduce(lambda x, y: x*y, shape, 1)
         if len(args) != n_elements:
             raise ValueError(
-                    'Dimension and size do not match: expected {} got {}'
-                    .format(n_elements,len(args)))
+                'Dimension and size do not match: expected {} got {}'
+                .format(n_elements, len(args)))
         # TODO
         # until a decent way to check equality of Acquisition/Image geometries
-        # required to fullfil "Operators in a Block are required to have the same 
+        # required to fullfil "Operators in a Block are required to have the same
         # domain column-wise and the same range row-wise."
-        # let us just not check if column/row-wise compatible, which is actually 
+        # let us just not check if column/row-wise compatible, which is actually
         # the same achieved by the column_wise_compatible and row_wise_compatible methods.
-        
+
         # # test if operators are compatible
         # if not self.column_wise_compatible():
         #     raise ValueError('Operators in each column must have the same domain')
         # if not self.row_wise_compatible():
         #     raise ValueError('Operators in each row must have the same range')
-    
+
     def column_wise_compatible(self):
         '''Operators in a Block should have the same domain per column'''
         rows, cols = self.shape
         compatible = True
         for col in range(cols):
             column_compatible = True
-            for row in range(1,rows):
-                dg0 = self.get_item(row-1,col).domain_geometry()
-                dg1 = self.get_item(row,col).domain_geometry()
-                if hasattr(dg0,'handle') and hasattr(dg1,'handle'):
+            for row in range(1, rows):
+                dg0 = self.get_item(row-1, col).domain_geometry()
+                dg1 = self.get_item(row, col).domain_geometry()
+                if hasattr(dg0, 'handle') and hasattr(dg1, 'handle'):
                     column_compatible = True and column_compatible
                 else:
                     column_compatible = dg0.__dict__ == dg1.__dict__ and column_compatible
             compatible = compatible and column_compatible
         return compatible
-    
+
     def row_wise_compatible(self):
         '''Operators in a Block should have the same range per row'''
         rows, cols = self.shape
         compatible = True
         for row in range(rows):
             row_compatible = True
-            for col in range(1,cols):
-                dg0 = self.get_item(row,col-1).range_geometry()
-                dg1 = self.get_item(row,col).range_geometry()
-                if hasattr(dg0,'handle') and hasattr(dg1,'handle'):
+            for col in range(1, cols):
+                dg0 = self.get_item(row, col-1).range_geometry()
+                dg1 = self.get_item(row, col).range_geometry()
+                if hasattr(dg0, 'handle') and hasattr(dg1, 'handle'):
                     row_compatible = True and column_compatible
                 else:
                     row_compatible = dg0.__dict__ == dg1.__dict__ and row_compatible
-                
+
             compatible = compatible and row_compatible
-            
+
         return compatible
 
     def get_item(self, row, col):
         '''returns the Operator at specified row and col'''
         if row > self.shape[0]:
-            raise ValueError('Requested row {} > max {}'.format(row, self.shape[0]))
+            raise ValueError(
+                'Requested row {} > max {}'.format(row, self.shape[0]))
         if col > self.shape[1]:
-            raise ValueError('Requested col {} > max {}'.format(col, self.shape[1]))
-        
+            raise ValueError(
+                'Requested col {} > max {}'.format(col, self.shape[1]))
+
         index = row*self.shape[1]+col
         return self.operators[index]
-    
+
     def norm(self):
         '''Returns the square root of the sum of the norms of the individual operators in the BlockOperators 
         '''
-        return numpy.sqrt(numpy.sum(numpy.array(self.norms())**2))    
-    
-    def norms(self, ):
+        return numpy.sqrt(numpy.sum(numpy.array(self.get_norms())**2))
+
+    def get_norms(self, ):
         '''Returns a list of the individual norms of the Operators in the BlockOperator
         '''
-        norms= []
-        for op in self.operators:
-            norms.append(op.norm())
-        return norms
-    
+        return [op.norm() for op in self.operators]
+
     def set_norms(self, norms):
         '''Uses the set_norm() function in Operator to set the norms of the operators in the BlockOperator from a list of custom values. 
         '''
-        if len(norms)==len(self):
-            pass
-        else:
-            raise ValueError("The length of the list of norms should be equal to the number of operators in the BlockOperator")
-        
-        for j,value in enumerate(norms):
-            self.operators[j].set_norm(value)
-        
+        if len(norms) != len(self):
+            raise ValueError(
+                "The length of the list of norms should be equal to the number of operators in the BlockOperator")
 
+        for j, value in enumerate(norms):
+            self.operators[j].set_norm(value)
 
     def direct(self, x, out=None):
         '''Direct operation for the BlockOperator
@@ -168,41 +166,43 @@ class BlockOperator(Operator):
         BlockOperator work on BlockDataContainer, but they will work on DataContainers
         and inherited classes by simple wrapping the input in a BlockDataContainer of shape (1,1)
         '''
-        
-        if not isinstance (x, BlockDataContainer):
+
+        if not isinstance(x, BlockDataContainer):
             x_b = BlockDataContainer(x)
         else:
             x_b = x
         shape = self.get_output_shape(x_b.shape)
         res = []
-        
+
         if out is None:
-        
+
             for row in range(self.shape[0]):
                 for col in range(self.shape[1]):
                     if col == 0:
-                        prod = self.get_item(row,col).direct(x_b.get_item(col))
+                        prod = self.get_item(row, col).direct(
+                            x_b.get_item(col))
                     else:
-                        prod += self.get_item(row,col).direct(x_b.get_item(col))
+                        prod += self.get_item(row,
+                                              col).direct(x_b.get_item(col))
                 res.append(prod)
             return BlockDataContainer(*res, shape=shape)
-                
+
         else:
-            
+
             tmp = self.range_geometry().allocate()
             for row in range(self.shape[0]):
                 for col in range(self.shape[1]):
-                    if col == 0:       
-                        self.get_item(row,col).direct(
-                                                      x_b.get_item(col),
-                                                      out=out.get_item(row))                        
+                    if col == 0:
+                        self.get_item(row, col).direct(
+                            x_b.get_item(col),
+                            out=out.get_item(row))
                     else:
                         a = out.get_item(row)
-                        self.get_item(row,col).direct(
-                                                      x_b.get_item(col), 
-                                                      out=tmp.get_item(row))
+                        self.get_item(row, col).direct(
+                            x_b.get_item(col),
+                            out=tmp.get_item(row))
                         a += tmp.get_item(row)
-                
+
     def adjoint(self, x, out=None):
         '''Adjoint operation for the BlockOperator
 
@@ -217,7 +217,7 @@ class BlockOperator(Operator):
         '''
         if not self.is_linear():
             raise ValueError('Not all operators in Block are linear.')
-        if not isinstance (x, BlockDataContainer):
+        if not isinstance(x, BlockDataContainer):
             x_b = BlockDataContainer(x)
         else:
             x_b = x
@@ -227,11 +227,13 @@ class BlockOperator(Operator):
             for col in range(self.shape[1]):
                 for row in range(self.shape[0]):
                     if row == 0:
-                        prod = self.get_item(row, col).adjoint(x_b.get_item(row))
+                        prod = self.get_item(row, col).adjoint(
+                            x_b.get_item(row))
                     else:
-                        prod += self.get_item(row, col).adjoint(x_b.get_item(row))
+                        prod += self.get_item(row,
+                                              col).adjoint(x_b.get_item(row))
                 res.append(prod)
-            if self.shape[1]==1:
+            if self.shape[1] == 1:
                 # the output is a single DataContainer, so we can take it out
                 return res[0]
             else:
@@ -242,74 +244,80 @@ class BlockOperator(Operator):
                 for row in range(self.shape[0]):
                     if row == 0:
                         if issubclass(out.__class__, DataContainer) or \
-                ( has_sirf and issubclass(out.__class__, SIRFDataContainer) ):
+                                (has_sirf and issubclass(out.__class__, SIRFDataContainer)):
                             self.get_item(row, col).adjoint(
-                                                x_b.get_item(row),
-                                                out=out)
+                                x_b.get_item(row),
+                                out=out)
                         else:
-                            op = self.get_item(row,col)
+                            op = self.get_item(row, col)
                             self.get_item(row, col).adjoint(
-                                                x_b.get_item(row),
-                                                out=out.get_item(col))
+                                x_b.get_item(row),
+                                out=out.get_item(col))
                     else:
                         if issubclass(out.__class__, DataContainer) or \
-                ( has_sirf and issubclass(out.__class__, SIRFDataContainer) ):
-                            out += self.get_item(row,col).adjoint(
-                                                        x_b.get_item(row))
+                                (has_sirf and issubclass(out.__class__, SIRFDataContainer)):
+                            out += self.get_item(row, col).adjoint(
+                                x_b.get_item(row))
                         else:
                             a = out.get_item(col)
-                            a += self.get_item(row,col).adjoint(
-                                                        x_b.get_item(row),
-                                                        )
+                            a += self.get_item(row, col).adjoint(
+                                x_b.get_item(row),
+                            )
+
     def is_linear(self):
         '''returns whether all the elements of the BlockOperator are linear'''
         return functools.reduce(lambda x, y: x and y.is_linear(), self.operators, True)
 
     def get_output_shape(self, xshape, adjoint=False):
         '''returns the shape of the output BlockDataContainer
-        
+
         A(N,M) direct u(M,1) -> N,1
         A(N,M)^T adjoint u(N,1) -> M,1
         '''
-        rows , cols = self.shape
+        rows, cols = self.shape
         xrows, xcols = xshape
         if xcols != 1:
-            raise ValueError('BlockDataContainer cannot have more than 1 column')
+            raise ValueError(
+                'BlockDataContainer cannot have more than 1 column')
         if adjoint:
             if rows != xrows:
-                raise ValueError('Incompatible shapes {} {}'.format(self.shape, xshape))
-            return (cols,xcols)
+                raise ValueError(
+                    'Incompatible shapes {} {}'.format(self.shape, xshape))
+            return (cols, xcols)
         if cols != xrows:
-            raise ValueError('Incompatible shapes {} {}'.format((rows,cols), xshape))
-        return (rows,xcols)
-        
+            raise ValueError(
+                'Incompatible shapes {} {}'.format((rows, cols), xshape))
+        return (rows, xcols)
+
     def __rmul__(self, scalar):
         '''Defines the left multiplication with a scalar
 
         :paramer scalar: (number or iterable containing numbers):
 
         Returns: a block operator with Scaled Operators inside'''
-        if isinstance (scalar, list) or isinstance(scalar, tuple) or \
+        if isinstance(scalar, list) or isinstance(scalar, tuple) or \
                 isinstance(scalar, numpy.ndarray):
             if len(scalar) != len(self.operators):
-                raise ValueError('dimensions of scalars and operators do not match')
+                raise ValueError(
+                    'dimensions of scalars and operators do not match')
             scalars = scalar
         else:
             scalars = [scalar for _ in self.operators]
         # create a list of ScaledOperator-s
-        ops = [ v * op for v,op in zip(scalars, self.operators)]
-        #return BlockScaledOperator(self, scalars ,shape=self.shape)
+        ops = [v * op for v, op in zip(scalars, self.operators)]
+        # return BlockScaledOperator(self, scalars ,shape=self.shape)
         return type(self)(*ops, shape=self.shape)
+
     @property
     def T(self):
         '''Return the transposed of self
-        
+
         input in a row-by-row'''
         newshape = (self.shape[1], self.shape[0])
         oplist = []
         for col in range(newshape[1]):
             for row in range(newshape[0]):
-                oplist.append(self.get_item(col,row))
+                oplist.append(self.get_item(col, row))
         return type(self)(*oplist, shape=newshape)
 
     def domain_geometry(self):
@@ -320,51 +328,50 @@ class BlockOperator(Operator):
         '''
         if self.shape[1] == 1:
             # column BlockOperator
-            return self.get_item(0,0).domain_geometry()
+            return self.get_item(0, 0).domain_geometry()
         else:
             # get the geometries column wise
             # we need only the geometries from the first row
             # since it is compatible from __init__
             tmp = []
             for i in range(self.shape[1]):
-                tmp.append(self.get_item(0,i).domain_geometry())
-            return BlockGeometry(*tmp)                
-                                    
-            #shape = (self.shape[0], 1)
-            #return BlockGeometry(*[el.domain_geometry() for el in self.operators],
+                tmp.append(self.get_item(0, i).domain_geometry())
+            return BlockGeometry(*tmp)
+
+            # shape = (self.shape[0], 1)
+            # return BlockGeometry(*[el.domain_geometry() for el in self.operators],
             #        shape=self.shape)
 
     def range_geometry(self):
         '''returns the range of the BlockOperator'''
-        
+
         tmp = []
         for i in range(self.shape[0]):
-            tmp.append(self.get_item(i,0).range_geometry())
-        return BlockGeometry(*tmp)            
-        
-        
-        #shape = (self.shape[1], 1)
-        #return BlockGeometry(*[el.range_geometry() for el in self.operators],
+            tmp.append(self.get_item(i, 0).range_geometry())
+        return BlockGeometry(*tmp)
+
+        # shape = (self.shape[1], 1)
+        # return BlockGeometry(*[el.range_geometry() for el in self.operators],
         #            shape=shape)
-        
+
     def sum_abs_row(self):
-        
+
         res = []
         for row in range(self.shape[0]):
-            for col in range(self.shape[1]):                            
+            for col in range(self.shape[1]):
                 if col == 0:
-                    prod = self.get_item(row,col).sum_abs_row()
+                    prod = self.get_item(row, col).sum_abs_row()
                 else:
-                    prod += self.get_item(row,col).sum_abs_row()
+                    prod += self.get_item(row, col).sum_abs_row()
             res.append(prod)
-            
-        if self.shape[1]==1:
+
+        if self.shape[1] == 1:
             tmp = sum(res)
             return ImageData(tmp)
         else:
-            
+
             return BlockDataContainer(*res)
-        
+
     def sum_abs_col(self):
 
         res = []
@@ -379,9 +386,9 @@ class BlockOperator(Operator):
         return BlockDataContainer(*res)
 
     def __len__(self):
-        
-        return len(self.operators)    
-    
+
+        return len(self.operators)
+
     def __getitem__(self, index):
         '''returns the index-th operator in the block irrespectively of it's shape'''
         return self.operators[index]
@@ -389,6 +396,3 @@ class BlockOperator(Operator):
     def get_as_list(self):
         '''returns the list of operators'''
         return self.operators
-        
-        
-        

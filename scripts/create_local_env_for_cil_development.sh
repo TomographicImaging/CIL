9bd01bd579442fcd0d8dd2523ee31684d67eb68e
Casper da Costa-Luis <casper.dcl@physics.org>
2023-11-21 18:50:58 +0000

    tidy dev scripts

--- scripts/create_local_env_for_cil_development.sh
+++ scripts/create_local_env_for_cil_development.sh
@@ -1,47 +1,87 @@
-#  -*- coding: utf-8 -*-
+#!/usr/bin/env bash
+set -euxo pipefail
+numpy='1.24'
+python='3.10'
+name=cil
+test_deps=0
+cil_ver=''
+while getopts hn:p:e:tv: option ; do
+  case "${option}" in
+  n) numpy="${OPTARG}" ;;
+  p) python="${OPTARG}" ;;
+  e) name="${OPTARG}" ;;
+  t) test_deps=1 ;;
+  v) test_deps=1 ; cil_ver="${OPTARG}" ;;
+  h)
+    echo "Usage: $0 [-n numpy_version] [-p python_version] [-e environment_name] [-t] [-v cil_version]"
+    echo 'Where `-t` installs test dependencies, and `-v cil_version` implies `-t`'
+    exit
+    ;;
+  *)
+    echo "Wrong option passed. Use the -h option to get some help." >&2
+    exit 1
+    ;;
+  esac
+done
+
+echo "Numpy $numpy"
+echo "Python $python"
+echo "Environment name $name"
+
+conda_args=(create --name="$name"
+  python="$python"
+  numpy="$numpy"
+  cmake'>=3.16'
+  dxchange
+  h5py
+  ipp
+  ipp-devel
+  ipp-include
+  libgcc-ng
+  matplotlib
+  numba
+  olefile
+  packaging
+  pillow
+  python-wget
+  pywavelets
+  scikit-image
+  scipy
+  tqdm
+)
+if test -n "$cil_ver"; then
+  echo "CIL version $cil_ver"
+  conda_args+=(cil="${cil_ver}")
+fi
+
+if test $test_deps = 0; then
+  conda_args+=(-c conda-forge -c intel -c defaults --override-channels)
+else
+  conda_args+=(
+    astra-toolbox'>=1.9.9.dev5,<2.1'
+    ccpi-regulariser=22.0.0
+    cil-data
+    cvxpy
+    ipywidgets
+    setuptools
+    tigre=2.4
+    tomophantom=2.0.0
+    -c conda-forge
+    -c intel
+    -c ccpi/label/dev
+    -c ccpi
+    -c astra-toolbox
+    -c astra-toolbox/label/dev
+    --override-channels
+  )
+fi
+
+conda "${conda_args[@]}"
+
+# Authors: CIL Developers (https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt)
 # Copyright 2020 United Kingdom Research and Innovation
 # Copyright 2020 The University of Manchester
-    
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-    
 #     http://www.apache.org/licenses/LICENSE-2.0
-    
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-    
-# Authors:
-# CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
-#! /bin/bash
-
-while getopts hn:p:e: option
- do
- case "${option}"
- in
-  n) numpy=${OPTARG};;
-  p) python=${OPTARG};;
-  e) name=${OPTARG};;
-  h)
-   echo "Usage: $0 [-n numpy_version] [-p python version] [-e environment name]"
-   exit 
-   ;;
-  *)
-   echo "Wrong option passed. Use the -h option to get some help." >&2
-   exit 1
-  ;;
- esac
-done
-
-echo Numpy $numpy
-echo Python $python
-echo Environment name $name
-
-set -x
-
-conda create --name $name cmake python=$python numpy=$numpy scipy matplotlib \
-  h5py pillow libgcc-ng dxchange olefile pywavelets python-wget scikit-image \
-  packaging  numba ipp ipp-devel ipp-include tqdm -c conda-forge -c intel  -c defaults --override-channels

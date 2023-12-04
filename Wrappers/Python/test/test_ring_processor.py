b947f2ed4f7f17c9d235fc5f1baaf357ddb75995
Hannah Robarts <hannah.robarts@stfc.ac.uk>
2023-12-04 09:03:38 +0000

    Add test for odd size array in ring remover

--- Wrappers/Python/test/test_ring_processor.py
+++ Wrappers/Python/test/test_ring_processor.py
@@ -18,12 +18,14 @@
 # CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
 
 import unittest
-from cil.processors import RingRemover
+from cil.processors import RingRemover, TransmissionAbsorptionConverter, Slicer
 from cil.framework import ImageData, ImageGeometry, AcquisitionGeometry
+from cil.utilities import dataexample
 
 import os
 import numpy as np
 from utils import has_tomophantom, initialise_tests
+from testclass import CCPiTestClass
 
 initialise_tests()
 
@@ -75,3 +77,51 @@ class TestL1NormRR(unittest.TestCase):
         error = (ring_recon - sin).abs().as_array().mean()
         np.testing.assert_almost_equal(error, 83.20592, 4)
         
+class TestRingRemover(CCPiTestClass):
+    def add_bad_pixels(self, data, number_of_columns, number_of_hot_pix, seed):
+
+        data_corrupted = data.copy()
+
+        # get intensity range
+        low = np.amin(data.as_array())
+        high = np.amax(data.as_array())
+
+        # we seed random number generator for repeatability
+        rng = np.random.RandomState(seed=seed) 
+        # indices of bad columns
+        columns = rng.randint(0, data.shape[1], size=number_of_columns)
+        # indices of hot pixels
+        pix_row = rng.randint(0, data.shape[0], size=number_of_hot_pix)
+        pix_col = rng.randint(0, data.shape[1], size=number_of_hot_pix)
+        # values in hot pixels
+        pixel_values = rng.uniform(low=low, high=high, size=number_of_hot_pix)
+
+        for i in range(number_of_columns):
+            col_pattern = rng.uniform(low=low, high=high, size=data.shape[0])
+            data_corrupted.as_array()[:, columns[i]] = data.as_array()[:, columns[i]]+col_pattern
+
+        for i in range(number_of_hot_pix):
+            data_corrupted.as_array()[pix_row[i], pix_col[i]] = pixel_values[i]
+
+        return data_corrupted
+    
+    def test_ring_remover(self):
+        
+        data = dataexample.SIMULATED_PARALLEL_BEAM_DATA.get().get_slice(vertical=80)
+
+        data_corrupted = self.add_bad_pixels(data, 1, 1, 1312)
+        data_corrupted = TransmissionAbsorptionConverter()(data_corrupted)
+        r = RingRemover(8,'db20', 1.5)
+        r.set_input(data_corrupted)
+        data_corrected = r.get_output()
+
+        self.assertEqual(data_corrupted.shape, data_corrected.shape)
+
+        data_corrupted_odd = Slicer(roi={'horizontal': (1, None)})(data_corrupted)
+        data_corrupted_odd = Slicer(roi={'angle': (1,None)})(data_corrupted_odd)
+        r = RingRemover(8,'db20', 1.5)
+        r.set_input(data_corrupted_odd)
+        data_corrected_odd = r.get_output()
+
+        self.assertEqual(data_corrupted_odd.shape, data_corrected_odd.shape)
+

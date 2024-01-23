743875b6d62625bfa8db60cfc0030f3cfbbb506d
hrobarts <hannah.robarts@stfc.ac.uk>
2024-01-23 11:44:46 +0000

    Add test for 30 and 31 angles

--- Wrappers/Python/test/test_AcquisitionGeometry.py
+++ Wrappers/Python/test/test_AcquisitionGeometry.py
@@ -1630,7 +1630,29 @@ class TestSubset(unittest.TestCase):
         angles = re.findall('Angles [\d]+-[\d]+ in degrees:\s+\[.*\]+', capturedOutput.getvalue(), re.MULTILINE)
         self.assertEqual(angles[0], 'Angles 0-9 in degrees: [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]')
         self.assertEqual(angles[1], 'Angles 80-89 in degrees: [80., 81., 82., 83., 84., 85., 86., 87., 88., 89.]')
+
+        # test output when angles=31
+        AG = AcquisitionGeometry.create_Parallel2D(detector_position=[0,10])\
+            .set_panel(num_pixels=10)\
+            .set_angles(angles=range(31))\
+            .set_labels(['horizontal', 'angle'])
+        AD = AcquisitionData(np.zeros([10,31]), geometry=AG, deep_copy=False)
+        print(AD.geometry)
+        angles = re.findall('Angles [\d]+-[\d]+ in degrees:\s+\[.*\]+', capturedOutput.getvalue(), re.MULTILINE)
+        self.assertEqual(angles[2], 'Angles 0-9 in degrees: [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]')
+        self.assertEqual(angles[3], 'Angles 21-30 in degrees: [21., 22., 23., 24., 25., 26., 27., 28., 29., 30.]')
         
+        # test output when angles=30
+        AG = AcquisitionGeometry.create_Parallel2D(detector_position=[0,10])\
+            .set_panel(num_pixels=10)\
+            .set_angles(angles=range(30))\
+            .set_labels(['horizontal', 'angle'])
+        AD = AcquisitionData(np.zeros([10,30]), geometry=AG, deep_copy=False)
+        print(AD.geometry)
+        angles = re.findall('Number of positions: 30\n\tAngles [\d]+-[\d]+ in degrees:\s+\[.*\n.*\]+', capturedOutput.getvalue(), re.MULTILINE)
+        self.assertEqual(angles[0],\
+                'Number of positions: 30\n\tAngles 0-29 in degrees: [ 0.,  1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9., 10., 11., 12., 13., 14.,\n 15., 16., 17., 18., 19., 20., 21., 22., 23., 24., 25., 26., 27., 28., 29.]')
+ 
         # test no error occurs when angles<20
         AG = AcquisitionGeometry.create_Parallel2D(detector_position=[0,10])\
             .set_panel(num_pixels=10)\
@@ -1656,5 +1678,5 @@ class TestSubset(unittest.TestCase):
         print(AD.geometry)
 
         # return to standard print output
-        sys.stdout = sys.__stdout__      
-        
+        sys.stdout = sys.__stdout__ 
+

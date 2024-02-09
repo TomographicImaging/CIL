069398774168ff0deb4801d8200ada014850d852
Joshua DM Hellier <joshua.hellier-2@manchester.ac.uk>
2024-02-09 11:23:12 +0000

    Make AcquisitionGeometry inherit from a blank base class.

--- Wrappers/Python/cil/framework/framework.py
+++ Wrappers/Python/cil/framework/framework.py
@@ -2052,7 +2052,13 @@ class Configuration(object):
         return False
 
 
-class AcquisitionGeometry(object):
+class BaseAcquisitionGeometry:
+    """This class only exists to give get_order_for_engine something to typecheck for. At some point separate interface
+    stuff into here or refactor get_order_for_engine to not need it."""
+    pass
+
+
+class AcquisitionGeometry(BaseAcquisitionGeometry):
     """This class holds the AcquisitionGeometry of the system.
     
     Please initialise the AcquisitionGeometry using the using the static methods:

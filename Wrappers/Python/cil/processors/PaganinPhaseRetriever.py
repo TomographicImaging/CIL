c8dd7e2ba968f0adb8f99a286adb0d7f8c9d0e3d
hrobarts <hannah.robarts@stfc.ac.uk>
2024-02-29 13:24:50 +0000

    Add generalised paganin method

--- Wrappers/Python/cil/processors/PaganinPhaseRetriever.py
+++ Wrappers/Python/cil/processors/PaganinPhaseRetriever.py
@@ -19,7 +19,7 @@
 
 from cil.framework import DataProcessor
 
-from .PhaseRetriever_Paganin import PaganinPhaseRetrieval
+from .PhaseRetriever_Paganin import PaganinPhaseRetrieval, GeneralisedPhaseRetrieval, OriginalPhaseRetrieval
 from cil.framework import Processor
 
 
@@ -29,7 +29,7 @@ class PaganinPhaseRetriever(Processor):
     """
 
     @staticmethod
-    def paganin(energy_eV = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, normalise = False, units_output='absorption'):
+    def paganin(energy_eV = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, propagation_distance = None):
         """Method to create a phase retrieval processor using the Paganin phase retrieval algorithm
         described in https://doi.org/10.1046/j.1365-2818.2002.01010.x 
         
@@ -40,22 +40,19 @@ class PaganinPhaseRetriever(Processor):
             Energy of the incident photon in eV
             
         delta: float
-            Real part of the material refractive index
+            Real part of the deviation of the material refractive index from 1, where refractive index n = (1 - delta) + i beta 
+            energy-dependent refractive index information can be found at https://refractiveindex.info/
         
         beta: float
-            Complex part of the material refractive index
+            Complex part of the material refractive index, where refractive index n = (1 - delta) + i beta
+            energy-dependent refractive index information can be found at https://refractiveindex.info/ 
 
         unit_multiplier: float
             Multiplier to convert units stored in geometry to metres, conversion applies to pixel size and propagation distance
 
-        normalise: boolean
-            Flag to indicate whether the data should be normalised before applying the filter
-
-        units_output: string
-            if 'absorption' (default), returns the projected absorption of the sample corrected for phase effects absorption = µT 
-            if 'thickness', returns the projected thickness T of the sample projected onto the image plane 
-            if 'phase', returns the phase of the beam at the material exit ϕ(r⊥) = −δ T(r⊥) · 2π/λ
-
+        propagation_distance: float (optional)
+            The sample to detector distance in meters. If not specified, the value in data.geometry.dist_center_detector will be used
+            
         Returns
         -------
         Processor
@@ -66,7 +63,20 @@ class PaganinPhaseRetriever(Processor):
         -------
         >>> processor = PhaseRetriever.paganin(energy_eV, delta, beta, unit_multiplier)
         >>> processor.set_input(self.data)
-
+        >>> processor.get_output()
+        
+        Or calling only the Paganin filter step
+        >>> processor = PhaseRetriever.paganin(energy_eV, delta, beta, unit_multiplier)
+        >>> processor.set_input(self.data)
+        >>> processor.filter_image()
         """
-        processor = PaganinPhaseRetrieval(energy_eV, delta, beta, unit_multiplier, normalise, units_output)
+        processor = PaganinPhaseRetrieval(energy_eV, delta, beta, unit_multiplier, propagation_distance)
         return processor
+    
+    @staticmethod
+    def generalised_paganin(energy_eV = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, propagation_distance = None):
+        return GeneralisedPhaseRetrieval(energy_eV, delta, beta, unit_multiplier, propagation_distance)
+    
+    @staticmethod
+    def original_paganin(energy_eV = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, propagation_distance = None):
+        return OriginalPhaseRetrieval(energy_eV, delta, beta, unit_multiplier, propagation_distance)
--- Wrappers/Python/cil/processors/PhaseRetriever_Paganin.py
+++ Wrappers/Python/cil/processors/PhaseRetriever_Paganin.py
@@ -4,17 +4,11 @@ import numpy as np
 from scipy.fft import fft2
 from scipy.fft import ifft2
 from scipy.fft import fftshift
-from scipy.fft import ifftshift
-
 from scipy import constants
 
 from tqdm import tqdm
-import weakref
 
-from multiprocessing import Pool, TimeoutError
-from functools import partial
-from cil.utilities.display import show2D
-
+import logging
 import dask
 from dask import delayed
 
@@ -22,10 +16,10 @@ class PaganinPhaseRetrieval(Processor):
 
     """docstring for Paganin"""
 
-    def __init__(self, energy = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, normalise = False, units_output=False):
+    def __init__(self, energy = 40000, delta = 1, beta = 1e-3, unit_multiplier = 1, propagation_distance=None):
 
         """
-        Method to create a phase retrieval processor using the Paganin phase retrieval algorithm
+        Method to retrieve phase information from  using the Paganin phase retrieval algorithm
         described in https://doi.org/10.1046/j.1365-2818.2002.01010.x 
     
 
@@ -35,26 +29,23 @@ class PaganinPhaseRetrieval(Processor):
             Energy of the incident photon in eV
             
         delta: float
-            Real part of the material refractive index
+            Real part of the deviation of the material refractive index from 1, where refractive index n = (1 - delta) + i beta 
+            energy-dependent refractive index information can be found at https://refractiveindex.info/
         
         beta: float
-            Complex part of the material refractive index
+            Complex part of the material refractive index, where refractive index n = (1 - delta) + i beta
+            energy-dependent refractive index information can be found at https://refractiveindex.info/ 
 
         unit_multiplier: float
             Multiplier to convert units stored in geometry to metres, conversion applies to pixel size and propagation distance
 
-        normalise: boolean
-            Flag to indicate whether the data should be normalised before applying the filter
-
-        units_output: string
-            if 'absorption' (default), returns the projected absorption of the sample corrected for phase effects absorption = µT 
-            if 'thickness', returns the projected thickness T of the sample projected onto the image plane 
-            if 'phase', returns the phase of the beam at the material exit ϕ(r⊥) = −δ T(r⊥) · 2π/λ
+        propagation_distance: float (optional)
+            The sample to detector distance in meters. If not specified, the value in data.geometry.dist_center_detector will be used
 
         Returns
         -------
         DataContainer
-            returns the absorption corrected data, thickness of phase (dependent on units_output) 
+            returns the data corrected for the effects of phase difference in the material. 
         
         """
 
@@ -64,12 +55,12 @@ class PaganinPhaseRetrieval(Processor):
                     'delta': delta,
                     'beta': beta,
                     'unit_multiplier' : unit_multiplier,
-                    'normalise' : normalise,
-                    'units_output' : units_output,
+                    'propagation_distance_user' : propagation_distance,
+                    'output_type' : None,
                     'mu' : None,
                     'alpha' : None,
                     'pixel_size' : None,
-                    'effective_distance' : None,
+                    'propagation_distance' : None,
                     'magnification' : None,
                     'filter' : None
                  }
@@ -85,50 +76,62 @@ class PaganinPhaseRetrieval(Processor):
         self.mu = 4.0*np.pi*self.beta/self.wavelength   
 
     def __calculate_alpha(self):
-        self.alpha = 4.0*(np.pi**2)*self.effective_distance*self.delta/self.mu
-
-    def set_input(self, dataset):
-        """
-        Set the input data to the processor
-
-        Parameters
-        ----------
-        input : DataContainer
-            The input DataContainer
-        """
-
-        if issubclass(type(dataset), DataContainer):
-            if self.check_input(dataset):
-                self.__dict__['input'] = weakref.ref(dataset)
-                self.__dict__['shouldRun'] = True
-            else:
-                raise ValueError('Input data not compatible')
-        else:
-            raise TypeError("Input type mismatch: got {0} expecting {1}"\
-                            .format(type(dataset), DataContainer))
-        
-       
-        self.pixel_size = (dataset.geometry.pixel_size_h / dataset.geometry.magnification)*self.unit_multiplier 
-
-        self.magnification = dataset.geometry.magnification
-
-        if (dataset.geometry.dist_source_center is not None) and (dataset.geometry.dist_center_detector is not None):
-            effective_distance = dataset.geometry.dist_center_detector / self.magnification
-            self.effective_distance = effective_distance*self.unit_multiplier
-        else:
-            self.effective_distance = 1
-        self.__calculate_alpha()
+        self.alpha = (self.propagation_distance)*self.delta/self.mu
         
     def check_input(self, data):
         geometry = data.geometry
+
+        if geometry.magnification == None:
+            self.magnification = 1
+        else:
+            self.magnification = geometry.magnification
+
         if (geometry.pixel_size_h - geometry.pixel_size_v ) / \
             (geometry.pixel_size_h + geometry.pixel_size_v ) < 1e-5:
-            pass
+            self.pixel_size = data.geometry.pixel_size_h*self.unit_multiplier
         else:
             raise ValueError('Panel pixel size is not homogeneous up to 1e-5: got {} {}'\
                     .format( geometry.pixel_size_h, geometry.pixel_size_v )
                 )
+
+        if self.propagation_distance_user is None: 
+            if data.geometry.dist_center_detector is None:
+                raise ValueError('Propagation distance not found, please provide propagation_distance as an argument to PaganinPhaseRetriever or update geometry.dist_center_detector')
+            elif data.geometry.dist_center_detector == 0:
+                raise ValueError('Found geometry.dist_center_detector = 0, phase retrieval is not compatible with virtual magnification\
+                                 please provide a real propagation_distance as an argument to PaganinPhaseRetriever or update geometry.dist_center_detector')
+            else:
+                propagation_distance = data.geometry.dist_center_detector
+                self.propagation_distance = (propagation_distance)*self.unit_multiplier
+        else:
+            self.propagation_distance = self.propagation_distance_user
+
+        self.__calculate_alpha()
+
         return True
+
+    def get_output(self, out=None, output_type = 'attenuation'):
+        '''
+        Runs the configured processor and returns the processed data
+
+        Parameters
+        ----------
+        out : DataContainer, optional
+           Fills the referenced DataContainer with the processed data and suppresses the return
+        
+        output_type: string, optional
+            if 'attenuation' (default), returns the projected attenuation of the sample corrected for phase effects, attenuation = µT 
+            if 'thickness', returns the projected thickness T of the sample projected onto the image plane 
+            if 'phase', returns the phase of the beam at the material exit, phase ϕ(r⊥) = −δ T(r⊥) · 2π/λ
+        
+        Returns
+        -------
+        DataContainer
+            The processed data. Suppressed if `out` is passed
+
+        '''
+        self.output_type = output_type
+        return super().get_output(out)
     
     def energy_to_wavelength(self, energy_eV):
         """Converts photon energy in eV to wavelength in m
@@ -145,51 +148,6 @@ class PaganinPhaseRetrieval(Processor):
 
         """
         return (constants.h*constants.speed_of_light)/(energy_eV*constants.electron_volt)
-
-    def create_filter(self, image):
-        
-        Nx, Ny = image.shape
-        dfx = 1./self.pixel_size/Nx
-        dfy = 1./self.pixel_size/Ny
-
-        FX,FY = np.meshgrid( 
-            np.arange(-Nx/2, Nx/2, 1, dtype=np.float64) * dfx,
-            np.arange(-Ny/2, Ny/2, 1, dtype=np.float64) * dfy,
-            sparse=False, 
-            indexing='ij'
-            )
-
-        self.filter = 1. + self.alpha*(FX**2 + FY**2)/self.magnification  
-
-    def phase_retrieval(self, Image, normalise=False):
-        
-        if normalise:
-            Image /= np.mean(Image)
-        I = Image
-        
-        fI = fftshift(
-            fft2(self.magnification**2*I)
-            )
-        
-        tmp = fI / self.filter 
-        
-        tmp = fftshift(tmp)
-        Txy = ifft2(tmp)
-
-        np.abs(Txy, out=Txy)
-        np.log(Txy, out=Txy)
-        
-        Txy *= -1/self.mu
-        
-        if self.units_output == 'absorption':
-            return self.mu*Txy
-        elif self.units_output == 'thickness':
-            return(Txy)
-        elif self.units_output == 'phase':
-            return -self.delta*Txy*2*np.pi/self.wavelength
-        else:
-            raise ValueError("units_output not recognised: got {0} expected one of 'absorption', 'thickness' or 'phase'"\
-                            .format(self.units_output))
 
     def process(self, out=None):
         
@@ -219,7 +177,7 @@ class PaganinPhaseRetrieval(Processor):
                 procs = []
                 for idx in range(j):
                     projection = data.get_slice(angle=i+idx).as_array()
-                    procs.append(delayed(self.phase_retrieval)(projection, normalise=self.normalise))
+                    procs.append(delayed(self.process_projection)(projection))
                 # call compute on j (< num_parallel_processes) processes concurrently
                 # this limits the amount of memory required to store the output of the 
                 # phase retrieval to j projections.
@@ -233,3 +191,137 @@ class PaganinPhaseRetrieval(Processor):
             
         if must_return:
             return out
+        
+    def process_projection(self, image):
+        
+        iffI = self.paganin_filter(image)
+        if self.output_type == 'filtered_image':
+            return iffI
+        if self.output_type == 'attenuation':
+            return -np.log(iffI)
+        elif self.output_type == 'thickness':
+            return 
+        elif self.output_type == 'phase':
+            return (self.delta*2*np.pi/self.wavelength)*np.log(iffI)/self.mu
+        else:
+            raise ValueError("output_type not recognised: got {0} expected one of 'attenuation', 'thickness' or 'phase'"\
+                            .format(self.output_type))
+    
+    def filter_image(self, out=None):
+        self.output_type = 'filtered_image' 
+        return self.process(out=out)
+
+    def create_filter(self, image):
+        
+        Nx, Ny = image.shape
+
+        kx,ky = np.meshgrid( 
+            np.arange(-Nx/2, Nx/2, 1, dtype=np.float64) * (2*np.pi)/(Nx*self.pixel_size),
+            np.arange(-Ny/2, Ny/2, 1, dtype=np.float64) * (2*np.pi)/(Nx*self.pixel_size),
+            sparse=False, 
+            indexing='ij'
+            )
+        
+        kW = np.abs(kx.max()*self.pixel_size)       
+        if (kW >= 1): 
+            logging.warning("This algorithm is valid for k*W << 1, found np.abs(kx.max()*self.pixel_size) = {}, results may not be accurate".format(kW))
+
+        self.filter =  (1. + self.alpha*(kx**2 + ky**2)/self.magnification)
+    
+    def paganin_filter(self, image):
+        
+        fI = fftshift(
+            fft2(self.magnification**2*image)
+            )
+        
+        iffI = ifft2(fftshift(fI/self.filter))
+
+        return iffI
+    
+
+class GeneralisedPhaseRetrieval(PaganinPhaseRetrieval):
+    
+    def create_filter(self, image):
+        
+        Nx, Ny = image.shape
+
+        kx,ky = np.meshgrid( 
+            np.arange(-Nx/2, Nx/2, 1, dtype=np.float64) * (2*np.pi)/(Nx*self.pixel_size),
+            np.arange(-Ny/2, Ny/2, 1, dtype=np.float64) * (2*np.pi)/(Nx*self.pixel_size),
+            sparse=False, 
+            indexing='ij'
+            )
+        
+        kW = np.abs(kx.max()*self.pixel_size)       
+        if (kW > np.pi): 
+            logging.warning("This algorithm is valid for |k*W| <= pi, found np.abs(kx.max()*self.pixel_size) = {}, results may not be accurate".format(kW))
+
+        self.filter =  (1. - (2*self.alpha/self.pixel_size**2)*(np.cos(self.pixel_size*kx) + np.cos(self.pixel_size*ky) -2)/self.magnification)
+
+class OriginalPhaseRetrieval(PaganinPhaseRetrieval):
+    def __calculate_alpha(self):
+        self.alpha = 4.0*(np.pi**2)*self.propagation_distance*self.delta/self.mu
+
+    def check_input(self, dataset):
+        geometry = dataset.geometry
+        if (geometry.pixel_size_h - geometry.pixel_size_v ) / \
+            (geometry.pixel_size_h + geometry.pixel_size_v ) < 1e-5:
+            self.pixel_size = (geometry.pixel_size_h / geometry.magnification)* self.unit_multiplier
+        else:
+            raise ValueError('Panel pixel size is not homogeneous up to 1e-5: got {} {}'\
+                    .format( geometry.pixel_size_h, geometry.pixel_size_v )
+                )
+        
+        if geometry.magnification == None:
+            self.magnification = 1
+        else:
+            self.magnification = geometry.magnification
+        
+        if self.propagation_distance_user is None:
+            distance_origin_detector = geometry.dist_center_detector
+            distance_source_origin   = geometry.dist_source_center
+            z = distance_origin_detector * distance_source_origin/(distance_source_origin + distance_origin_detector)
+            self.propagation_distance = z* self.unit_multiplier
+        else:
+            self.propagation_distance = self.propagation_distance_user
+        
+        self.__calculate_alpha()
+        return True
+    
+    def create_filter(self, image):
+        Nx, Ny = image.shape
+        dfx = 1./self.pixel_size/Nx
+        dfy = 1./self.pixel_size/Ny
+
+        FX,FY = np.meshgrid( 
+            np.arange(-Nx/2, Nx/2, 1, dtype=np.float64) * dfx,
+            np.arange(-Ny/2, Ny/2, 1, dtype=np.float64) * dfy,
+            sparse=False, 
+            indexing='ij'
+            )
+
+        self.filter = 1. + self.alpha*(FX**2 + FY**2)/self.magnification 
+
+    def retrieve_phase(self, Image):
+        
+        I = Image    
+        fI = fftshift(
+                        fft2(self.magnification**2*I)
+                    )
+
+        tmp = fI / self.filter 
+        tmp = fftshift(tmp)
+        Txy = ifft2(tmp)
+        np.abs(Txy, out=Txy)
+        np.log(Txy, out=Txy)
+        Txy *= -1/self.mu
+
+        if self.output_type == 'attenuation':
+            return self.mu*Txy
+        elif self.output_type == 'thickness':
+            return Txy
+        elif self.output_type == 'phase':
+            return -self.delta*Txy*2*np.pi/self.wavelength
+        else:
+            raise ValueError("output_type not recognised: got {0} expected one of 'attenuation', 'thickness' or 'phase'"\
+                            .format(self.output_type))

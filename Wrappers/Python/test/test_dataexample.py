de37399bbd99512e5e93c725298294465d8262b8
hrobarts <hannah.robarts@stfc.ac.uk>
2024-03-20 08:39:52 +0000

    Make zip file a temp named file

--- Wrappers/Python/test/test_dataexample.py
+++ Wrappers/Python/test/test_dataexample.py
@@ -28,6 +28,7 @@ import numpy as np
 from unittest.mock import patch, MagicMock 
 from urllib import request
 from zipfile import ZipFile
+from tempfile import NamedTemporaryFile
 from io import StringIO
 
 initialise_tests()
@@ -158,41 +159,68 @@ class TestRemoteData(unittest.TestCase):
     def setUp(self):
         
         self.data_list = ['WALNUT','USB','KORN','SANDSTONE']
-        self.tmp_file = 'tmp.txt'
-        self.tmp_zip = 'tmp.zip'
-        with ZipFile(self.tmp_zip, 'w') as zipped_file:
-            zipped_file.writestr(self.tmp_file, np.array([1, 2, 3]))
-        with open(self.tmp_zip, 'rb') as zipped_file:
-            self.zipped_bytes = zipped_file.read()        
+        # self.tmp_file = 'tmp.txt'
+        # self.tmp_zip = 'tmp.zip'
+        # with ZipFile(self.tmp_zip, 'w') as zipped_file:
+        #     zipped_file.writestr(self.tmp_file, np.array([1, 2, 3]))
+        # with open(self.tmp_zip, 'rb') as zipped_file:
+        #     self.zipped_bytes = zipped_file.read()        
     
-    def tearDown(self):
-        for data in self.data_list:
-            test_func = getattr(dataexample, data)
-            if os.path.exists(os.path.join(test_func.FOLDER)):
-                shutil.rmtree(test_func.FOLDER)
+    # def tearDown(self):
+    #     for data in self.data_list:
+    #         test_func = getattr(dataexample, data)
+    #         if os.path.exists(os.path.join(test_func.FOLDER)):
+    #             shutil.rmtree(test_func.FOLDER)
         
-        if os.path.exists(self.tmp_zip):
-            os.remove(self.tmp_zip)
+    #     if os.path.exists(self.tmp_zip):
+    #         os.remove(self.tmp_zip)
 
-        if os.path.exists(self.tmp_file):
-            os.remove(self.tmp_file)
+    #     if os.path.exists(self.tmp_file):
+    #         os.remove(self.tmp_file)
 
-    def mock_urlopen(self, mock_urlopen):
+    def mock_urlopen(self, mock_urlopen, zipped_bytes):
         mock_response = MagicMock()
-        mock_response.read.return_value = self.zipped_bytes
+        mock_response.read.return_value = zipped_bytes
         mock_response.__enter__.return_value = mock_response
         mock_urlopen.return_value = mock_response
 
     @patch('cil.utilities.dataexample.urlopen')
     def test_unzip_remote_data(self, mock_urlopen):
-        self.mock_urlopen(mock_urlopen)
-        dataexample.REMOTEDATA._download_and_extract_from_url('.')
-        self.assertTrue(os.path.isfile(self.tmp_file))
+
+        tf = NamedTemporaryFile()
+        tf_name = tf.name
+        zip_name = tf_name + '.zip'
+        with ZipFile(zip_name, 'w') as zipped_file:
+                    zipped_file.writestr(os.path.basename(tf_name), np.array([1, 2, 3]))
+        tf.close()
+        with open(zip_name , 'rb') as zipped_file:
+            zipped_bytes = zipped_file.read() 
+        
+        self.mock_urlopen(mock_urlopen, zipped_bytes)
+        dataexample.REMOTEDATA._download_and_extract_from_url(os.path.dirname(tf_name))
+        
+        self.assertTrue(os.path.isfile(tf_name))
+
+        if os.path.exists(zip_name):
+            os.remove(zip_name)
+
+        if os.path.exists(tf_name):
+            os.remove(tf_name)
 
     @patch('cil.utilities.dataexample.input', return_value='n')    
     @patch('cil.utilities.dataexample.urlopen')
     def test_download_data_input_n(self, mock_urlopen, input):
-        self.mock_urlopen(mock_urlopen)
+        
+        tf = NamedTemporaryFile()
+        tf_name = tf.name
+        zip_name = tf_name + '.zip'
+        with ZipFile(zip_name, 'w') as zipped_file:
+                    zipped_file.writestr(os.path.basename(tf_name), np.array([1, 2, 3]))
+        tf.close()
+        with open(zip_name , 'rb') as zipped_file:
+            zipped_bytes = zipped_file.read() 
+
+        self.mock_urlopen(mock_urlopen, zipped_bytes)
 
         data_list = ['WALNUT','USB','KORN','SANDSTONE']
         for data in data_list:
@@ -200,28 +228,47 @@ class TestRemoteData(unittest.TestCase):
             capturedOutput = StringIO()                 
             sys.stdout = capturedOutput  
             test_func = getattr(dataexample, data)
-            test_func.download_data('.')
-            
-            self.assertFalse(os.path.isfile(self.tmp_file))
+            test_func.download_data(tf_name)            
+            self.assertFalse(os.path.isfile(os.path.join(tf_name, test_func.FOLDER, os.path.basename(tf_name))))
             self.assertEqual(capturedOutput.getvalue(),'Download cancelled\n')
-        
         # return to standard print output
         sys.stdout = sys.__stdout__ 
+        
+        if os.path.exists(zip_name):
+            os.remove(zip_name)
+
+        if os.path.exists(tf_name):
+            shutil.rmtree(tf_name)
 
     @patch('cil.utilities.dataexample.input', return_value='y')    
     @patch('cil.utilities.dataexample.urlopen')
     def test_download_data_input_y(self, mock_urlopen, input):
-        self.mock_urlopen(mock_urlopen)
+        
+        tf = NamedTemporaryFile()
+        tf_name = tf.name
+        zip_name = tf_name + '.zip'
+        with ZipFile(zip_name, 'w') as zipped_file:
+                    zipped_file.writestr(os.path.basename(tf_name), np.array([1, 2, 3]))
+        tf.close()
+        with open(zip_name , 'rb') as zipped_file:
+            zipped_bytes = zipped_file.read() 
+
+        self.mock_urlopen(mock_urlopen, zipped_bytes)
 
         # redirect print output
         capturedOutput = StringIO()                 
         sys.stdout = capturedOutput         
 
-        
         for data in self.data_list:
             test_func = getattr(dataexample, data)
-            test_func.download_data('.')
-            self.assertTrue(os.path.isfile(os.path.join(test_func.FOLDER,self.tmp_file)))
+            test_func.download_data(tf_name)
+            self.assertTrue(os.path.isfile(os.path.join(tf_name, test_func.FOLDER, os.path.basename(tf_name))))
         
         # return to standard print output
         sys.stdout = sys.__stdout__ 
+        
+        if os.path.exists(zip_name):
+            os.remove(zip_name)
+
+        if os.path.exists(tf_name):
+            shutil.rmtree(tf_name)

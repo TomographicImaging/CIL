fe21db09b1f28d9ac5e3bd8f3d2efa0659889a09
Margaret Duff <margaret.duff@stfc.ac.uk>
2024-03-21 13:20:07 +0000

    Try to fix SGD docs

--- Wrappers/Python/cil/optimisation/functions/SGFunction.py
+++ Wrappers/Python/cil/optimisation/functions/SGFunction.py
@@ -27,14 +27,14 @@ from .Function import SumFunction
 
 class SGFunction(ApproximateGradientSumFunction):
 
-    """
+    r"""
     Stochastic gradient function, a child class of `ApproximateGradientSumFunction`, which defines from a list of functions, :math:`{f_0,...,f_{n-1}}` a `SumFunction`, :math:`f_0+...+f_{n-1}` where each time the `gradient` is called, the `sampler` provides an index, :math:`i \in {0,...,n-1}` 
-   and the gradient function returns the approximate gradient :math:`n\nabla_xf_i(x)`. This can be used with the `cil.optimisation.algorithms` algorithm GD to give a stochastic gradient descent algorithm. 
+    and the gradient function returns the approximate gradient :math:`\nabla_x f_i(x)`. This can be used with the `cil.optimisation.algorithms` algorithm GD to give a stochastic gradient descent algorithm. 
    
     Parameters:
     -----------
     functions : `list`  of functions
-                A list of functions: :code:`[f_{0}, f_{1}, ..., f_{n-1}]`. Each function is assumed to be smooth with an implemented :func:`~Function.gradient` method. All functions must have the same domain. The number of functions must be strictly greater than 1. 
+        A list of functions: :code:`[f_{0}, f_{1}, ..., f_{n-1}]`. Each function is assumed to be smooth with an implemented :func:`~Function.gradient` method. All functions must have the same domain. The number of functions must be strictly greater than 1. 
     sampler: An instance of a CIL Sampler class ( :meth:`~optimisation.utilities.sampler`) or of another class which has a `next` function implemented to output integers in {0,...,n-1}.
         This sampler is called each time gradient is called and  sets the internal `function_num` passed to the `approximate_gradient` function.  The `num_indices` must match the number of functions provided. Default is `Sampler.random_with_replacement(len(functions))`. 
     """

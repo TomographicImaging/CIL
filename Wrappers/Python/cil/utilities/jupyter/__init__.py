b007e9459c9acb57c5693c519c7dd4bfdab87fc7
Casper da Costa-Luis <casper.dcl@physics.org>
2024-04-18 13:50:49 +0100

    bump ipywidgets (#1780)
    fixes #1600

--- Wrappers/Python/cil/utilities/jupyter/__init__.py
+++ Wrappers/Python/cil/utilities/jupyter/__init__.py
@@ -22,15 +22,7 @@ try:
     from ipywidgets import interactive_output
     import ipywidgets as widgets
 except ImportError as ie:
-    raise ImportError(ie , "\n\n", 
-                      "islicer requires the additional package ipywidgets\n" +
-                      "Please install it via conda as ipywidgets from the conda-forge channel\n")
-
-from packaging import version
-if version.parse(widgets.__version__) >= version.parse('8'):
-    raise ImportError(f'{__name__} requires ipywidgets version < 8,'
-                      + f' found version {widgets.__version__}')
-
+    raise ImportError("please conda/pip install ipywidgets") from ie
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import numpy

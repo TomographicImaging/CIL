de78fc8da4da44adb03ae55eeb8233febc677b22
effepivi <franck.p.vidal@gmail.com>
2024-05-28 16:52:52 +0100

    Add SOUV vector.

--- Wrappers/Python/cil/plugins/astra/utilities/convert_geometry_to_astra_vec_3D.py
+++ Wrappers/Python/cil/plugins/astra/utilities/convert_geometry_to_astra_vec_3D.py
@@ -42,19 +42,24 @@ def convert_geometry_to_astra_vec_3D(volume_geometry, sinogram_geometry_in):
 
     sinogram_geometry = sinogram_geometry_in.copy()
 
-    #this catches behaviour modified after CIL 21.3.1
-    try:
-        sinogram_geometry.config.system.align_reference_frame('cil')
-    except:
-        sinogram_geometry.config.system.update_reference_frame()
+    # Only needed when using the traditional geometry set with rotation angles 
+    if sinogram_geometry.geom_type != 'cone_souv':
+        
+        #this catches behaviour modified after CIL 21.3.1    
+        try:
+            sinogram_geometry.config.system.align_reference_frame('cil')
+        except:
+            sinogram_geometry.config.system.update_reference_frame()
 
 
-    angles = sinogram_geometry.config.angles
+        angles = sinogram_geometry.config.angles
+
+        #get units
+        degrees = angles.angle_unit == sinogram_geometry.DEGREE
+
     system = sinogram_geometry.config.system
     panel = sinogram_geometry.config.panel
-
-    #get units
-    degrees = angles.angle_unit == sinogram_geometry.DEGREE
+        
 
     if sinogram_geometry.dimension == '2D':
         #create a 3D astra geom from 2D CIL geometry
@@ -91,34 +96,52 @@ def convert_geometry_to_astra_vec_3D(volume_geometry, sinogram_geometry_in):
     else:
         volume_geometry_temp = volume_geometry.copy()
 
-        row = panel.pixel_size[0] * system.detector.direction_x.reshape(3,1)
-        col = panel.pixel_size[1] * system.detector.direction_y.reshape(3,1)
-        det = system.detector.position.reshape(3, 1)
+        # Only needed when using the traditional geometry set with rotation angles 
+        if sinogram_geometry.geom_type != 'cone_souv':
+            row = panel.pixel_size[0] * system.detector.direction_x.reshape(3,1)
+            col = panel.pixel_size[1] * system.detector.direction_y.reshape(3,1)
+            det = system.detector.position.reshape(3, 1)
 
-        if 'right' in panel.origin:
-            row *= -1
-        if 'top' in panel.origin:
-            col *= -1
+            if 'right' in panel.origin:
+                row *= -1
+            if 'top' in panel.origin:
+                col *= -1
 
-        if sinogram_geometry.geom_type == 'parallel':
-            src = system.ray.direction.reshape(3,1)
-            projector = 'parallel3d_vec'
+            if sinogram_geometry.geom_type == 'parallel':
+                src = system.ray.direction.reshape(3,1)
+                projector = 'parallel3d_vec'
+            elif sinogram_geometry.geom_type != 'cone_souv':
+                src = system.source.position.reshape(3,1)
+                projector = 'cone_vec'
+        # Use the per-projection geometry
         else:
-            src = system.source.position.reshape(3,1)
             projector = 'cone_vec'
 
     #Build for astra 3D only
-    vectors = np.zeros((angles.num_positions, 12))
+    # Use the traditional geometry set with rotation angles
+    if sinogram_geometry.geom_type != 'cone_souv':
+        vectors = np.zeros((angles.num_positions, 12))
 
-    for i, theta in enumerate(angles.angle_data):
-        ang = - angles.initial_angle - theta
+        for i, theta in enumerate(angles.angle_data):
+            ang = - angles.initial_angle - theta
 
-        rotation_matrix = rotation_matrix_z_from_euler(ang, degrees=degrees)
+            rotation_matrix = rotation_matrix_z_from_euler(ang, degrees=degrees)
 
-        vectors[i, :3]  = rotation_matrix.dot(src).reshape(3)
-        vectors[i, 3:6] = rotation_matrix.dot(det).reshape(3)
-        vectors[i, 6:9] = rotation_matrix.dot(row).reshape(3)
-        vectors[i, 9:]  = rotation_matrix.dot(col).reshape(3)
+            vectors[i, :3]  = rotation_matrix.dot(src).reshape(3)
+            vectors[i, 3:6] = rotation_matrix.dot(det).reshape(3)
+            vectors[i, 6:9] = rotation_matrix.dot(row).reshape(3)
+            vectors[i, 9:]  = rotation_matrix.dot(col).reshape(3)
+    # Use the per-projection geometry
+    else:
+        vectors = np.zeros((system.num_positions, 12))
+
+        for i, (src, det, row, col) in enumerate(zip(system.source.position_set, system.detector.position_set, system.detector.direction_x_set, system.detector.direction_y_set)):
+
+            vectors[i, :3]  = src.reshape(3)
+            vectors[i, 3:6] = det.reshape(3)
+            vectors[i, 6:9] = row.reshape(3)
+            vectors[i, 9:]  = col.reshape(3)
+
 
     proj_geom = astra.creators.create_proj_geom(projector, panel.num_pixels[1], panel.num_pixels[0], vectors)
     vol_geom = astra.create_vol_geom(volume_geometry_temp.voxel_num_y,

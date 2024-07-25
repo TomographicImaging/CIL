07ebfbe0343b6e16091b92f26f65c17c8c42ea4b
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-07-25 17:01:39 +0100

    added function to create TV + LS problem

--- Wrappers/Python/cil/optimisation/utilities/PDHG.py
+++ Wrappers/Python/cil/optimisation/utilities/PDHG.py
@@ -3,8 +3,8 @@ from cil.optimisation.functions import MixedL21Norm, BlockFunction, L2NormSquare
 from cil.optimisation.operators import BlockOperator, IdentityOperator, GradientOperator, \
     SymmetrisedGradientOperator, ZeroOperator
 
-def setup_explicit_TGV(A, data, alpha, delta=1.0):
-    '''Class to setup LS + TGV problem with explicit PDHG
+def setup_explicit_TGV(A, data, alpha, delta=1.0, omega=1):
+    '''Function to setup LS + TGV problem with explicit PDHG
 
     Parameters
     ----------
@@ -15,15 +15,23 @@ def setup_explicit_TGV(A, data, alpha, delta=1.0):
         Regularisation parameter.
     delta : float, default 1.0
         The Regularisation parameter for the symmetrised gradient, beta, can be controlled by delta
-        with beta = delta * alpha.       
+        with beta = delta * alpha.
+    omega : float, default 1.0
+        The constant in front of the data fitting term. Mathematicians like it to be 1/2 but it is 1 by default, 
+        i.e. it is ignored if it is 1.
+
+    Returns:
+    --------
+    K : BlockOperator
+    F : BlockFunction
     '''
 
     # delta = beta / alpha
     # beta =  alpha * delta
-    
     beta = alpha * delta
-    
-    f1 = 0.5 * L2NormSquared(b=data)
+    f1 = L2NormSquared(b=data)
+    if omega != 1:
+        f1 = omega * L2NormSquared(b=data)
     f2 = MixedL21Norm()
     f3 = MixedL21Norm() 
     F = BlockFunction(f1, f2, f3)         
@@ -43,5 +51,43 @@ def setup_explicit_TGV(A, data, alpha, delta=1.0):
     K31 = ZeroOperator(K11.domain, K32.range)
 
     K = BlockOperator(K11, K12, K21, K22, K31, K32, shape=(3,2) )
+
+    return K, F
+
+
+def setup_explicit_TV(A, data, alpha, omega=1):
+    '''Function to setup LS + TV problem with explicit PDHG
+
+    Parameters
+    ----------
+    A : ProjectionOperator
+        Forward operator.
+    data : AcquisitionData
+    alpha : float
+        Regularisation parameter.
+    omega : float, default 1.0
+        The constant in front of the data fitting term. Mathematicians like it to be 1/2 but it is 1 by default, 
+        i.e. it is ignored if it is 1.
+
+    Returns:
+    --------
+    K : BlockOperator
+    F : BlockFunction
+
+    '''
+
+    f1 = L2NormSquared(b=data)
+    if omega != 1:
+        f1 = omega * L2NormSquared(b=data)
+    f2 = MixedL21Norm()
+    F = BlockFunction(f1, f2)         
+
+    # Define BlockOperator K
+                
+    K11 = A
+    grad = GradientOperator(K11.domain)
+    K21 = alpha * grad
+    
+    K = BlockOperator(K11, K21)
 
     return K, F

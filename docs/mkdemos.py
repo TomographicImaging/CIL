27acec00b808c04232acc4114fd6c403cbbfc86e
hrobarts <hannah.robarts@stfc.ac.uk>
2024-10-29 09:12:30 +0000

    Add

--- docs/mkdemos.py
+++ docs/mkdemos.py
@@ -14,7 +14,8 @@ from tqdm import tqdm
 NOTEBOOKS = (
     "https://github.com/TomographicImaging/CIL-Demos/raw/main/demos/1_Introduction/00_CIL_geometry.ipynb",
     "https://github.com/TomographicImaging/CIL-User-Showcase/raw/main/003_1D_integral_inverse_problem/deriv2_cgls.ipynb",
-    "https://github.com/TomographicImaging/CIL-Demos/raw/main/misc/callback_demonstration.ipynb"
+    "https://github.com/TomographicImaging/CIL-Demos/raw/main/misc/callback_demonstration.ipynb",
+    "https://cil.stfc.ac.uk/manila/files/materials/Rendered%20notebooks/TIFFStackReader.ipynb"
 )
 SOURCE = Path(__file__).parent / "source" # sphinx documentation dir
 NBDIR = "demos" # notebook subdir to create

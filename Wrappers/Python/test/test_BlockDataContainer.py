37d93b2b65190af9aa2aa6e29f8bdc8ab144b104
Margaret Duff <margaret.duff@stfc.ac.uk>
2024-12-11 11:17:18 +0000

    Allow SAPYB for block to take None

--- Wrappers/Python/test/test_BlockDataContainer.py
+++ Wrappers/Python/test/test_BlockDataContainer.py
@@ -463,10 +463,10 @@ class TestBlockDataContainer(BDCUnittest):
         ig1 = ImageGeometry(2,3,5)
 
         data0 = ig0.allocate(-1)
-        data2 = ig0.allocate(1)
+        data2 = ig1.allocate(1)
 
         data1 = ig0.allocate(2)
-        data3 = ig0.allocate(3)
+        data3 = ig1.allocate(3)
 
         cp0 = BlockDataContainer(data0,data2)
         cp1 = BlockDataContainer(data1,data3)
@@ -478,7 +478,32 @@ class TestBlockDataContainer(BDCUnittest):
         # operation should be [  3 * -1 + (-2) * 2 , 3 * 1 + (-2) * 3 ]
         # output should be [ -7 , -3 ]
         res0 = ig0.allocate(-7)
-        res2 = ig0.allocate(-3)
+        res2 = ig1.allocate(-3)
+        res = BlockDataContainer(res0, res2)
+
+        self.assertBlockDataContainerEqual(out, res)
+        
+    def test_sapyb_out_none(self):
+        # test axpby between BlockDataContainers
+        ig0 = ImageGeometry(2,3,4)
+        ig1 = ImageGeometry(2,3,5)
+
+        data0 = ig0.allocate(-1)
+        data2 = ig1.allocate(1)
+
+        data1 = ig0.allocate(2)
+        data3 = ig1.allocate(3)
+
+        cp0 = BlockDataContainer(data0,data2)
+        cp1 = BlockDataContainer(data1,data3)
+
+
+        out = cp0.sapyb(3,cp1, -2, num_threads=4)
+
+        # operation should be [  3 * -1 + (-2) * 2 , 3 * 1 + (-2) * 3 ]
+        # output should be [ -7 , -3 ]
+        res0 = ig0.allocate(-7)
+        res2 = ig1.allocate(-3)
         res = BlockDataContainer(res0, res2)
 
         self.assertBlockDataContainerEqual(out, res)

16b13e744c15d4106b91c14a2781588b45245dc3
Margaret Duff <margaret.duff@stfc.ac.uk>
2024-12-19 14:02:48 +0000

    Tolerance on one test

--- Wrappers/Python/test/test_algorithm_convergence.py
+++ Wrappers/Python/test/test_algorithm_convergence.py
@@ -103,4 +103,4 @@ class TestAlgorithmConvergence(CCPiTestClass):
         self.assertNumpyArrayAlmostEqual( 
             alg_stochastic.x.as_array(), u_cvxpy.value)
         self.assertDataContainerAllClose(
-            alg_stochastic.x, b, rtol=1e-8)
+            alg_stochastic.x, b, rtol=1e-8, atol=1e-8)

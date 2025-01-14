3f2279bfdfb03a27e4946c26645bc169ef6607dc
hrobarts <hannah.robarts@stfc.ac.uk>
2025-01-14 14:40:20 +0000

    Check seed gives same random output

--- Wrappers/Python/test/test_AcquisitionGeometry.py
+++ Wrappers/Python/test/test_AcquisitionGeometry.py
@@ -519,16 +519,18 @@ class Test_AcquisitionGeometry(unittest.TestCase):
         self.assertEqual(test3.shape, test2.shape)
 
         test4 = AG.allocate('random', seed=42)
+        test5 = AG.allocate('random', seed=42)
         self.assertEqual(test4.shape, test2.shape)
+        np.testing.assert_array_almost_equal(test4.array, test5.array)
 
-        test5 = AG.allocate('random_int')
-        self.assertEqual(test5.shape, test2.shape)
-        self.assertEqual(test5.dtype, AG.dtype)
-
-        test6 = AG.allocate('random_int', seed=42, max_value=10, dtype=np.uint8)
+        test6 = AG.allocate('random_int')
         self.assertEqual(test6.shape, test2.shape)
-        self.assertEqual(test6.dtype, np.uint8)
-        self.assertTrue(np.all(test6 < 10))
+        self.assertEqual(test6.dtype, AG.dtype)
+
+        test7 = AG.allocate('random_int', seed=42, max_value=10, dtype=np.uint8)
+        self.assertEqual(test7.shape, test2.shape)
+        self.assertEqual(test7.dtype, np.uint8)
+        self.assertTrue(np.all(test7 < 10))
 
         test7 = AG.allocate(11)
         np.testing.assert_array_almost_equal(test7.array, np.ones([2,5,3,4])*11)

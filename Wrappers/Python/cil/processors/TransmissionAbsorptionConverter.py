a6c96e38e648c456f82b3208d71782849b1462a8
Robarts <Hannah.Robarts@stfc.ac.uk>
2025-02-27 17:22:19 +0000

    Move multiply and clip into numba loop, remove data.min check

--- Wrappers/Python/cil/processors/TransmissionAbsorptionConverter.py
+++ Wrappers/Python/cil/processors/TransmissionAbsorptionConverter.py
@@ -29,17 +29,16 @@ class TransmissionAbsorptionConverter(DataProcessor):
 
     Parameters:
     -----------
-
-    white_level: float, optional
+    min_intensity: float, default=0
+        Clips data below this value to ensure log is taken of positive numbers only. 
+        If you're unsure whether the data contains non-positive values, set this parameter to a small positive value.
+    
+    white_level: float, default=1.0
         A float defining incidence intensity in the Beer-Lambert law.
 
-    min_intensity: float, optional
-        A float defining some threshold to avoid 0 in log, is applied after normalisation by white_level
-    
-    accelerated: bool, optional
+    accelerated: bool, default=True
         Specify whether to use multi-threading using numba. 
-        Default is True
-
+    
     Returns:
     --------
     AcquisitionData, ImageData or DataContainer depending on input data type, return is suppressed if 'out' is passed
@@ -51,7 +50,7 @@ class TransmissionAbsorptionConverter(DataProcessor):
     '''
 
     def __init__(self,
-                 min_intensity = 0.0,
+                 min_intensity = 0,
                  white_level = 1.0,
                  accelerated = True,
                  ):
@@ -69,10 +68,9 @@ class TransmissionAbsorptionConverter(DataProcessor):
                             ' - ImageData\n - AcquisitionData\n' +
                             ' - DataContainer')
 
-        # Check if this is slow. Maybe we can remove this and catch the error when we run log
-        if data.min() <= 0 and self.min_intensity <= 0:
-            raise ValueError('Zero or negative values found in the dataset. Please use `min_intensity` to provide a clipping value.')
-
+        if self.min_intensity <= 0:
+            warnings.warn(f"Please ensure your data only contains positive values or set min_intensity to a small positive value. Current min_intensity = {self.min_intensity}. ")
+        
         return True
 
     def process(self, out=None):
@@ -85,35 +83,27 @@ class TransmissionAbsorptionConverter(DataProcessor):
         arr_in = data.as_array()
         arr_out = out.as_array()
 
-        #whitelevel. 
-        # Move this inside the numba loop. Check if it's quicker to always divide even if =1, or we have if statement in loop or make 2 different loops
-        # Multiply by 1/white level instead of divide?
-        if self.white_level != 1:
-            numpy.divide(arr_in, self.white_level, out=arr_out)
-            arr_in = arr_out
-
-        #threshold. Move this inside the numba loop and get rid of check? If log fails catch error. If they process in place this will corrupt the data if it fails half way through
-        if self.min_intensity > 0:
-            numpy.clip(arr_in, self.min_intensity, None, out=arr_out)
-            arr_in = arr_out
-
         # we choose an arbitrary chunk size of 6400, which is a multiple of 32, to allow for efficient threading
         chunk_size = 6400
         num_chunks = data.size // chunk_size
-        # Accelerate using numba if _accelerated is True, 
-        # and if the calculated number of chunks is greater than 5, to avoid making the overhead of threading too large
+        # Use numba if _accelerated is True and if the number of chunks is greater than 5, to avoid the overhead of threading when the data is small
         if (self._accelerated) & (num_chunks > 5):
-            # we process in an integer number of chunks, so calculate the remainder to process after the loop
             remainder = data.size % chunk_size
-            # set the number of threads to the value specified in cil_mp.NUM_THREADS
             num_threads_original = numba.get_num_threads()
             numba.set_num_threads(cil_mp.NUM_THREADS)
-            numba_loop(arr_in, num_chunks, chunk_size, remainder, arr_out)
-            # reset the number of threads to the original value
+            numba_loop(arr_in, num_chunks, chunk_size, remainder, 1/self.white_level, self.min_intensity,  arr_out)
             numba.set_num_threads(num_threads_original)
 
         else:
-            numpy.log(arr_in,out=arr_out)
+            if self.white_level != 1:
+                numpy.multiply(arr_in, 1/self.white_level, out=arr_out)
+                arr_in = arr_out
+
+            if self.min_intensity > 0:
+                numpy.clip(arr_in, self.min_intensity, None, out=arr_out)
+                arr_in = arr_out
+                
+            numpy.log(arr_in, out=arr_out)
             numpy.negative(arr_out,out=arr_out)
 
         out.fill(arr_out)
@@ -121,19 +111,21 @@ class TransmissionAbsorptionConverter(DataProcessor):
         return out
     
 @numba.njit(parallel=True)
-def numba_loop(arr_in, num_chunks, chunk_size, remainder, arr_out):
+def numba_loop(arr_in, num_chunks, chunk_size, remainder, multiplier, min_intensity, arr_out):
     in_flat = arr_in.ravel()
     out_flat = arr_out.ravel()
     for i in numba.prange(num_chunks):
         start = i * chunk_size
         end = start + chunk_size
-        #beer-lambert
-        out_flat[start:end] = -numpy.log(in_flat[start:end])
+        out_flat[start:end] = numpy.multiply(in_flat[start:end], multiplier)
+        out_flat[start:end] = numpy.clip(out_flat[start:end], min_intensity, None)
+        out_flat[start:end] = -numpy.log(out_flat[start:end])
 
     if remainder > 0:
         start = num_chunks * chunk_size
         end = start + remainder
-        #beer-lambert
-        out_flat[start:end] = -numpy.log(in_flat[start:end])
+        out_flat[start:end] = numpy.multiply(in_flat[start:end], multiplier)
+        out_flat[start:end] = numpy.clip(out_flat[start:end], min_intensity, None)
+        out_flat[start:end] = -numpy.log(out_flat[start:end])
 
 

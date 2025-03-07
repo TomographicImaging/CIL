6b323da8a195c74650349b8ce4045f1e14266c89
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-03-07 09:01:05 +0000

    test import error if has file

--- Wrappers/Python/cil/io/ZEISSDataReader.py
+++ Wrappers/Python/cil/io/ZEISSDataReader.py
@@ -160,12 +160,11 @@ class ZEISSDataReader(object):
 
     def read_metadata(self):
         # defer import
-        import olefile
         import dxchange
-
         # Read one image to get the metadata
         _,metadata = dxchange.read_txrm(self.file_name,((0,1),(None),(None)))
 
+        import olefile
         with olefile.OleFileIO(self.file_name) as ole:
             #Configure beam geometry
             xray_geometry = dxchange.reader._read_ole_value(ole, 'ImageInfo/XrayGeometry', '<i')

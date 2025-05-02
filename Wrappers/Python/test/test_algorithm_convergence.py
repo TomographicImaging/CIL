e88749677cd55a15a4d12810780a3e125aba09d2
Margaret Duff <43645617+MargaretDuff@users.noreply.github.com>
2025-05-02 17:40:03 +0100

    Split FISTA and APGD to give more options for momentum (#2061)
    ---------
    
    Signed-off-by: Margaret Duff <43645617+MargaretDuff@users.noreply.github.com>
    Co-authored-by: epapoutsellis <epapoutsellis@gmail.com>

--- Wrappers/Python/test/test_algorithm_convergence.py
+++ Wrappers/Python/test/test_algorithm_convergence.py
@@ -1,10 +1,12 @@
-from cil.optimisation.algorithms import SPDHG, PDHG, GD, PD3O
-from cil.optimisation.functions import L2NormSquared, IndicatorBox, BlockFunction, ZeroFunction, LeastSquares, TotalVariation, MixedL21Norm
+from cil.optimisation.algorithms import SPDHG, PDHG, FISTA, APGD, GD, PD3O
+from cil.optimisation.functions import L2NormSquared, IndicatorBox, BlockFunction, ZeroFunction, KullbackLeibler, OperatorCompositionFunction, LeastSquares, TotalVariation, MixedL21Norm
 from cil.optimisation.operators import BlockOperator, IdentityOperator, MatrixOperator, GradientOperator
 from cil.optimisation.utilities import Sampler, BarzilaiBorweinStepSizeRule
-from cil.framework import AcquisitionGeometry, BlockDataContainer, BlockGeometry, VectorData
+from cil.framework import AcquisitionGeometry, BlockDataContainer, BlockGeometry, VectorData, ImageGeometry
+
 
 from cil.utilities import dataexample
+from cil.utilities import noise as applynoise
 
 import numpy as np
 import unittest
@@ -106,6 +108,83 @@ class TestAlgorithmConvergence(CCPiTestClass):
             alg_stochastic.x.as_array(), b.as_array(), decimal=6)
         
 
+    def test_FISTA_Denoising(self):
+        # adapted from demo FISTA_Tikhonov_Poisson_Denoising.py in CIL-Demos repository
+        data = dataexample.SHAPES.get()
+        ig = data.geometry
+        ag = ig
+        # Create Noisy data with Poisson noise
+        scale = 5
+        noisy_data = applynoise.poisson(data/scale, seed=10) * scale
+
+        # Regularisation Parameter
+        alpha = 10
+
+        # Setup and run the FISTA algorithm
+        operator = GradientOperator(ig)
+        fid = KullbackLeibler(b=noisy_data)
+        reg = OperatorCompositionFunction(alpha * L2NormSquared(), operator)
+
+        initial = ig.allocate()
+        fista = FISTA(initial=initial, f=reg, g=fid)
+        fista.update_objective_interval = 500
+        fista.run(3000, verbose=0)
+        rmse = (fista.get_output() - data).norm() / data.as_array().size
+        self.assertLess(rmse, 4.2e-4)
+        
+    def test_APGD(self):
+        ig = ImageGeometry(41, 43, 47)
+        initial = ig.allocate(0)
+        b = ig.allocate("random")**2
+        identity = IdentityOperator(ig)
+
+        f = OperatorCompositionFunction(L2NormSquared(b=b), identity)
+        g= IndicatorBox(lower=0)
+        
+        apgd = APGD(f=f, g=g, initial=initial, update_objective_interval=100, momentum=0.5)
+        apgd.run(500, verbose=0)
+        self.assertNumpyArrayAlmostEqual(apgd.solution.as_array(), b.as_array(), decimal=3)
+        
+    
+
+    def test_APGD_dossal_chambolle(self):
+        
+        np.random.seed(10)
+        n = 100  
+        m = 50 
+        A = np.random.normal(0,1, (m, n)).astype('float32') 
+        b = np.random.normal(0,1, m).astype('float32')
+        reg = 0.5
+        
+        Aop = MatrixOperator(A)
+        bop = VectorData(b) 
+        ig = Aop.domain
+
+        # cvxpy solutions
+        u_cvxpy = cvxpy.Variable(ig.shape[0])
+        objective = cvxpy.Minimize( 0.5 * cvxpy.sum_squares(Aop.A @ u_cvxpy - bop.array) + reg/2 * cvxpy.sum_squares(u_cvxpy))
+        p = cvxpy.Problem(objective)
+        p.solve(verbose=False, solver=cvxpy.SCS, eps=1e-4)  
+
+        # default fista
+        f = LeastSquares(A=Aop, b=bop, c=0.5)
+        g = reg/2*L2NormSquared()        
+        fista = FISTA(initial=ig.allocate(), f=f, g=g, update_objective_interval=1)
+        fista.run(500)        
+        np.testing.assert_allclose(fista.objective[-1], p.value, atol=1e-3)
+        np.testing.assert_allclose(fista.solution.array, u_cvxpy.value, atol=1e-3)
+
+        # fista Dossal Chambolle "On the convergence of the iterates of ”FISTA”
+        from cil.optimisation.algorithms.APGD import ScalarMomentumCoefficient
+        class DossalChambolle(ScalarMomentumCoefficient):        
+            def __call__(self, algo=None):
+                return (algo.iteration-1)/(algo.iteration+50)
+        momentum = DossalChambolle()
+        fista_dc = APGD(initial=ig.allocate(), f=f, g=g, update_objective_interval=1, momentum=momentum)
+        fista_dc.run(500)
+        np.testing.assert_allclose(fista_dc.solution.array, u_cvxpy.value, atol=1e-3)
+        np.testing.assert_allclose(fista_dc.solution.array, u_cvxpy.value, atol=1e-3)
+
     def test_pd3o_convergence(self):
         data = dataexample.CAMERA.get(size=(32, 32))
         # pd30 convergence test using TV denoising
@@ -135,6 +214,7 @@ class TestAlgorithmConvergence(CCPiTestClass):
         # pd30 vs fista
         np.testing.assert_allclose(
             tv_cil.array, pd3O_with_f.solution.array, atol=1e-2)
+
         
         
     def test_bb_step_size_gd_converge(self):
@@ -170,3 +250,4 @@ class TestAlgorithmConvergence(CCPiTestClass):
         alg = GD(initial=initial, f=f, step_size=ss_rule)
         alg.run(300, verbose=0)
         self.assertNumpyArrayAlmostEqual(alg.x.as_array(), x, decimal=4)
+

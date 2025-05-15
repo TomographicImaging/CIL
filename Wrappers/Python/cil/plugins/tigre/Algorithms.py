c6225907fa365afe6772f770c477c97ed63b0189
Margaret Duff <margaret.duff@stfc.ac.uk>
2025-05-15 14:22:04 +0000

    Docstrings, 2D geometry and improved objective update calculation

--- Wrappers/Python/cil/plugins/tigre/Algorithms.py
+++ Wrappers/Python/cil/plugins/tigre/Algorithms.py
@@ -16,36 +16,63 @@
 # Authors:
 # CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
 
+import numpy as np
 from cil.optimisation.algorithms import Algorithm
 from cil.plugins.tigre import CIL2TIGREGeometry
 import tigre.algorithms as algs
 from cil.framework import ImageData
 import logging
+from tigre.utilities.Ax import Ax
+from tigre.utilities.im3Dnorm import im3DNORM
+
+from cil.framework.labels import AcquisitionDimension
 
 log = logging.getLogger(__name__)
-import numpy as np
 
 
 class ART(Algorithm):
-    r"""  TODO:
+
+    r"""
+    Algebraic Reconstruction Technique (ART) implementation using the TIGRE backend.
+
+    This class provides an interface to perform iterative image reconstruction using the ART algorithm.
+    It leverages the TIGRE library for GPU-accelerated computations and supports configurable parameters
+    such as block size and non-negativity constraints.
+
+    Parameters
+    ----------
+    initial : ImageData, optional
+            Initial guess for the reconstruction. If None, a zero-initialized image is used.
+    image_geometry : ImageGeometry
+            The geometry of the image to be reconstructed.
+    data : AcquisitionData
+            The measured projection data.
+    blocksize : int
+            Number of projections to use per iteration (subset size).
+    noneg : bool, default=True
+            If True, enforces non-negativity constraint on the reconstructed image.
+    **kwargs : dict
+            Additional keyword arguments passed to the TIGRE reconstruction algorithm.
     """
 
-    def __init__(self, initial=None, operator=None, data=None, blocksize = None, nonneg = False, **kwargs):
+    def __init__(self, initial=None, image_geometry=None,  data=None, blocksize=None, noneg=True, **kwargs):
 
-        super(ART, self).__init__(**kwargs)
+        update_objective_interval = kwargs.pop('update_objective_interval', 1)
+        super(ART, self).__init__(
+            update_objective_interval=update_objective_interval)
 
-        self.set_up(initial=initial, operator=operator, data=data, blocksize = blocksize, nonneg = nonneg)
+        self.set_up(initial=initial, image_geometry=image_geometry,
+                    data=data, blocksize=blocksize, noneg=noneg, **kwargs)
 
-
-    def set_up(self, initial=None, operator=None, data=None, blocksize = None, nonneg=False):
+    def set_up(self, initial=None, image_geometry=None,  data=None, blocksize=None, noneg=False, **kwargs):
         '''Set up the algorithm'''
-        
+
         log.info("%s setting up", self.__class__.__name__)
-        
+
         warning = 0
-        if operator is None:
+        if image_geometry is None:
             warning += 1
-            msg = "an `operator`"
+            msg = "an `image_geometry`"
         if data is None:
             warning += 10
             if warning > 10:
@@ -54,74 +81,158 @@ class ART(Algorithm):
                 msg = "`data`"
         if blocksize is None:
             warning += 10
-            if warning > 20:
+            if warning > 10:
                 msg += " and `blocksize`"
             else:
                 msg = "`blocksize`"
         if warning > 0:
-            raise ValueError(f'You must pass {msg} to the ART algorithm' )
-        
+            raise ValueError(f'You must pass {msg} to the ART algorithm')
+
         if initial is None:
-            initial = operator.domain_geometry().allocate(0)
-            
-        
-        self.initial = initial.copy()
-        self.operator = operator # TODO: is this needed or just an image and acquisition geometry 
-        self.ig = operator.domain_geometry()
-        self.ag = operator.range_geometry()
-        self.data = data
+            initial = image_geometry.allocate(0)
+
+        tigre_initial = initial.copy().as_array()
+        self.ig = image_geometry
+        self.ag = data.geometry
         self.tigre_geom, self.tigre_angles = CIL2TIGREGeometry.getTIGREGeometry(
             self.ig, self.ag)
         self.tigre_projections = data.as_array()
-        self.blocksize = blocksize
-
-        self.tigre_alg = algs.iterative_recon_alg.IterativeReconAlg( self.tigre_projections, self.tigre_geom, self.tigre_angles, niter=0, blocksize= self.blocksize)
         
+        if data.dimension_labels[0] != AcquisitionDimension.ANGLE: #TODO: Not sure when this is used/ if it is needed 
+            self.tigre_projections = np.expand_dims(self.tigre_projections, axis=0)
+            
+        if self.tigre_geom.is2D:
+            self.tigre_projections = np.expand_dims(self.tigre_projections, axis=1)
+            tigre_initial = np.expand_dims(tigre_initial, axis=0)
+            
+        self.tigre_alg = algs.iterative_recon_alg.IterativeReconAlg(
+            self.tigre_projections, self.tigre_geom, self.tigre_angles, init=tigre_initial, niter=0, blocksize=blocksize, noneg=noneg, **kwargs)
+
         self.configured = True
         log.info("%s configured", self.__class__.__name__)
+
     def update(self):
+        """
+        Performs one iteration of the ART algorithm using the TIGRE backend.
+        """
+
         self.tigre_alg.art_data_minimizing()
 
     def get_output(self):
         r""" Returns the current solution. 
-        
+
         Returns
         -------
-        DataContainer
-            The current solution 
-             
+        ImageData
+            The current estimate of the reconstructed image.
+
         """
-        return ImageData(self.tigre_alg.getres(), geometry=self.ig) # TODO: Note that this only works for CT problems at the moment
+        return ImageData(self.tigre_alg.getres(), geometry=self.ig)
 
     def update_objective(self):
-        r""" Appends the current objective value to the list of previous objective values
+        r""" 
+        Computes and stores the current value of the objective function.
+
+        The objective function is defined as:
 
         .. math:: \frac{1}{2}\|A x - b\|^{2}
 
+
+        where :math:`A` is the system matrix, :math:`x` is the current image estimate,
+        and :math:`b` is the measured projection data
+
+
         """
-        self.loss.append(0.5*np.sum(((self.operator.direct(self.get_output()).as_array() - self.data.as_array())**2))) #TODO: this could possibly be done more efficiently - TIGRE might calculate it internally 
+        self.loss.append(im3DNORM(
+            self.tigre_alg.proj - Ax(self.tigre_alg.res, self.tigre_geom,
+                                     self.tigre_angles, "Siddon", gpuids=self.tigre_alg.gpuids), 2
+        ))
 
 
+class OSSART(ART): # TODO: This is just an alias of the ART parent algorithm - do we want them both? 
 
-class OSSART(ART):
+    r"""
+    Ordered Subsets Simultaneous Algebraic Reconstruction Technique (OS-SART) from the Tigre library.
 
-    def __init__(self, initial=None, operator=None, data=None, blocksize = None, nonneg = False, **kwargs):
+    This subclass of ART implements the OS-SART algorithm, which accelerates convergence
+    by dividing the projection data into subsets (blocks) and updating the image using
+    each subset sequentially within an iteration.
 
-        super(OSSART, self).__init__(initial=initial, operator=operator, data=data, blocksize = blocksize, nonneg = nonneg, **kwargs)
-        
+    Parameters
+    ----------
+    initial : ImageData, optional
+        Initial guess for the reconstruction.
+    image_geometry : ImageGeometry
+        Geometry of the image to be reconstructed.
+    data : AcquisitionData
+        Measured projection data.
+    blocksize : int
+        Number of projections to use per subset.
+    noneg : bool, default=True
+        Enforce non-negativity constraint.
+    **kwargs : dict
+        Additional parameters for the TIGRE algorithm.
+    """
+
+
+def __init__(self, initial=None, image_geometry=None, data=None, blocksize=None, noneg=True, **kwargs):
+
+    super(OSSART, self).__init__(initial=initial, image_geometry=image_geometry,
+                                 data=data, blocksize=blocksize, noneg=noneg, **kwargs)
+
+
 class SIRT(ART):
-    
-    def __init__(self, initial=None, operator=None, data=None, nonneg = False, **kwargs):
+    """
+    Simultaneous Iterative Reconstruction Technique (SIRT) from the Tigre library. 
 
-        blocksize=len(operator.range_geometry().angles)
-        
-        super(SIRT, self).__init__(initial=initial, operator=operator, data=data, blocksize = blocksize, nonneg = nonneg, **kwargs)
-      
+    This subclass of ART implements the SIRT algorithm, which uses all projections
+    in each iteration (i.e., full blocksize). It is known for its stability and
+    smooth convergence, especially in noisy data scenarios.
+
+    Parameters
+    ----------
+    initial : ImageData, optional
+            Initial guess for the reconstruction.
+    image_geometry : ImageGeometry
+            Geometry of the image to be reconstructed.
+    data : AcquisitionData
+            Measured projection data.
+    noneg : bool, default=True
+            Enforce non-negativity constraint.
+    **kwargs : dict
+            Additional parameters for the TIGRE algorithm.
+    """
+
+    def __init__(self, initial=None, image_geometry=None, data=None, noneg=True, **kwargs):
+
+        blocksize = len(data.geometry.angles)
+
+        super(SIRT, self).__init__(initial=initial, image_geometry=image_geometry,
+                                   data=data, blocksize=blocksize, noneg=noneg, **kwargs)
+
+
 class SART(ART):
-    
-    def __init__(self, initial=None, operator=None, data=None, nonneg = False, **kwargs):
+    """ 
+    Simultaneous Algebraic Reconstruction Technique (SART) from the Tigre library.
 
-        blocksize=1
-        
-        super(SART, self).__init__(initial=initial, operator=operator, data=data, blocksize = blocksize, nonneg = nonneg, **kwargs)
-    
+    This subclass of ART implements the SART algorithm, which updates the image
+    using one projection at a time (i.e., blocksize = 1). 
+
+    Parameters
+    ----------
+    initial : ImageData, optional
+            Initial guess for the reconstruction.
+    image_geometry : ImageGeometry
+            Geometry of the image to be reconstructed.
+    data : AcquisitionData
+            Measured projection data.
+    noneg : bool, default=True
+            Enforce non-negativity constraint.
+    **kwargs : dict
+            Additional parameters for the TIGRE algorithm.
+    """
+
+    def __init__(self, initial=None, image_geometry=None, data=None, noneg=True, **kwargs):
+
+        super(SART, self).__init__(initial=initial, image_geometry=image_geometry,
+                                   data=data, blocksize=1, noneg=noneg, **kwargs)

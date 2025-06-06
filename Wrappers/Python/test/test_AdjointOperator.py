2534b09e1aaddadb18b91e1df336e27cb390db49
hrobarts <hannah.robarts@stfc.ac.uk>
2025-06-06 10:40:06 +0000

    Remove deprecated methods in test_AdjointOperator

--- Wrappers/Python/test/test_AdjointOperator.py
+++ Wrappers/Python/test/test_AdjointOperator.py
@@ -21,8 +21,8 @@ class TestAdjointOperator(CCPiTestClass):
     def test_direct_adjoint_gradient_operator(self):
 
         G = GradientOperator(self.ig_real)
-        x = G.domain_geometry().allocate("random_deprecated", seed=10)
-        y = G.range_geometry().allocate("random_deprecated", seed=10)
+        x = G.domain_geometry().allocate("random", seed=10)
+        y = G.range_geometry().allocate("random", seed=10)
         G_adjoint = AdjointOperator(G)
 
         res1 = G.adjoint(y)
@@ -44,8 +44,8 @@ class TestAdjointOperator(CCPiTestClass):
         Amat = MatrixOperator(Anp)
         Amat_tr = MatrixOperator(Anp.T)
 
-        x = Amat.domain_geometry().allocate("random_deprecated")
-        y = Amat.range_geometry().allocate("random_deprecated")
+        x = Amat.domain_geometry().allocate("random", seed=10)
+        y = Amat.range_geometry().allocate("random", seed=11)
 
         res1 = Amat.adjoint(y)
         res2 = Amat_tr.direct(y)
@@ -75,8 +75,8 @@ class TestAdjointOperator(CCPiTestClass):
         Amat = MatrixOperator(Anp)
         Amat_tr = AdjointOperator(Amat)
 
-        x = Amat.domain_geometry().allocate("random_deprecated", dtype="complex")
-        y = Amat.range_geometry().allocate("random_deprecated", dtype="complex")
+        x = Amat.domain_geometry().allocate("random", dtype="complex", seed=10)
+        y = Amat.range_geometry().allocate("random", dtype="complex", seed=11)
 
 
         # <Ax,y> = <x, A^* y> using numpy arrays
@@ -89,7 +89,6 @@ class TestAdjointOperator(CCPiTestClass):
         lhs = Amat.direct(x).dot(y) # conjugate of y is applied in the dot method
         rhs_a = x.dot(Amat.adjoint(y)) # conjugate of y is applied in the dot method
         rhs_b = x.dot(Amat_tr.direct(y)) 
-
         np.testing.assert_allclose(lhs, rhs_a, atol=1e-6)  
         np.testing.assert_allclose(lhs, rhs_b, atol=1e-6)         
 
@@ -99,9 +98,8 @@ class TestAdjointOperator(CCPiTestClass):
         G = GradientOperator(ig)
         div = AdjointOperator(G)
 
-        x = G.domain.allocate("random_int_deprecated")
-        y = G.range.allocate("random_int_deprecated")
-        print(x.array)
+        x = G.domain.allocate("random_int")
+        y = G.range.allocate("random_int")
         res1 = G.direct(x).dot(y)
         res2 = x.dot(div.direct(y))
         self.assertAlmostEqual(res1,res2)

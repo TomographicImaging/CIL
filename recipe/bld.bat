417329313ae3fe564baee50f744e6c8b6f456af8
Casper da Costa-Luis <casper.dcl@physics.org>
2025-06-10 10:25:23 +0100

    windows: auto-generator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,5 @@ set SETUPTOOLS_SCM_PRETEND_VERSION_FOR_CIL=%PKG_VERSION%
 if not "%GIT_DESCRIBE_NUMBER%"=="0" (
     set SETUPTOOLS_SCM_PRETEND_VERSION_FOR_CIL=%PKG_VERSION%.dev%GIT_DESCRIBE_NUMBER%+%GIT_DESCRIBE_HASH%
 )
-:: -G "Visual Studio 16 2019" specifies the the generator
-:: -T v142 specifies the toolset
-pip install . --no-deps -Ccmake.args="-G Visual Studio 16 2019 -T v142"
+pip install . --no-deps
 if errorlevel 1 exit 1

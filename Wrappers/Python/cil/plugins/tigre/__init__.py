99186cb7987e138794a9b5b14793549fa6d6587c
Margaret Duff <margaret.duff@stfc.ac.uk>
2025-08-20 16:57:06 +0000

    Removed old imports

--- Wrappers/Python/cil/plugins/tigre/__init__.py
+++ Wrappers/Python/cil/plugins/tigre/__init__.py
@@ -19,5 +19,4 @@
 from .Geometry import CIL2TIGREGeometry
 from .ProjectionOperator import ProjectionOperator
 from .FBP import FBP
-from .ARTAlgorithms import ART, SART, SIRT, OSSART
 from .Algorithms import tigre_algo_wrapper

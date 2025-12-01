9529715e146d73f974b3167ee806002af00397f3
Hannah Robarts <77114597+hrobarts@users.noreply.github.com>
2025-12-01 09:24:25 +0000

    Update Wrappers/Python/cil/io/TIFF.py
    Co-authored-by: Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
    Signed-off-by: Hannah Robarts <77114597+hrobarts@users.noreply.github.com>

--- Wrappers/Python/cil/io/TIFF.py
+++ Wrappers/Python/cil/io/TIFF.py
@@ -318,7 +318,7 @@ class TIFFStackReader(object):
             In 'slice' mode 'step' defines standard numpy slicing.
             Note: in general output array size in bin mode != output array size in slice mode
 
-        file_prefix : str, default 'None'
+        file_prefix : str, default None
             Leading string for the tiff files to be read. Used only when the file_name 
             is a path to a folder, if None all files in the folder are loaded. 
 

da0265f88aefbbc18f83c2decc3500dcf008e15a
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2026-02-24 18:20:36 +0000

    Fix issues with negative start point

--- Wrappers/Python/cil/io/ZEISSDataReader.py
+++ Wrappers/Python/cil/io/ZEISSDataReader.py
@@ -131,27 +131,20 @@ class ZEISSDataReader:
 
             # check roi labels and create tuple for slicing
             for key in roi.keys():
+                if key not in zeiss_data_order:
+                    raise ValueError('Invalid roi key: {}. Keys should be one of {}'.format(key, list(zeiss_data_order.keys())))
                 idx = zeiss_data_order[key]
                 if roi[key] != -1:
-                    if key == AcquisitionDimension.ANGLE:
-                        if roi[key][1] > default_roi[0][1]:
-                            raise ValueError('Requested angle range {} exceeds maximum of {}'.format(roi[key], default_roi[0][1]))
-                    elif key == ImageDimension.VERTICAL or key == AcquisitionDimension.VERTICAL:
-                        if roi[key][1] > default_roi[1][1]:
-                            raise ValueError('Requested vertical range {} exceeds maximum of {}'.format(roi[key], default_roi[1][1]))
-                    elif key == ImageDimension.HORIZONTAL_X or key == ImageDimension.HORIZONTAL_Y or key == AcquisitionDimension.HORIZONTAL:
-                        if roi[key][1] > default_roi[2][1]:
-                            raise ValueError('Requested horizontal range {} exceeds maximum of {}'.format(roi[key], default_roi[2][1]))
-
                     for i, x in enumerate(roi[key]):
                         if x is None:
                             continue
-
                         if i != 2: #start and stop
+                            adjusted_x = x if x >= 0 else default_roi[idx][1] + x
+                            if adjusted_x < 0 or adjusted_x > default_roi[idx][1]:
+                                raise ValueError('Invalid roi value: {} for key: {}. Values should be between -{} and {}'.format(x, key, default_roi[idx][1], default_roi[idx][1]))
                             default_roi[idx][i] = x if x >= 0 else default_roi[idx][1] + x
                         else: #step
                             default_roi[idx][i] =  x if x > 0 else 1
-
             self._roi = default_roi
             self._metadata = self.slice_metadata(metadata)
         else:

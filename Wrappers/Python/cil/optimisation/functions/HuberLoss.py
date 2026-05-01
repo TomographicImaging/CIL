42c645e6c6f1763699d7d16fba238bdd940feb36
Laura Murgatroyd <laura.murgatroyd@stfc.ac.uk>
2026-05-01 13:24:30 +0100

    Render equations in docs

--- Wrappers/Python/cil/optimisation/functions/HuberLoss.py
+++ Wrappers/Python/cil/optimisation/functions/HuberLoss.py
@@ -1,3 +1,22 @@
+#  Copyright 2026 United Kingdom Research and Innovation
+#  Copyright 2026 The University of Manchester
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# Authors:
+# CIL Developers, listed at: https://github.com/TomographicImaging/CIL/blob/master/NOTICE.txt
+# Martin Sæbye Carøe (Technical University of Denmark, DTU Compute)
+
 import numpy as np
 import warnings
 from numbers import Number
@@ -10,11 +29,14 @@ class HuberLoss(Function):
     r"""
     (Weighted) Huber loss
 
-    For residual r = Ax - b:
+    For residual :math:`r = Ax - b`:
 
-    phi_delta(r) =
-        0.5 * r^2                  if |r| <= delta
-        delta * (|r| - 0.5*delta)  otherwise
+    .. math::
+        \phi_\delta(r) =
+        \begin{cases}
+            0.5 * r^2 & \text{if } |r| \leq \delta \\
+            \delta * (|r| - 0.5*\delta) & \text{otherwise}
+        \end{cases}
 
     Parameters
     ----------
@@ -122,10 +144,10 @@ class HuberLoss(Function):
         Lipschitz constant of gradient.
 
         For Huber:
-            max phi'' = 1
+            .. math:: \max \phi'' = 1
         so:
-            L = c * ||A||^2
-        (weighted: multiplied by ||W||)
+            .. math:: L = c * ||A||^2
+        (weighted: multiplied by :math:`||W||`)
         """
         try:
             self._L = np.abs(self.c) * (self.A.norm() ** 2)
